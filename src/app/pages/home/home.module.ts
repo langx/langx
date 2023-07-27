a666a3ef58b79edd5b609b2e1579c2fdad776f8d
bhc <behicsakar@gmail.com>
2023-07-27 23:43:04 +0300

    user-list component added and implemented

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { UserListComponent } from 'src/app/components/user-list/user-list.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, UserListComponent]
 })
 export class HomePageModule {}
