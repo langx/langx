0ca7604f954122864ac70fcb8560e9ed0a7b6b98
bhc <behicsakar@gmail.com>
2023-07-28 00:40:06 +0300

    @Input @Output @ViewChild completed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ModalController, PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  @ViewChild('new_chat') modal: ModalController;
+  @ViewChild('popover') popover: PopoverController;
+
   segment = "chats";
   open_new_chat = false;
+  //users: Observable<any>;
+  users = [
+    {id:1, name:"Test", photo:"https://i.pravatar.cc/385"},
+    {id:2, name:"Test2", photo:"https://i.pravatar.cc/375"},
+    {id:3, name:"Test3", photo:"https://i.pravatar.cc/365"},
+  ];
 
   constructor() { }
 
@@ -16,7 +26,7 @@ export class HomePage implements OnInit {
   }
   
   logout(){
-    console.log('logout clicked!');
+    this.popover.dismiss();
   }
 
   onSegmentChanged(event: any) {
@@ -31,8 +41,17 @@ export class HomePage implements OnInit {
   onWillDismiss(event: any) {}
 
   cancel(){
+    this.modal.dismiss()
     this.open_new_chat = false;
-    //console.log('cancel clicked!', this.open_new_chat)
+  }
+  // cancel when the escape button pressed
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    this.modal.dismiss()
+    this.open_new_chat = false;
+  }
+
+  startChat(item: any) {
+    console.log(item.id, item.name)
   }
 
 }
