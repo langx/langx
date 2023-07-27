7c97e338f9feaf3501eacb6ea970e6d9d8fcbae9
bhc <behicsakar@gmail.com>
2023-07-28 00:59:44 +0300

    routing added for chatrooms

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: HomePage
   },
   {
-    path: 'chat',
+    path: 'chats/:id',
     loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
   }
 ];
