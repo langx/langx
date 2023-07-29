85ee0b46932ee7c6a097e09cfb5ec03562698e6f
bhc <behicsakar@gmail.com>
2023-07-29 18:37:54 +0300

    login and signup pages created

---
+++ src/app/pages/login/signup/signup.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SignupPage } from './signup.page';
+
+describe('SignupPage', () => {
+  let component: SignupPage;
+  let fixture: ComponentFixture<SignupPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(SignupPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
