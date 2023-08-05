d052e72c2ee5a558a663909c0e31b888d305eecd
bhc <behicsakar@gmail.com>
2023-08-05 22:31:40 +0300

    firebase and signup works

--- src/app/pages/login/signup/signup.module.ts
+++ src/app/pages/login/signup/signup.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +12,7 @@ import { SignupPage } from './signup.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     SignupPageRoutingModule
   ],
