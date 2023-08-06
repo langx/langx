19aacc96f23437ba92eb0fcaf81c5713b6dea4ca
bhc <behicsakar@gmail.com>
2023-08-06 05:16:53 +0300

    api and auth service is ready!

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private firestore: Firestore
+  ) { }
+
+  docRef(path) {
+    return doc(this.firestore, path);
+  }
+
+  setDocument(path, data) {
+    const dataRef = this.docRef(path);
+    return setDoc<any>(dataRef, data); //set()
+  }
+
+  getDocById(path) {
+    const dataRef = this.docRef(path);
+    return getDoc(dataRef);
+  }
+
 }
