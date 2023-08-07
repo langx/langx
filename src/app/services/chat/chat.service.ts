493729286846044347705b5923107259fd447fac
bhc <behicsakar@gmail.com>
2023-08-07 17:02:03 +0300

    createChatRoom in chat.service

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -10,6 +10,7 @@ export class ChatService {
 
   currentUserId: string;
   public users: Observable<any>;
+  public chatRooms: Observable<any>;
 
   constructor(
     public auth: AuthService,
@@ -27,5 +28,44 @@ export class ChatService {
       'users',
       this.api.whereQuery('uid', '!=', this.currentUserId)
     )
+  }
+
+  async createChatRoom(user_id) {
+    try {
+      // check for existing chatroom
+      let room: any;
+      const querySnapshot = await this.api.getDocs(
+        'chatRooms',
+        this.api.whereQuery(
+          'members',
+          'in',
+          [[user_id, this.currentUserId], [this.currentUserId, user_id]]
+        )
+      );
+
+      room = await querySnapshot.docs.map((doc: any) => {
+        let item = doc.data();
+        item.id = doc.id;
+        return item;
+      });
+      console.log('exist docs: ', room);
+      if (room?.length > 0) return room[0];
+
+      // if no existing room, create new one
+      const data = {
+        members: [
+          this.currentUserId,
+          user_id
+        ],
+        type: 'private',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+      room = await this.api.addDocument('chatRooms', data);
+      return room;
+
+    } catch(e) {
+      throw(e);
+    }
   }
 }
