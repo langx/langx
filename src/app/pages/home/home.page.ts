951b120d488ec0e5c7eee74d689a996d8187e3a3
bhc <behicsakar@gmail.com>
2023-08-07 17:02:25 +0300

    startChat() in home.page.ts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { ModalController, PopoverController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { ChatService } from 'src/app/services/chat/chat.service';
@@ -13,6 +13,9 @@ export class HomePage implements OnInit {
 
   @ViewChild('new_chat') modal: ModalController;
   @ViewChild('popover') popover: PopoverController;
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    this.cancel();
+  }
 
   segment = "community";
   open_new_chat = false;
@@ -57,7 +60,6 @@ export class HomePage implements OnInit {
   newChat(){
     this.open_new_chat = true;
     if(!this.users) this.getUser();
-    //console.log('newChat clicked!', this.open_new_chat)
   }
 
   getUser() {
@@ -68,19 +70,29 @@ export class HomePage implements OnInit {
   onWillDismiss(event: any) {}
 
   cancel(){
-    this.modal.dismiss()
-    this.open_new_chat = false;
-  }
-  // cancel when the escape button pressed
-  // TODO: it could be the upper side of this file
-  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
-    this.modal.dismiss()
+    this.modal.dismiss();
     this.open_new_chat = false;
   }
 
-  startChat(item: any) {
-    console.log('startChat() in home.page.ts', item);
-  }
+  async startChat(item) {
+    try {
+      // show.loader();
+      // create chatroom
+      const room = await this.chatService.createChatRoom(item?.uid);
+      console.log('room: ', room);
+      this.cancel();
+      const navData: NavigationExtras = {
+        queryParams: {
+          name: item?.name
+        }
+      };
+      this.router.navigate(['/', 'home', 'chats', room?.id], navData);
+      // hide.loader();
+    } catch(e) {
+      console.log(e);
+      // hide.loader();
+    }
+  }  
 
   getChat(item) {
     this.router.navigate(['/', 'home', 'chats', item.id])
