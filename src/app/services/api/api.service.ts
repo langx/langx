3f7d67396fa865baa33959177bac119a56d12c8d
bhc <behicsakar@gmail.com>
2023-08-07 13:27:05 +0300

    apiService collectionRef, collectionDataQuery

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, getDoc, query, setDoc, where } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class ApiService {
     return doc(this.firestore, path);
   }
 
+  collectionRef(path) {
+    return collection(this.firestore, path);
+  }
+
   setDocument(path, data) {
     const dataRef = this.docRef(path);
     return setDoc<any>(dataRef, data); //set()
@@ -22,6 +26,20 @@ export class ApiService {
   getDocById(path) {
     const dataRef = this.docRef(path);
     return getDoc(dataRef);
+  }
+
+  collectionDataQuery(path, queryFn?) {
+    let dataRef: any = this.collectionRef(path);
+    if(queryFn) {
+      const q = query(dataRef, queryFn);
+      dataRef = q;
+    }
+    const collection_data = collectionData<any>(dataRef);
+    return collection_data;
+  }
+
+  whereQuery(fieldPath, condition, value) {
+     return where(fieldPath, condition, value);
   }
 
 }
