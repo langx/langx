9fa03faab34f7476b06433df93ac799ed61c2569
bhc <behicsakar@gmail.com>
2023-08-07 13:27:56 +0300

    chatService getId() getUsers() from api.service

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
+  currentUserId: string;
+  public users: Observable<any>;
+
   constructor(
-    public auth: AuthService
-  ) { }
+    public auth: AuthService,
+    public api: ApiService
+  ) { 
+    this.getId();
+  }
+
+  getId() {
+    this.currentUserId = this.auth.getId()
+  }
+
+  getUsers() {
+    this.users = this.api.collectionDataQuery(
+      'users',
+      this.api.whereQuery('uid', '!=', this.currentUserId)
+    )
+  }
 }
