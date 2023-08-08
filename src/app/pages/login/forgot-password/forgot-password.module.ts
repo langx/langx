442a42fe04eb7ab33f2878a0aa92ad5913d5923b
bhc <behicsakar@gmail.com>
2023-08-08 13:44:29 +0300

    fromGroup fromControl Validator created-reset pass

--- src/app/pages/login/forgot-password/forgot-password.module.ts
+++ src/app/pages/login/forgot-password/forgot-password.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +12,7 @@ import { ForgotPasswordPage } from './forgot-password.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     ForgotPasswordPageRoutingModule
   ],
