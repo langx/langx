7f9b986f80b2b4eede6080cd3690c4050476c4c5
bhc <behicsakar@gmail.com>
2023-08-08 13:44:29 +0300

    fromGroup fromControl Validator created-reset pass

--- src/app/pages/login/forgot-password/forgot-password.page.ts
+++ src/app/pages/login/forgot-password/forgot-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPage implements OnInit {
 
+  form: FormGroup;
+  isLoading: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      email: new FormControl('',
+      {validators: [Validators.required, Validators.email]}
+      )
+    })
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 
 }
