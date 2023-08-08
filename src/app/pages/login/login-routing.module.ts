83fcd5de283b9c4ebcc0d683e26a613eadec840a
bhc <behicsakar@gmail.com>
2023-08-08 13:36:28 +0300

    forgot-password page added

--- src/app/pages/login/login-routing.module.ts
+++ src/app/pages/login/login-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   }
 ];
 
