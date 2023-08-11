6eba3917bc001a4e47540fd733c8bfc1316222ec
bhc <behicsakar@gmail.com>
2023-08-11 13:45:33 +0300

    3 tabs added sample implemented home.page html hidden

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -6,23 +6,31 @@ import { HomePage } from './home.page';
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'community',
+        pathMatch: 'full',
+      },
+      {
+        path: 'messages',
+        loadChildren: () => import('./messages/messages.module').then(m => m.MessagesPageModule)
+      },
+      {
+        path: 'community',
+        loadChildren: () => import('./community/community.module').then(m => m.CommunityPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+      }
+    ]
   },
+  
   {
     path: 'chats/:id',
     loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
-  },
-  {
-    path: 'community',
-    loadChildren: () => import('./community/community.module').then( m => m.CommunityPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'messages',
-    loadChildren: () => import('./messages/messages.module').then( m => m.MessagesPageModule)
   }
 ];
 
