1fe7580227b0e1b41edc0b5f0016d099a89f8f71
bhc <behicsakar@gmail.com>
2023-08-12 17:37:57 +0300

    first Firebase Functions DEPLOYED as helloWorld()

---
+++ functions/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest((request, response) => {
+  response.send("Hello World from Firebase!");
+});
