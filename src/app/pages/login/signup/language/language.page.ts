4cf64d49eb54ae456f02995e22824bac1d5978ef
bhc <behicsakar@gmail.com>
2023-08-14 17:30:55 +0300

    SEARCH for realtime done Radio group with console.log done

--- src/app/pages/login/signup/language/language.page.ts
+++ src/app/pages/login/signup/language/language.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { languagesData } from '../data';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-language',
@@ -6,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./language.page.scss'],
 })
 export class LanguagePage implements OnInit {
+  
   public progress: number = 0.93;
+  public languages = languagesData;
+  public results = [...this.languages];
+  term: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  search(event){
+    console.log(event.detail.value);
+  }
+
+  languageChange(event){
+    console.log(event.detail.value);
+  }
+
+  handleInput(event) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.languages.filter((d) => {
+      let tmp = d.name.toLowerCase();
+      let tmp2 = tmp.indexOf(query) 
+      return tmp2 > -1;
+    }); 
+  }
 }
