3158673d51cb7ed25345cde336a0ff47d81d17a8
bhc <behicsakar@gmail.com>
2023-08-14 23:33:57 +0300

    step2 studyLang -> step3 passed array

--- src/app/pages/login/signup/language/step2/step2.page.ts
+++ src/app/pages/login/signup/language/step2/step2.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { languagesData } from '../../data';
 import { ActivatedRoute } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-step2',
@@ -8,15 +10,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Step2Page implements OnInit {
 
-  completeData: Object = {};
+  public progress: number = 0.66;
+  isLoading: boolean = false;
+  public languages = languagesData;
+  term: string;
+  
+  motherLanguage: string;
+  studyLanguage: Array<string> = [];
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     const data: any = this.route.snapshot.queryParams;
     console.log('navData coming from step1', data);
+    this.motherLanguage = data.motherLanguage;
+  }
+
+  checkboxChecked(event){
+    if(this.studyLanguage.includes(event.detail.value)){
+      this.studyLanguage = this.studyLanguage.filter(item => item !== event.detail.value);
+      console.log(this.studyLanguage);
+      return;
+    } else {
+      this.studyLanguage.push(event.detail.value);
+      console.log(this.studyLanguage);
+      return;
+    }
+  }
+
+  step2Completed(){
+    this.isLoading = true; //showLoader();
+    const navData: NavigationExtras = {
+      queryParams: {
+        motherLanguage: this.motherLanguage,
+        studyLanguage: this.studyLanguage
+      }
+    };
+    this.isLoading = false; //hideLoader();
+    this.router.navigate(['/','login','signup','language','step3'], navData);
+    console.log('step2 completed');
   }
 
 }
