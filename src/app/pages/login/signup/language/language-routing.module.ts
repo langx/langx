210aeffbc067590633f67a1d26beab3115cf3794
bhc <behicsakar@gmail.com>
2023-08-14 19:03:55 +0300

    typo fix

--- src/app/pages/login/signup/language/language-routing.module.ts
+++ src/app/pages/login/signup/language/language-routing.module.ts
@@ -6,20 +6,28 @@ import { LanguagePage } from './language.page';
 const routes: Routes = [
   {
     path: '',
-    component: LanguagePage
+    component: LanguagePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'step1',
+        pathMatch: 'full'
+      },
+      {
+        path: 'step1',
+        loadChildren: () => import('./step1/step1.module').then( m => m.Step1PageModule)
+      },
+      {
+        path: 'step2',
+        loadChildren: () => import('./step2/step2.module').then( m => m.Step2PageModule)
+      },
+      {
+        path: 'step3',
+        loadChildren: () => import('./step3/step3.module').then( m => m.Step3PageModule)
+      }
+    ]
   },
-  {
-    path: 'step1',
-    loadChildren: () => import('./step1/step1.module').then( m => m.Step1PageModule)
-  },
-  {
-    path: 'step2',
-    loadChildren: () => import('./step2/step2.module').then( m => m.Step2PageModule)
-  },
-  {
-    path: 'step3',
-    loadChildren: () => import('./step3/step3.module').then( m => m.Step3PageModule)
-  }
+  
 ];
 
 @NgModule({
