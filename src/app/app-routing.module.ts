8213a04b19d70c8223d94ba05e162f212e742ca7
bhc <behicsakar@gmail.com>
2023-08-14 20:21:05 +0300

    comment deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,10 +17,6 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
-//  {
-//    path: 'signup',
-//    loadChildren: () => import('./pages/login/signup/signup.module').then( m => m.SignupPageModule)
-//  }
 ];
 
 @NgModule({
