8deb93a165c98fc49dc24738ee0a72e289662a7d
bhc <behicsakar@gmail.com>
2023-08-17 01:55:46 +0300

    Pages generated for settings

---
+++ src/app/pages/home/profile/settings/notifications/notifications.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NotificationsPage } from './notifications.page';
+
+describe('NotificationsPage', () => {
+  let component: NotificationsPage;
+  let fixture: ComponentFixture<NotificationsPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(NotificationsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
