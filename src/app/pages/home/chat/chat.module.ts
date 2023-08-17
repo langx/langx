4275b677ff960ead788bd792eaac92b0fb0725cc
bhc <behicsakar@gmail.com>
2023-08-17 14:37:11 +0300

    unused import deleted

--- src/app/pages/home/chat/chat.module.ts
+++ src/app/pages/home/chat/chat.module.ts
@@ -8,7 +8,6 @@ import { ChatPageRoutingModule } from './chat-routing.module';
 
 import { ChatPage } from './chat.page';
 import { ChatBoxComponent } from 'src/app/components/chat-box/chat-box.component';
-import { EmptyScreenComponent } from 'src/app/components/empty-screen/empty-screen.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
