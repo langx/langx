e57afb33d21be172dca0f50d50e58b6066c2affc
bhc <behicsakar@gmail.com>
2023-08-18 17:24:25 +0300

    themeToggle imported as shown as in the exampl doc

--- src/app/pages/home/profile/settings/appearance/appearance.page.ts
+++ src/app/pages/home/profile/settings/appearance/appearance.page.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppearancePage implements OnInit {
 
+  themeToggle = false;
+
   constructor() { }
 
   ngOnInit() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    console.log('prefersDark: ', prefersDark);
+
+    // Initialize the dark theme based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+  }
+
+  // Check/uncheck the toggle and update the theme based on isDark
+  initializeDarkTheme(isDark) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
+
+  // Listen for the toggle check/uncheck to toggle the dark theme
+  toggleChange(ev) {
+    console.log('toggleChange: ', ev.detail.checked);
+    this.toggleDarkTheme(ev.detail.checked);
+  }
+
+  // Add or remove the "dark" class on the document body
+  toggleDarkTheme(shouldAdd) {
+      document.body.classList.toggle('dark', shouldAdd);
   }
 
   modeChange = (event) => {  
