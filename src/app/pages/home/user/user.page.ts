8ea3680c510c5dd609e42558ea6689a13ff5c635
bhc <behicsakar@gmail.com>
2023-08-21 21:44:28 +0300

    user.page done

--- src/app/pages/home/user/user.page.ts
+++ src/app/pages/home/user/user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { getAge, lastSeen } from 'src/app/extras/utils';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -10,7 +11,9 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class UserPage implements OnInit {
 
   userId: string;
-  userData: any;
+  user: any;
+
+  isLoading: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,7 +28,20 @@ export class UserPage implements OnInit {
 
   async getUserData() {
     // getting user data from the database while using getUserData() method in auth.service.ts
-    this.userData = await this.authService.getUserData(this.userId);
-    console.log('userData: ', this.userData);
+    this.user = await this.authService.getUserData(this.userId);
+    console.log('userData: ', this.user);
   }
+
+  lastSeen(d: any) { 
+    if (!d) return null;
+    let a = new Date(d.seconds * 1000)
+    return lastSeen(a);
+   }
+
+  getAge(d: any) {
+    if (!d) return null;
+    let a = new Date(d.seconds * 1000)
+    return getAge(a);
+  }
+
 }
