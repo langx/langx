7f8691a912913d4871829b4eb4a584060368ab2d
bhc <behicsakar@gmail.com>
2023-08-21 20:54:30 +0300

    getting asyncly user date with using auth.service

--- src/app/pages/home/user/user.page.ts
+++ src/app/pages/home/user/user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-user',
@@ -9,15 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 export class UserPage implements OnInit {
 
   userId: string;
+  userData: any;
 
   constructor(
     private route: ActivatedRoute,
+    private authService: AuthService,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     const id: string = this.route.snapshot.paramMap.get('id');
     if(id) this.userId = id;
-    console.log('check userId: ', this.userId);
+    this.getUserData();
   }
 
+  async getUserData() {
+    // getting user data from the database while using getUserData() method in auth.service.ts
+    this.userData = await this.authService.getUserData(this.userId);
+    console.log('userData: ', this.userData);
+  }
 }
