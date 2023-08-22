98ed500222d361424f504c3dce1845eaa2f1fc0d
bhc <behicsakar@gmail.com>
2023-08-22 22:58:33 +0300

    filters.page form markup added

--- src/app/pages/home/community/filters/filters/filters.module.ts
+++ src/app/pages/home/community/filters/filters/filters.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { FiltersPage } from './filters.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    FiltersPageRoutingModule
+    FiltersPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [FiltersPage]
 })
