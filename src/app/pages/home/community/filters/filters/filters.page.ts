91d7ecce61ac36ed8f213016ce96e56a5521ae91
bhc <behicsakar@gmail.com>
2023-08-22 22:32:46 +0300

    community/filters.page generated

---
+++ src/app/pages/home/community/filters/filters/filters.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.page.html',
+  styleUrls: ['./filters.page.scss'],
+})
+export class FiltersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
