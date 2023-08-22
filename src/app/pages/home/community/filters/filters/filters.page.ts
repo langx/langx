eec8a43a0a4cf6d31e0296357d09cab965809d88
bhc <behicsakar@gmail.com>
2023-08-22 22:58:33 +0300

    filters.page form markup added

--- src/app/pages/home/community/filters/filters/filters.page.ts
+++ src/app/pages/home/community/filters/filters/filters.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-filters',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltersPage implements OnInit {
 
+  isLoading: boolean = false;
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      name: new FormControl('', 
+        {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)]}
+      ),
+      email: new FormControl('', 
+        {validators: [Validators.required, Validators.email]}
+      ),
+      password: new FormControl('', 
+        {validators: [Validators.required, Validators.minLength(8)]}
+      ),
+    });
+  }
+
+  onSubmit() { 
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form.value);
   }
 
 }
