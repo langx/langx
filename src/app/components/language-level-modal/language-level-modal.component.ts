08225778c202dee3793273ebf67fdae2d9365b01
bhc <behicsakar@gmail.com>
2023-08-25 13:37:12 +0300

    passing through langname into component as Input()

--- src/app/components/language-level-modal/language-level-modal.component.ts
+++ src/app/components/language-level-modal/language-level-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -8,6 +8,7 @@ import { ModalController } from '@ionic/angular';
 })
 export class LanguageLevelModalComponent  implements OnInit {
 
+  @Input() langName: string;
   name: string;
 
   constructor(
