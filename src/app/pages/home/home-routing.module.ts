2d541efc5fd7e5dccba8b1e1df31aec4c11c4660
bhc <behicsakar@gmail.com>
2023-08-27 20:35:25 +0300

    for test, created, home/infinite page deleted

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -54,10 +54,6 @@ const routes: Routes = [
     path: 'user/:id',
     loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
   },
-  {
-    path: 'infinite',
-    loadChildren: () => import('./infinite/infinite.module').then( m => m.InfinitePageModule)
-  },
 ];
 
 @NgModule({
