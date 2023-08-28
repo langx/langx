9d539abfa2281cac3913d91d753a6f85d0308767
bhc <behicsakar@gmail.com>
2023-08-29 00:22:41 +0300

    added TODO with infiniteScroll disabled if block.

--- src/app/pages/home/community/community.page.ts
+++ src/app/pages/home/community/community.page.ts
@@ -85,6 +85,7 @@ export class CommunityPage implements OnInit {
       this.lastVisible = l || null;
 
     } else {
+      console.log('lastVisible: ', this.lastVisible.get('name'));
       // Use the query for pagination
       const nextDocSnap = await this.chatService.getMoreUsers(this.lastVisible);
       this.users.push(...nextDocSnap.docs.map(doc => doc.data()).filter(user => user.uid !== this.chatService.currentUserId));
@@ -92,7 +93,13 @@ export class CommunityPage implements OnInit {
       // Get the last visible document
       let l = nextDocSnap.docs[nextDocSnap.docs.length-1];
       this.lastVisible = l || null;
+
+      // TODO: check double, go another page and back, test with filters
+      // Because if i disable the infiniteScroll, it will not work anymore until i refresh the page
       infiniteScroll.target.complete();
+      if (!this.lastVisible) {
+        infiniteScroll.target.disabled = true;
+      }
     }
 
   }
