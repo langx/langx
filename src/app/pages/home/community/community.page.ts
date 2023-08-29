0111d1f113bdf5aab1ea62033b2da9d31accd0d8
bhc <behicsakar@gmail.com>
2023-08-29 13:54:21 +0300

    loadMore with filters works but some bugs in next ``` typescript if (!this.lastVisible) {     infiniteScroll.target.disabled = true; } ```` this line of code disables infiniteScroll unnecesseraly

--- src/app/pages/home/community/community.page.ts
+++ src/app/pages/home/community/community.page.ts
@@ -14,6 +14,8 @@ import { FilterService, isFilter } from 'src/app/services/filter/filter.service'
 export class CommunityPage implements OnInit {
 
   filterSubscription: any;
+  filterData: isFilter;
+  queryFn: QueryFieldFilterConstraint = null;
 
   users = [];
   lastVisible: any;
@@ -37,7 +39,6 @@ export class CommunityPage implements OnInit {
   //
 
   checkFilter() {
-
     this.authService.getUserData().then((currentUserData) => {
       if(currentUserData?.isFilterData) {
         this.filterService.setEvent(currentUserData?.filterData);
@@ -49,27 +50,28 @@ export class CommunityPage implements OnInit {
     this.filterSubscription = this.filterService.getEvent()
     .subscribe(
       (filterData: isFilter) => {
-        this.doSomething(filterData);
+        console.log('filter: ', filterData);
+        if (!filterData) {
+          this.filterData = null;
+          this.getUsers();
+        } else {
+          this.filterData = filterData;
+          this.getUsersWithFilter();
+        }
       }
     );
   }
 
-  doSomething(filterData: isFilter) {
-    console.log('filter: ', filterData);
+  getUsersWithFilter() {
 
-    let queryFn: QueryFieldFilterConstraint = null;
-
-    if (!filterData) {
-      this.getUsers();
-      return;
+    if (this.filterData?.isFilterGender) {
+      this.queryFn = this.apiService.whereQuery("gender", "==", this.filterData?.filterGender);
+    } else {
+      this.queryFn = null;
     }
 
-    if (filterData?.isFilterGender) {
-      queryFn = this.apiService.whereQuery("gender", "==", filterData?.filterGender);
-    }
-
-    console.log('queryFn: ', queryFn);
-    this.getUsers(queryFn);
+    console.log('queryFn: ', this.queryFn);
+    this.getUsers(this.queryFn);
   }
 
   //
@@ -77,7 +79,11 @@ export class CommunityPage implements OnInit {
   //
 
   loadMore(event) {
-    this.getMoreUsers(event);
+    if (!this.filterData) {
+      this.getMoreUsers(event);
+    } else {
+      this.getMoreUsers(event, this.queryFn);
+    }
   }
 
   async getUsers(queryFn?) {
@@ -102,8 +108,25 @@ export class CommunityPage implements OnInit {
 
   }
 
-  async getMoreUsers(infiniteScroll?) {
+  async getMoreUsers(infiniteScroll, queryFn?) {
 
+    if(queryFn) {
+      console.log('lastVisible: ', this.lastVisible.get('name'));
+      // Use the query for pagination
+      const nextDocSnap = await this.chatService.getMoreUsersWithFilter(this.lastVisible, queryFn);
+      this.users.push(...nextDocSnap.docs.map(doc => doc.data()).filter(user => user.uid !== this.chatService.currentUserId));
+
+      // Get the last visible document
+      let l = nextDocSnap.docs[nextDocSnap.docs.length-1];
+      this.lastVisible = l || null;
+
+      // TODO: check double, go another page and back, test with filters
+      // Because if i disable the infiniteScroll, it will not work anymore until i refresh the page
+      infiniteScroll.target.complete();
+      if (!this.lastVisible) {
+        infiniteScroll.target.disabled = true;
+      }
+    } else {
       console.log('lastVisible: ', this.lastVisible.get('name'));
       // Use the query for pagination
       const nextDocSnap = await this.chatService.getMoreUsers(this.lastVisible);
@@ -119,7 +142,7 @@ export class CommunityPage implements OnInit {
       if (!this.lastVisible) {
         infiniteScroll.target.disabled = true;
       }
-    
+    }    
   }
 
   //
