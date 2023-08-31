2df67482636d4e38a14abd76e7a3a76c7d63096d
bhc <behicsakar@gmail.com>
2023-08-31 21:35:52 +0300

    getUser method moved from chat.servc to user.servc

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, query, where, getDocs, QuerySnapshot, Query, orderBy } from '@angular/fire/firestore';
 import { getAge } from 'src/app/extras/utils';
+import { ApiService } from '../api/api.service';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(
-    private firestore: Firestore
+    private firestore: Firestore,
+    private api: ApiService
   ) {}
 
   async getUsersWithFilters(gender: string, country: string, languages: string[], minAge: number, maxAge: number): Promise<any[]> {
@@ -43,4 +45,54 @@ export class UserService {
     console.log('users: ', users);
     return users;
   }
+
+    //
+  // Get User Methods
+  //
+
+  async getUsers() {
+    return await this.api.getDocs(
+      "users",
+      this.api.orderByQuery("lastSeen", "desc"),
+      this.api.limitQuery(3)
+    )
+  }
+
+  // async getMoreUsers(lastItem) {
+  //   return await this.api.getDocs(
+  //     "users",
+  //     this.api.orderByQuery("lastSeen", "desc"),
+  //     this.api.startAfterQuery(lastItem),
+  //     this.api.limitQuery(5)
+  //   )
+  // }
+
+  //
+  // Get User With Filter Methods
+  //
+
+  async getUsersWithFilter(queryFn) {
+    return await this.api.getDocs(
+      "users",
+      queryFn,
+      this.api.orderByQuery("lastSeen", "desc"),
+      this.api.limitQuery(3)
+    )
+  }
+
+  // TODO: its still not used in community page
+  // async getMoreUsersWithFilter(lastItem, queryFn) {
+  //   return await this.api.getDocs(
+  //     "users",
+  //     queryFn,
+  //     this.api.orderByQuery("lastSeen", "desc"),
+  //     this.api.startAfterQuery(lastItem),
+  //     this.api.limitQuery(10)
+  //   )
+  // }
+
+  //
+  // Chat Room Methods
+  //
+
 }
