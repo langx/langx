1d1b519eaaf3feb7d5338885271cfb8cd65c0a37
bhc <behicsakar@gmail.com>
2023-09-01 21:39:33 +0300

    test.page deleted

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -57,11 +57,7 @@ const routes: Routes = [
   {
     path: 'user/:id',
     loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
-  },
-  {
-    path: 'test',
-    loadChildren: () => import('./test/test.module').then( m => m.TestPageModule)
-  },
+  }
 ];
 
 @NgModule({
