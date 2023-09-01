91fe59975a26cf6b48da10e735948055d0699477
bhc <behicsakar@gmail.com>
2023-09-01 23:42:30 +0300

    profile/edit page generated

---
+++ src/app/pages/home/profile/edit/edit.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.page.html',
+  styleUrls: ['./edit.page.scss'],
+})
+export class EditPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
