bcbc6f05a8b23021ae82aa79ff26108e8fda720b
bhc <behicsakar@gmail.com>
2023-09-01 20:51:14 +0300

    deleted unused comments

--- src/app/pages/home/community/community.page.ts
+++ src/app/pages/home/community/community.page.ts
@@ -41,16 +41,6 @@ export class CommunityPage implements OnInit {
   //
 
   async checkFilter() {
-    // Getting the filter data from DB
-    /*
-    this.authService.getUserData().then((currentUserData) => {
-      if(currentUserData?.isFilterData) {
-        this.filterService.setEvent(currentUserData?.filterData);
-      }
-    }).catch((error) => {
-      console.log('error: ', error);
-    });
-    */
 
     await this.checkLocalStorage();
 
@@ -65,6 +55,7 @@ export class CommunityPage implements OnInit {
         this.getUsers(filterData);
       }
     );
+
   }
 
   async checkLocalStorage() {
@@ -115,43 +106,6 @@ export class CommunityPage implements OnInit {
     console.log('getUsersWithFilters');
     //this.users = await this.userService.getUsersWithFilters(this.filterData);
   }
-
-  // async getMoreUsers(infiniteScroll, queryFn?) {
-
-  //   if(queryFn) {
-  //     console.log('lastVisible: ', this.lastVisible.get('name'));
-  //     // Use the query for pagination
-  //     const nextDocSnap = await this.chatService.getMoreUsersWithFilter(this.lastVisible, queryFn);
-  //     this.users.push(...nextDocSnap.docs.map(doc => doc.data()).filter(user => user.uid !== this.chatService.currentUserId));
-
-  //     // Get the last visible document
-  //     let l = nextDocSnap.docs[nextDocSnap.docs.length-1];
-  //     this.lastVisible = l || null;
-
-  //     // TODO: check double, go another page and back, test with filters
-  //     // Because if i disable the infiniteScroll, it will not work anymore until i refresh the page
-  //     infiniteScroll.target.complete();
-  //     if (!this.lastVisible) {
-  //       infiniteScroll.target.disabled = true;
-  //     }
-  //   } else {
-  //     console.log('lastVisible: ', this.lastVisible.get('name'));
-  //     // Use the query for pagination
-  //     const nextDocSnap = await this.chatService.getMoreUsers(this.lastVisible);
-  //     this.users.push(...nextDocSnap.docs.map(doc => doc.data()).filter(user => user.uid !== this.chatService.currentUserId));
-
-  //     // Get the last visible document
-  //     let l = nextDocSnap.docs[nextDocSnap.docs.length-1];
-  //     this.lastVisible = l || null;
-
-  //     // TODO: check double, go another page and back, test with filters
-  //     // Because if i disable the infiniteScroll, it will not work anymore until i refresh the page
-  //     infiniteScroll.target.complete();
-  //     if (!this.lastVisible) {
-  //       infiniteScroll.target.disabled = true;
-  //     }
-  //   }    
-  // }
 
   //
   // Start Chat
