4b503cc02c79927f9b91bc23d1e26600750aaba1
bhc <behicsakar@gmail.com>
2023-09-02 16:46:52 +0300

    next page generated and navigated with state <-new

---
+++ src/app/pages/home/profile/edit/languages/new/next/next.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NextPageRoutingModule } from './next-routing.module';
+
+import { NextPage } from './next.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NextPageRoutingModule
+  ],
+  declarations: [NextPage]
+})
+export class NextPageModule {}
---
+++ src/app/pages/home/profile/edit/languages/new/next/next.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-next',
+  templateUrl: './next.page.html',
+  styleUrls: ['./next.page.scss'],
+})
+export class NextPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
