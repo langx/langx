3abe123f1f24b6f419ca4f4d68476ebe900cad24
bhc <behicsakar@gmail.com>
2023-09-02 17:34:08 +0300

    saveDB updateUserStudyLanguagesData implemented

--- src/app/pages/home/profile/edit/languages/new/next/next.page.ts
+++ src/app/pages/home/profile/edit/languages/new/next/next.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-next',
@@ -9,19 +10,32 @@ import { ToastController } from '@ionic/angular';
 })
 export class NextPage implements OnInit {
 
+  currentUser: any;
+
   isLoading: boolean = false;
   selectedLanguage: any;
 
   constructor(
     private router: Router,
-    private toastController: ToastController
+    private toastController: ToastController,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     const selectedLanguage: any = this.router.getCurrentNavigation().extras.state;
     this.selectedLanguage = selectedLanguage;
     console.log('selectedLanguage:' + selectedLanguage.code);
+
+    this.getProfileInfo();
   }
+
+  getProfileInfo() {
+    this.authService.getUserData().then(user => {
+      this.currentUser = user;
+    });
+  }
+
+
 
   radioChecked(event, selectedLanguage) {
     this.selectedLanguage.level = event.detail.value;
@@ -35,7 +49,14 @@ export class NextPage implements OnInit {
       this.presentToast('Please select a level.');
       return;
     }
+
+    this.currentUser.studyLanguages.push(this.selectedLanguage);
+    this.currentUser.languagesArray.push(this.selectedLanguage.code);
     // SAVE TO DB
+
+    this.authService.updateUserStudyLanguagesData(this.currentUser);
+
+    this.presentToast('Language added.');
     this.router.navigate(['/home/profile/edit']);
   }
 
