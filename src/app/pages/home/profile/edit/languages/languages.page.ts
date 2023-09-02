29e1310008d8ccc08e78b872d4fa55c0d4efe65a
bhc <behicsakar@gmail.com>
2023-09-02 14:24:57 +0300

    edit-langs page markup without modal yet

--- src/app/pages/home/profile/edit/languages/languages.page.ts
+++ src/app/pages/home/profile/edit/languages/languages.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-languages',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LanguagesPage implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+  studyLanguages: any;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.getProfileInfo();
   }
 
+  getProfileInfo() {
+    this.authService.getUserData().then(user => {
+      this.currentUser = user;
+      this.studyLanguages = this.currentUser.studyLanguages;
+    });
+  }
 }
