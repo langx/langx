cc737a5b64447a4b28929a6e5f87c74bb479f8de
bhc <behicsakar@gmail.com>
2023-09-02 15:36:44 +0300

    edit/languages/new page generated

---
+++ src/app/pages/home/profile/edit/languages/new/new.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.page.html',
+  styleUrls: ['./new.page.scss'],
+})
+export class NewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
