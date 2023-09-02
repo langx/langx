5ed5eadcdc150daba0b3582f0c2363533aa674c4
bhc <behicsakar@gmail.com>
2023-09-02 15:32:08 +0300

    language modal added implemented with search custm

--- src/app/pages/home/profile/edit/languages/languages.module.ts
+++ src/app/pages/home/profile/edit/languages/languages.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LanguagesPageRoutingModule } from './languages-routing.module';
 
 import { LanguagesPage } from './languages.page';
+import { AppExtrasModule } from 'src/app/app.extras.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LanguagesPageRoutingModule
+    LanguagesPageRoutingModule,
+    AppExtrasModule
   ],
   declarations: [LanguagesPage]
 })
