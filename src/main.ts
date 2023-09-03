129efe4b6762c16a3b6d4443b0eae7b0c33d7913
bhc <behicsakar@gmail.com>
2023-09-03 14:43:33 +0300

    main.ts pwa-elements define

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
+  
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
