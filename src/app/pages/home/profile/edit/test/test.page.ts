fe85246327b88f15716dd9c423f097fcf0fec439
bhc <behicsakar@gmail.com>
2023-09-03 14:59:42 +0300

    Camera Plugin works w/ PWA, -from img -take a pic

--- src/app/pages/home/profile/edit/test/test.page.ts
+++ src/app/pages/home/profile/edit/test/test.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPage implements OnInit {
 
+  image: string = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async takePicture() {
+    try {
+      if(Capacitor.getPlatform() != 'web') await Camera.requestPermissions();
+
+      const image = await Camera.getPhoto({
+        quality: 100,
+        // allowEditing: false,
+        source: CameraSource.Prompt,
+        resultType: CameraResultType.DataUrl
+      }).then((image) => {
+        console.log('image:', image);
+        this.image = image.dataUrl;
+      }).catch((error) => {
+        console.log(error);
+      })
+      
+    } catch (e) {
+      console.log(e); 
+    }
+      
   }
 
 }
