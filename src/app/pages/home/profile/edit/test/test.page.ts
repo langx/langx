107e53b2405d4b6041f327d26c1a50ac8e83ef08
bhc <behicsakar@gmail.com>
2023-09-03 17:55:09 +0300

    dataURLtoBlob() added but not used yet

--- src/app/pages/home/profile/edit/test/test.page.ts
+++ src/app/pages/home/profile/edit/test/test.page.ts
@@ -38,4 +38,13 @@ export class TestPage implements OnInit {
       
   }
 
+  dataURLtoBlob(dataurl: any) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
+  }
+
 }
