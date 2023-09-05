dc3cdcc57e9d1ab461e8fca51d41fef13120cefc
bhc <behicsakar@gmail.com>
2023-09-05 13:36:58 +0300

    Preview Photo Page generated nd works w/ close btn

--- src/app/pages/home/profile/profile-routing.module.ts
+++ src/app/pages/home/profile/profile-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ProfilePage
+  },
+  {
+    path: 'preview-photo',
+    loadChildren: () => import('./preview-photo/preview-photo.module').then( m => m.PreviewPhotoPageModule)
   }
 ];
 
