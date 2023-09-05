6237c0870a6d3caa1ffdae840093f5f327bed6c7
bhc <behicsakar@gmail.com>
2023-09-05 15:59:05 +0300

    CUSTOM_ELEMENTS_SCHEMA imp same line w/ NgModule

--- src/app/pages/home/user/user.module.ts
+++ src/app/pages/home/user/user.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { UserPage } from './user.page';
     IonicModule,
     UserPageRoutingModule
   ],
-  declarations: [UserPage]
+  declarations: [UserPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UserPageModule {}
