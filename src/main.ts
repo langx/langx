aee1dadd111ddd292eb391cd34570bd200a99d98
bhc <behicsakar@gmail.com>
2023-09-06 13:14:55 +0300

    hammerjs imported

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs';
 
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
