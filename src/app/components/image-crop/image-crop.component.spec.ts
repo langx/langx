bc5c1ce50daff66aff60f061e4922f3dd42f380c
bhc <behicsakar@gmail.com>
2023-09-06 20:01:49 +0300

    image-crop component generated

---
+++ src/app/components/image-crop/image-crop.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ImageCropComponent } from './image-crop.component';
+
+describe('ImageCropComponent', () => {
+  let component: ImageCropComponent;
+  let fixture: ComponentFixture<ImageCropComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ImageCropComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ImageCropComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
