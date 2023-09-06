57334ae4697ee6a9d81f2bc47eeb95e8cd4ff300
bhc <behicsakar@gmail.com>
2023-09-06 20:01:49 +0300

    image-crop component generated

---
+++ src/app/components/image-crop/image-crop.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-crop',
+  templateUrl: './image-crop.component.html',
+  styleUrls: ['./image-crop.component.scss'],
+})
+export class ImageCropComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
