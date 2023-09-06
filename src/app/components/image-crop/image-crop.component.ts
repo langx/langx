e35a9c5cd8a36941e21e3b8d3c4506b1b76185df
bhc <behicsakar@gmail.com>
2023-09-06 22:25:18 +0300

    unused fn deleted and var default = null

--- src/app/components/image-crop/image-crop.component.ts
+++ src/app/components/image-crop/image-crop.component.ts
@@ -12,8 +12,8 @@ export class ImageCropComponent  implements OnInit {
 
   @Input() image: any;
 
-  imageChangedEvent: any = '';
-  croppedImage: any = '';
+  imageChangedEvent: any = null;
+  croppedImage: any = null;
 
   constructor(
     private modalCtrl: ModalController,
@@ -28,19 +28,15 @@ export class ImageCropComponent  implements OnInit {
   // Image Cropper Test
   //
 
-  fileChangeEvent(event: any): void {
-    this.imageChangedEvent = event;
-  }
   imageCropped(event: ImageCroppedEvent) {
     this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
     // event.blob can be used to upload the cropped image
   }
+
   imageLoaded(image?: LoadedImage) {
       // show cropper
   }
-  cropperReady() {
-      // cropper ready
-  }
+
   loadImageFailed() {
       // show message
   }
