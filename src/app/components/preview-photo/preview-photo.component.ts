19f67dba5b0b700e842c1a5304ee4c5b29aa915b
bhc <behicsakar@gmail.com>
2023-09-07 04:16:24 +0300

    preview-photo container generated

---
+++ src/app/components/preview-photo/preview-photo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-photo',
+  templateUrl: './preview-photo.component.html',
+  styleUrls: ['./preview-photo.component.scss'],
+})
+export class PreviewPhotoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
