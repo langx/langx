d30d00f082cd683b46cba4afd34071c1f1f0ad4d
bhc <behicsakar@gmail.com>
2023-09-13 23:55:08 +0300

    getChatRoomMessages in chat2 service

--- src/app/services/chat/chat2.service.ts
+++ src/app/services/chat/chat2.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Chat2Service {
+  constructor(private api: ApiService) {}
 
-  constructor() { }
+  getChatRoomMessages(chatRoomId: string) {
+    return this.api
+      .collectionDataQuery(
+        `chats/${chatRoomId}/messages`,
+        this.api.orderByQuery('createdAt', 'desc')
+      )
+      .pipe(map((arr: any) => arr.reverse()));
+  }
 }
