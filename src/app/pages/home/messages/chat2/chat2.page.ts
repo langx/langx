062bb12278f6123c5931895a4cddaa13d239b815
bhc <behicsakar@gmail.com>
2023-09-13 18:20:12 +0300

    chat2 scrolltobottom ex.

--- src/app/pages/home/messages/chat2/chat2.page.ts
+++ src/app/pages/home/messages/chat2/chat2.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-chat2',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Chat2Page implements OnInit {
 
+  @ViewChild('chatContainer') chatContainer: ElementRef;
+  @ViewChild('chatMessages') chatMessages: ElementRef;
+  messages: Observable<string[]>;
+
   constructor() { }
 
   ngOnInit() {
+    this.createmessages();
   }
 
+  scrollToBottom() {
+    this.chatContainer.nativeElement.scrollTop = this.chatMessages.nativeElement.scrollHeight;
+  }
+
+  createmessages()  {
+    this.messages = of([
+      'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',
+      'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',    'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',
+      'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',    'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',
+      'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',    'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',
+      'Hello',
+      'How are you?',
+      'I am good, thanks!',
+      'What about you?',
+    ])
+  }
 }
