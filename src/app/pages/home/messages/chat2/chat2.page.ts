246231aa67ccfe2fbbccb0133348da0e7ebcfff7
bhc <behicsakar@gmail.com>
2023-09-13 19:48:31 +0300

    scrollToBottom one fn

--- src/app/pages/home/messages/chat2/chat2.page.ts
+++ src/app/pages/home/messages/chat2/chat2.page.ts
@@ -37,7 +37,7 @@ export class Chat2Page implements OnInit {
     const scrollTop = scrollElement.scrollTop;
     const clientHeight = scrollElement.clientHeight;
     if (scrollTop + clientHeight >= 0.8 * scrollHeight) {
-      this.content.scrollToBottom();
+      this.scrollToBottom();
     }
   }
 
