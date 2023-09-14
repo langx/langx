8b3601799f1c3775041105db6dd68d838e2fb15b
bhc <behicsakar@gmail.com>
2023-09-14 12:29:34 +0300

    typingStatus added to DB

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -46,7 +46,10 @@ export class ChatService {
       // if no existing room, create new one
       const data = {
         members: [this.currentUserId, user_id],
-        type: 'private',
+        typingStatus: {
+          [this.currentUserId]: false,
+          [user_id]: false,
+        },
         createdAt: new Date(),
         updatedAt: new Date(),
       };
