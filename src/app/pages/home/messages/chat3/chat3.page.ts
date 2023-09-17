9082518dc7fdb408f704e340af6d97ca53cde191
bhc <behicsakar@gmail.com>
2023-09-17 22:38:14 +0300

    first createDocument

--- src/app/pages/home/messages/chat3/chat3.page.ts
+++ src/app/pages/home/messages/chat3/chat3.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Database } from '@angular/fire/database';
 import { Chat3Service } from 'src/app/services/chat/chat3.service';
+import { Client, Databases, ID } from 'appwrite';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-chat3',
@@ -15,8 +16,38 @@ export class Chat3Page implements OnInit {
 
   ngOnInit() {}
 
+  connect() {
+    const client = new Client();
+
+    const databases = new Databases(client);
+
+    client
+      .setEndpoint(environment.appwrite.APP_ENDPOINT) // Your API Endpoint
+      .setProject(environment.appwrite.APP_PROJECT); // Your project ID
+
+    const promise = databases.createDocument(
+      environment.appwrite.APP_DATABASE,
+      '65075108a4025a4f5bd7',
+      ID.unique(),
+      {
+        message: 'Hello World',
+        sender: ID.unique(),
+      }
+    );
+
+    promise.then(
+      function (response) {
+        console.log(response); // Success
+      },
+      function (error) {
+        console.log(error); // Failure
+      }
+    );
+  }
+
   sendMessage() {
-    this.chatService.writeChatData('1', 'name', 'email', 'imageUrl');
+    // this.chatService.writeChatData('1', 'name', 'email', 'imageUrl');
+    this.connect();
   }
 
   typingFocus() {
