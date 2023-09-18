b0b8914d54eae53be604431839228501f125e45b
bhc <behicsakar@gmail.com>
2023-09-18 17:46:45 +0300

    Rooms Functions chat3.service

--- src/app/services/chat/chat3.service.ts
+++ src/app/services/chat/chat3.service.ts
@@ -8,6 +8,47 @@ import { environment } from 'src/environments/environment';
 export class Chat3Service {
   constructor(private appwrite: AppwriteService) {}
 
+  //
+  // Rooms
+  //
+
+  listRooms(): Promise<any> {
+    return this.appwrite.listDocuments(environment.appwrite.ROOMS_COLLECTION);
+  }
+
+  getRoom(roomId: string): Promise<any> {
+    return this.appwrite.getDocument(
+      environment.appwrite.ROOMS_COLLECTION,
+      roomId
+    );
+  }
+
+  createRoom(data: any): Promise<any> {
+    return this.appwrite.createDocument(
+      environment.appwrite.ROOMS_COLLECTION,
+      data
+    );
+  }
+
+  listenRooms() {
+    const client = this.appwrite.client$();
+    return client.subscribe('documents', (response) => {
+      if (
+        response.events.includes(
+          'databases.*.collections.' +
+            environment.appwrite.ROOMS_COLLECTION +
+            '.documents.*.update'
+        )
+      ) {
+        console.log(response.payload);
+      }
+    });
+  }
+
+  //
+  // Messages
+  //
+
   listDocuments(): Promise<any> {
     return this.appwrite.listDocuments(
       environment.appwrite.MESSAGES_COLLECTION
@@ -25,7 +66,11 @@ export class Chat3Service {
     const client = this.appwrite.client$();
     return client.subscribe('documents', (response) => {
       if (
-        response.events.includes('databases.*.collections.*.documents.*.update')
+        response.events.includes(
+          'databases.*.collections.' +
+            environment.appwrite.MESSAGES_COLLECTION +
+            '.documents.*.update'
+        )
       ) {
         console.log(response.payload);
       }
