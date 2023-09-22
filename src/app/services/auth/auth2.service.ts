6f3e2a0f1f438d839bb1b1fa76105701040af74b
bhc <behicsakar@gmail.com>
2023-09-22 19:00:16 +0300

    anonLogin, isLoggedIn, logout in auth2

--- src/app/services/auth/auth2.service.ts
+++ src/app/services/auth/auth2.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { AppwriteService } from '../appwrite/appwrite.service';
+import { Models } from 'appwrite';
+import { BehaviorSubject, concatMap, from, mergeMap, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Auth2Service {
+  private _user = new BehaviorSubject<Models.User<Models.Preferences> | null>(
+    null
+  );
 
-  constructor() { }
+  constructor(private appwrite: AppwriteService) {}
+
+  anonLogin(name: string) {
+    const authReq = this.appwrite.account.createAnonymousSession();
+
+    return from(authReq).pipe(
+      mergeMap(() => this.appwrite.account.updateName(name)),
+      concatMap(() => this.appwrite.account.get()),
+      tap((user) => this._user.next(user))
+    );
+  }
+
+  async isLoggedIn() {
+    try {
+      const user = await this.appwrite.account.get();
+      this._user.next(user);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.appwrite.account.deleteSession('current');
+    } catch (e) {
+      console.log(`${e}`);
+    } finally {
+      // this.router.navigate(['/']);
+      this._user.next(null);
+    }
+  }
 }
