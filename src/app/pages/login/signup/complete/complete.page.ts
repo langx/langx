307cffe9f7935870dc345ba0eaa7603f96f57768
bhc <behicsakar@gmail.com>
2023-09-24 18:13:09 +0300

    deleted unused import

--- src/app/pages/login/signup/complete/complete.page.ts
+++ src/app/pages/login/signup/complete/complete.page.ts
@@ -3,7 +3,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { countryData, birthdateData, genderData } from 'src/app/extras/data';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { getAge } from 'src/app/extras/utils';
 import { Auth2Service } from 'src/app/services/auth/auth2.service';
 
@@ -21,7 +20,6 @@ export class CompletePage implements OnInit {
   constructor(
     private router: Router,
     private alertController: AlertController,
-    private authService: AuthService,
     private auth2Service: Auth2Service
   ) {}
 
