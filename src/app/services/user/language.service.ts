8915e8b110d89d3d67d0562c0f9237d73ba7aee8
bhc <behicsakar@gmail.com>
2023-09-25 13:36:23 +0300

    get, create, update LanguageDoc

--- src/app/services/user/language.service.ts
+++ src/app/services/user/language.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { AppwriteService } from '../appwrite/appwrite.service';
+import { environment } from 'src/environments/environment';
+import { ID } from 'appwrite';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanguageService {
+  constructor(private appwrite: AppwriteService) {}
 
-  constructor() { }
+  getLanguageDoc(uid: string): Promise<any> {
+    return this.appwrite.getDocument(
+      environment.appwrite.LANGUAGES_COLLECTION,
+      uid
+    );
+  }
+
+  createLanguageDoc(data: any): Promise<any> {
+    return this.appwrite.createDocument(
+      environment.appwrite.LANGUAGES_COLLECTION,
+      ID.unique(),
+      data
+    );
+  }
+
+  updateLanguageDoc(uid: string, data: any): Promise<any> {
+    return this.appwrite.updateDocument(
+      environment.appwrite.LANGUAGES_COLLECTION,
+      uid,
+      data
+    );
+  }
 }
