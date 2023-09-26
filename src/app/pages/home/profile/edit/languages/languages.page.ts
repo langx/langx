912a1c5c4242c1f81b9b1798edf795e7d423ab97
bhc <behicsakar@gmail.com>
2023-09-26 14:52:27 +0300

    editLanguages markup

--- src/app/pages/home/profile/edit/languages/languages.page.ts
+++ src/app/pages/home/profile/edit/languages/languages.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { Auth2Service } from 'src/app/services/auth/auth2.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-languages',
@@ -8,34 +9,52 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./languages.page.scss'],
 })
 export class LanguagesPage implements OnInit {
-  currentUser: any;
+  cUserSession: any;
+  cUserDoc: any;
 
   isLoading: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private auth2Service: Auth2Service,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getProfileInfo();
   }
 
   getProfileInfo() {
+    //showLoader();
     this.isLoading = true;
-    this.authService.getUserData().then((user) => {
-      this.currentUser = user;
-      this.isLoading = false;
+
+    this.auth2Service
+      .getUser()
+      .subscribe((cUser) => {
+        if (cUser) {
+          console.log(cUser);
+          this.cUserSession = cUser;
+        }
+      })
+      .unsubscribe();
+    // TODO: Unsubscribe may not be necessary to update the user info
+
+    this.userService.getUserDoc(this.cUserSession.$id).then((user) => {
+      this.cUserDoc = user;
+      console.log(user);
     });
+
+    //hideLoader();
+    this.isLoading = false;
   }
 
-  newLangBtn() {
-    // console.log(this.currentUser.languagesArray);
-    this.router.navigate(['/home/profile/edit/languages/new'], {
-      state: this.currentUser.languagesArray,
-    });
+  getStudyLanguages() {
+    return this.cUserDoc?.languages.filter((lang) => !lang.motherLanguage);
   }
 
   radioChecked(event, selectedLanguage) {
     selectedLanguage.level = event.detail.value;
-    this.currentUser.studyLanguages.forEach((lang) => {
+    this.cUserDoc.studyLanguages.forEach((lang) => {
       if (lang.code === selectedLanguage.code) {
         lang.level = selectedLanguage.level;
       }
@@ -43,7 +62,14 @@ export class LanguagesPage implements OnInit {
   }
 
   save() {
-    this.authService.updateUserStudyLanguagesData(this.currentUser);
+    this.cUserDoc.updateUserStudyLanguagesData(this.cUserDoc);
     this.router.navigate(['/home/profile/edit']);
+  }
+
+  newLangBtn() {
+    // console.log(this.currentUser.languagesArray);
+    this.router.navigate(['/home/profile/edit/languages/new'], {
+      state: this.cUserDoc.languagesArray,
+    });
   }
 }
