98d8e75374e9d0301b2accee200d03d2f04e9064
bhc <behicsakar@gmail.com>
2023-09-29 01:44:47 +0300

    initStorage, createFile

--- src/app/services/storage/storage2.service.ts
+++ src/app/services/storage/storage2.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Storage } from 'appwrite';
+import { AppwriteService } from '../appwrite/appwrite.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Storage2Service {
-  constructor() {}
+  storage: Storage;
+
+  constructor(private appwrite: AppwriteService) {
+    this.initStorage();
+  }
+
+  initStorage(): void {
+    this.storage = new Storage(this.appwrite.client);
+  }
+
+  createFile(bucketId: string, fileId: string, file: any): Promise<any> {
+    return this.storage.createFile(bucketId, fileId, file);
+  }
 }
