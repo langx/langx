40599082843b9a70bc11971e90c46d9139107530
bhc <behicsakar@gmail.com>
2023-09-29 01:34:49 +0300

    USER_BUCKET

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,6 +22,7 @@ export const environment = {
     ROOMS_COLLECTION: '6507510fc71f989d5d1c',
     MESSAGES_COLLECTION: '65075108a4025a4f5bd7',
     LANGUAGES_COLLECTION: '6511599e2bf0bb1b4d2c',
+    USER_BUCKET: '6515f94d20becd47cb40',
   },
   production: false,
 };
