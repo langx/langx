f9ef2f08fde620c8c12610f9db8aefd80c2a1b16
bhc <behicsakar@gmail.com>
2023-09-28 16:34:51 +0300

    tested, todo deleted

--- src/app/pages/home/profile/edit/edit.page.ts
+++ src/app/pages/home/profile/edit/edit.page.ts
@@ -248,8 +248,7 @@ export class EditPage implements OnInit {
     // Show Loading
     this.isLoading = true;
     console.log(language);
-    // TODO: If it length is 2, then don't let the user to delete last study language.
-    // TODO: Test needed
+    // If it length is 2, then don't let the user to delete last study language.
     if (this.cUserDoc.languages.length <= 2) {
       this.presentToast('At least one study language required.', 'danger');
       this.isLoading = false;
