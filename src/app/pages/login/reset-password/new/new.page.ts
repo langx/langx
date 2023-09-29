e7d03a73d58593775a17a99f5501f1bd374114a1
bhc <behicsakar@gmail.com>
2023-09-29 17:09:47 +0300

    reset-password/net page generated

---
+++ src/app/pages/login/reset-password/new/new.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.page.html',
+  styleUrls: ['./new.page.scss'],
+})
+export class NewPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
