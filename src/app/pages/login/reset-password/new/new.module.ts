de7a7499f3cb0f15f2f5d5492d406a9679aaefb4
bhc <behicsakar@gmail.com>
2023-09-29 17:54:33 +0300

    ReactiveFormsModule

--- src/app/pages/login/reset-password/new/new.module.ts
+++ src/app/pages/login/reset-password/new/new.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,7 +9,13 @@ import { NewPageRoutingModule } from './new-routing.module';
 import { NewPage } from './new.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, NewPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    NewPageRoutingModule,
+  ],
   declarations: [NewPage],
 })
 export class NewPageModule {}
