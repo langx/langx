929da1045edd8ab29fe41b752fdb35161f4beb39
bhc <behicsakar@gmail.com>
2023-10-02 09:33:08 +0300

    Use Components for editing language level Fixes #157

---
+++ src/app/components/edit-language/edit-language/edit-language.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-language',
+  templateUrl: './edit-language.component.html',
+  styleUrls: ['./edit-language.component.scss'],
+})
+export class EditLanguageComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
