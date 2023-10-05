236305b7b36f5a87db452c7f9d643be3efa13272
bhc <behicsakar@gmail.com>
2023-10-05 13:58:54 +0300

    get, create, updateMembership, methods added

--- src/app/services/team/team.service.ts
+++ src/app/services/team/team.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { ID } from 'appwrite';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeamService {
-  constructor() {}
+  constructor(private api: ApiService) {}
+
+  getTeams() {
+    return this.api.teams.list();
+  }
+
+  getTeam(teamId: string) {
+    return this.api.teams.get(teamId);
+  }
+
+  createTeam(name: string) {
+    return this.api.teams.create(ID.unique(), name);
+  }
+
+  updateMembership(teamId: string, user: string) {
+    return this.api.teams.updateMembership(teamId, user, []);
+  }
 }
