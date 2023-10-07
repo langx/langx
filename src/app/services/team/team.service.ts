965ef5c4637283a152a8978af7ef984d4cb0ec0e
bhc <behicsakar@gmail.com>
2023-10-07 12:42:06 +0300

    bug issue link added as a comment

--- src/app/services/team/team.service.ts
+++ src/app/services/team/team.service.ts
@@ -17,7 +17,7 @@ export class TeamService {
   }
 
   createTeam(name: string) {
-    return this.api.teams.create(ID.unique(), name);
+    return this.api.teams.create(ID.unique(), name, ['owner']);
   }
 
   // TODO: There is an error when creating a membership
@@ -25,7 +25,16 @@ export class TeamService {
   // Error: AppwriteException: Permissions must be one of: (any, users, user:6512ed1d95e48c227190, user:6512ed1d95e48c227190/unverified, users/unverified)
   // Answer: Yes, you can use a team or an appwrite function
   // https://discord.com/channels/564160730845151244/1158556321801588860
+  // Bug Report: Code 400, URL is required, but createMembership method its optional. #6839 
+  // https://github.com/appwrite/appwrite/issues/6839
   createMembership(teamId: string, userId: string) {
-    return this.api.teams.createMembership(teamId, ['owner'], undefined, userId);
+    return this.api.teams.createMembership(
+      teamId,
+      [],
+      undefined,
+      userId,
+      undefined,
+      'http://localhost'
+    );
   }
 }
