d4d46bb679d25cc34979ffae4c252793a511be9a
bhc <behicsakar@gmail.com>
2023-10-09 16:23:34 +0300

    adding only a member with bodyRaw

--- functions/createMembership/src/main.js
+++ functions/createMembership/src/main.js
@@ -2,10 +2,26 @@ import { Client, Databases, Teams, Users, ID } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
 
+// to TEST, execute with POST request
+// body raw = {"teamId": "6523f37acbd9c82b2c06", "userId":"6512ee4f03b70ce08f5c"}
+// curl -X POST -H "Content-Type: application/json" -d "{"teamId": "6523f37acbd9c82b2c06", "userId":"6512ee4f03b70ce08f5c"}" http://localhost:8080
+export default async ({ req, res, log, error }) => {
+  if (req.method === 'GET') {
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.send('Hello, World!');
+  }
+  
+  // TODO: check if user is owner of team
+  // TODO: check req.user is session user
+
+  let body = JSON.parse(req.bodyRaw);
+  log('teamId:');
+  log(body.teamId);
+  log('userId:');
+  log(body.userId);
+  // The `req` object contains the request data
   const client = new Client()
     .setEndpoint('https://db.languagexchange.net/v1')
     .setProject('650750d21e4a6a589be3')
@@ -17,37 +33,19 @@ export default async ({ req, res, log, error }) => {
   const teams = new Teams(client);
   const users = new Users(client);
 
-  log('req.bodyRaw', req.bodyRaw);
+  const bodyData = await req.bodyRaw;
 
-  async function createTeam() {
+  await createMembership(body.teamId, body.userId);
+
+  const a = await teams.list();
+  log('teams.list');
+  log(a);
+
+  async function createMembership(teamId, userId) {
     let id = ID.unique();
-    let a = await teams
-      .create(id, 'test', ['owner']).then(async (response) => {
-        await teams.createMembership(response.$id, ['owner'], undefined, '6513054829d5d285c22c')
-        await teams.createMembership(response.$id, ['owner'], undefined, '65158134f31d32dd29b8')
-      });
-        /*
-      await Promise.all(
-        userList.map(async (username) => {
-          await teams.createMembership(username + '@languagexchange.net', username, '123456');
-        })
-      );
-      });
-      */
+    await teams.createMembership(teamId, ['owner'], undefined, userId);
   }
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    await createTeam();
-
-    let a = await teams.list()
-    log('teams.list', a);
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
   return res.json({
     motto: 'Build Fast. Scale Big. All in One Place.',
     learn: 'https://appwrite.io/docs',
