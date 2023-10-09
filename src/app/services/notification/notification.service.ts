4cdccb47f1719359c74dfb206163ad28bafaa7d4
bhc <behicsakar@gmail.com>
2023-10-09 22:15:14 +0300

    notification listen fn

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  constructor() {}
+  constructor(private api: ApiService) {}
+
+  listen() {
+    const client = this.api.client$();
+    return client.subscribe(
+      'databases.' + environment.appwrite.APP_DATABASE,
+      (response) => {
+        console.log(response);
+      }
+    );
+  }
 }
