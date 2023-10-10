9f20544267ed6590909d3a1b64bdc69beb831a19
bhc <behicsakar@gmail.com>
2023-10-10 17:07:05 +0300

    channels array

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -10,13 +10,31 @@ export class NotificationService {
 
   listen() {
     console.log('listener started');
+    let channels = [];
+
+    // channel for rooms
+    const c1 =
+      'databases.' +
+      environment.appwrite.APP_DATABASE +
+      '.collections.' +
+      environment.appwrite.ROOMS_COLLECTION +
+      '.documents';
+    // channel for messages
+    const c2 =
+      'databases.' +
+      environment.appwrite.APP_DATABASE +
+      '.collections.' +
+      environment.appwrite.MESSAGES_COLLECTION +
+      '.documents';
+
+    // add channels to array
+    channels.push(c1);
+    channels.push(c2);
+
     const client = this.api.client$();
-    return client.subscribe(
-      'databases.' + environment.appwrite.APP_DATABASE,
-      (response) => {
-        console.log('listener response');
-        console.log(response);
-      }
-    );
+    return client.subscribe(channels, (response) => {
+      console.log('listener response');
+      console.log(response);
+    });
   }
 }
