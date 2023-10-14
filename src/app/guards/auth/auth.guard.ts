59a1a2dd26fe32b8bb135ac05001f1fe4e0db1de
bhc <behicsakar@gmail.com>
2023-10-14 14:24:06 +0300

    authGuard startListener

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 // TODO: 'CanLoad' is deprecated.ts(6385)
 export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private notification: NotificationService
+  ) {}
 
   async canLoad(): Promise<boolean> {
     try {
       const isLoggedIn = await this.authService.isLoggedIn();
       // console.log('GUARD canLoad, user:', user);
       if (isLoggedIn) {
+        this.startListener();
         return true;
       } else {
         this.navigate('/login');
@@ -28,5 +34,10 @@ export class AuthGuard implements CanLoad {
 
   navigate(url) {
     this.router.navigateByUrl(url, { replaceUrl: true });
+  }
+
+  startListener() {
+    this.notification.listen();
+    console.log('listener started');
   }
 }
