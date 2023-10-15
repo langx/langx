d1f9ff840d088cf4c29a93feafcf1b81a2947307
bhc <behicsakar@gmail.com>
2023-10-16 00:49:18 +0300

    logout and login, Notification Service does not start Fixes #194 canLoad->canActivate

--- src/app/guards/login/login.guard.ts
+++ src/app/guards/login/login.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 // TODO: 'CanLoad' is deprecated.ts(6385)
-export class LoginGuard implements CanLoad {
+export class LoginGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  async canLoad(): Promise<boolean> {
+  async canActivate(): Promise<boolean> {
     try {
       const isLoggedIn = await this.authService.isLoggedIn();
       // console.log('GUARD canLoad, user:', user);
