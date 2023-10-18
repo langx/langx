9dc1f1770c025ab7384059dd598a4c1c8f741f20
bhc <behicsakar@gmail.com>
2023-10-18 15:18:25 +0300

    todo #219

--- src/app/pages/home/community/filters/filters.page.ts
+++ src/app/pages/home/community/filters/filters.page.ts
@@ -81,6 +81,7 @@ export class FiltersPage implements OnInit {
     this.router.navigateByUrl('/home/community');
   }
 
+  // TODO: #219 Test .toString() may create another BUG
   setLocalStorage(filterData: FilterData) {
     if (!filterData.languages) filterData.languages = [];
     if (filterData.languages.length > 0) {
