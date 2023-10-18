41e1e30271c2a87d23ccab96f5f4e5f46dbc99e5
bhc <behicsakar@gmail.com>
2023-10-18 12:43:38 +0300

    cap preferences set get remove functions

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from 'appwrite';
 import { ApiService } from '../api/api.service';
 import { Storage as localStorage } from '@ionic/storage-angular';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root',
@@ -29,6 +30,27 @@ export class StorageService {
 
   getFileView(bucketId: string, fileId: string): URL {
     return this.storage.getFileView(bucketId, fileId);
+  }
+
+  //
+  // Capacitor Preferences
+  //
+
+  async setValue(key: string, value: string) {
+    await Preferences.set({
+      key: key,
+      value: value,
+    });
+  }
+
+  async getValue(key: string) {
+    const { value } = await Preferences.get({ key: key });
+
+    console.log(`Hello ${value}!`);
+  }
+
+  async removeValue(key: string) {
+    await Preferences.remove({ key: key });
   }
 
   //
