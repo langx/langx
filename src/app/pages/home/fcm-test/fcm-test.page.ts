5aff7978f04699ad55ddfd13df3aa8c0e24ee447
bhc <behicsakar@gmail.com>
2023-10-19 20:04:36 +0300

    fcm-test page generated

---
+++ src/app/pages/home/fcm-test/fcm-test.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fcm-test',
+  templateUrl: './fcm-test.page.html',
+  styleUrls: ['./fcm-test.page.scss'],
+})
+export class FcmTestPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
