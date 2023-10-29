6830d2884706fb480fe4572e467cd16341f5e850
bhc <behicsakar@gmail.com>
2023-10-29 03:43:50 +0300

    save token db

--- src/app/services/fcm/fcm.service.ts
+++ src/app/services/fcm/fcm.service.ts
@@ -8,12 +8,13 @@ import {
   RegistrationError,
 } from '@capacitor/push-notifications';
 import { Router } from '@angular/router';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FcmService {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private api: ApiService) {}
 
   public initPush() {
     // TODO: #226 Web notification can also be implemented here
@@ -44,8 +45,24 @@ export class FcmService {
 
     // Listeners for registration, errors, and incoming notifications
     PushNotifications.addListener('registration', (token: Token) => {
-      // TODO: Save this token to database!
+      // TODO: It may handle in api.service.ts
       console.log('My token: ' + JSON.stringify(token));
+      this.api.account.getPrefs().then((prefs) => {
+        console.log(prefs);
+        if (prefs['ios'] !== '') {
+          if (prefs['ios'] !== token.value) {
+            prefs['ios'] = token.value;
+            this.api.account
+              .updatePrefs(prefs)
+              .then((res) => {
+                console.log('ios token updated', res);
+              })
+              .catch((err) => {
+                console.log('ios token update error', err);
+              });
+          }
+        }
+      });
     });
 
     PushNotifications.addListener(
