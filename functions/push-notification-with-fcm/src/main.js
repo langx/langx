73aff7d20a23601ba860acee60bf243d3414c580
bhc <behicsakar@gmail.com>
2023-10-29 16:08:12 +0300

    push work fine with msg event

--- functions/push-notification-with-fcm/src/main.js
+++ functions/push-notification-with-fcm/src/main.js
@@ -1,5 +1,7 @@
 import { throwIfMissing, sendPushNotification } from './utils.js';
 
+import { Client, Users } from 'node-appwrite';
+
 // Test Data with 11 Pro Simulator
 // {"deviceToken":"deR53P4J8EfejYGytvEcPA:APA91bGsYbpHZewq6WuYPGrw2HhJvg9imL__2c0YSPFkKXRJSLklzYWlR9VP7-6LXoIKl47wjPn5YTE4BXKGWW3h1eZ9Fw_BS7nKqYnbOgk0i7d2sG31djhISxXgjErbcxqeijbqQjHZ", "message":"Hello World"}
 // event trigger: databases.650750f16cd0c482bb83.collections.65075108a4025a4f5bd7
@@ -14,26 +16,40 @@ export default async ({ req, res, log, error }) => {
     return res.json({ ok: false, error: err.message }, 400);
   }
 
-  log(`Sending message to device: ${req.body.deviceToken}`);
+  // Init SDK
+  const client = new Client()
+    .setEndpoint(process.env.APP_ENDPOINT)
+    .setProject(process.env.APP_PROJECT)
+    .setKey(process.env.API_KEY);
 
-  try {
-    const response = await sendPushNotification({
-      notification: {
-        // title: req.body.message.title,
-        // body: req.body.message.body,
-        title: 'languageXchange',
-        body: req.body.message,
-      },
-      data: {
-        detailsId: '33',
-      },
-      token: req.body.deviceToken,
-    });
-    log(`Successfully sent message: ${response}`);
+  const users = new Users(client);
 
-    return res.json({ ok: true, messageId: response });
-  } catch (e) {
-    error(e);
-    return res.json({ ok: false, error: 'Failed to send the message' }, 500);
+  const prefs = await users.getPrefs(req.body.to);
+
+  log(prefs);
+
+  if (prefs['ios'] !== '') {
+    log(`Sending message to device: ${req.body.to}`);
+
+    try {
+      const response = await sendPushNotification({
+        notification: {
+          title: 'New Message',
+          body: req.body.body,
+        },
+        data: {
+          detailsId: '33',
+        },
+        token: prefs['ios'],
+      });
+      log(`Successfully sent message: ${response}`);
+
+      return res.json({ ok: true, messageId: response });
+    } catch (e) {
+      error(e);
+      return res.json({ ok: false, error: 'Failed to send the message' }, 500);
+    }
+  } else {
+    return res.json({ ok: false, error: 'User not found' }, 404);
   }
 };
