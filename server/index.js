acb2bd4abd6b34d8acab949733adae89244c108a
bhc <behicsakar@gmail.com>
2023-11-02 13:07:08 +0300

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+const env = require("dotenv").config();
+
+const sdk = require("node-appwrite");
+let client = new sdk.Client()
+  .setEndpoint(process.env.APP_ENDPOINT) // Your API Endpoint
+  .setProject(process.env.APP_PROJECT) // Your project ID
+  .setKey(process.env.API_KEY); // Your secret API key
+// .setSelfSigned(); // Use only on dev mode with a self-signed SSL cert
+
+let users = new sdk.Databases(client);
+
+console.log(process.env.APP_ENDPOINT);
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.post("/createRoom", (req, res) => {
+  res.json({ message: "Hello from /createRoom!" });
 });
 
 app.listen(3000, () => {
