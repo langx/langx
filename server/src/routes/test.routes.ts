f42cca0974996c93ecfa84e200f466b69c0ea6b6
bhc <behicsakar@gmail.com>
2023-11-02 16:07:29 +0300

    del unused endpoints

--- server/src/routes/test.routes.ts
+++ server/src/routes/test.routes.ts
@@ -12,18 +12,6 @@ class TutorialRoutes {
   intializeRoutes() {
     // Create a new Tutorial
     this.router.post('/', this.controller.create);
-
-    // Retrieve all Tutorials
-    this.router.get('/', this.controller.findAll);
-
-    // Retrieve a single Tutorial with id
-    this.router.get('/:id', this.controller.findOne);
-
-    // Update a Tutorial with id
-    this.router.put('/:id', this.controller.update);
-
-    // Delete a Tutorial with id
-    this.router.delete('/:id', this.controller.delete);
   }
 }
 
