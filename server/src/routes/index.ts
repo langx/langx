ea0a5de473b079a7e8f5ca1bd26d7bd940cf7905
bhc <behicsakar@gmail.com>
2023-11-02 16:11:53 +0300

    test to message

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Application } from 'express';
 import homeRoutes from './home.routes';
-import testRoutes from "./test.routes";
+import messageRoutes from "./message.routes";
 
 export default class Routes {
   constructor(app: Application) {
     app.use('/api', homeRoutes);
-    app.use("/api/test", testRoutes);
+    app.use("/api/message", messageRoutes);
   }
 }
