a8fc780e9a5ae80fbc19e5ef4735c6f1ade2f650
bhc <behicsakar@gmail.com>
2023-11-02 16:41:07 +0300

    throwIfMissing

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
+import { throwIfMissing } from '../utils/utils';
 
 export default class MessageController {
   async create(req: Request, res: Response) {
     try {
+      throwIfMissing(req.headers, ['user-agent']);
+      throwIfMissing(req.body, ['to']);
+
       res.status(201).json({
         message: 'create OK',
         reqBody: req.body,
