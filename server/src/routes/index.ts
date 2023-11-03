434b1d5335632f39f066f45d9439c733a0ea399a
bhc <behicsakar@gmail.com>
2023-11-03 17:03:07 +0300

    typi

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Application } from 'express';
 import homeRoutes from './home.routes';
-import roomRoutes from "./room.routes";
-import messageRoutes from "./message.routes";
+import roomRoutes from './room.routes';
+import messageRoutes from './message.routes';
 
 export default class Routes {
   constructor(app: Application) {
     app.use('/api', homeRoutes);
-    app.use("/api/room", roomRoutes);
-    app.use("/api/message", messageRoutes);
+    app.use('/api/room', roomRoutes);
+    app.use('/api/message', messageRoutes);
   }
 }
