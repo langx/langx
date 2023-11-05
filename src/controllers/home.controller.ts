677a02449d1f563963d2a8c5ade142a798226138
bhc <behicsakar@gmail.com>
2023-11-05 13:58:51 +0300

    init

---
+++ src/controllers/home.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export function welcome(req: Request, res: Response): Response {
+  return res.status(200).json({ message: 'Welcome to languageXchange API.' });
+}
