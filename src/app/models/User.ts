121581f29db123dd45dc1b8cfacde39fd33a8527
bhc <behicsakar@gmail.com>
2023-11-07 01:20:02 +0300

    Add Language and User models.

---
+++ src/app/models/User.ts
@@ -0,0 +1,16 @@
+import { Models } from 'appwrite';
+import { Language } from './Language';
+
+export type User = Models.Document & {
+  name: string;
+  country: string;
+  countyCode: string;
+  gender: string;
+  birthdate: Date;
+  languageArray: string[];
+  profilePhoto: URL;
+  languages: Language[];
+  otherPhotos: URL[];
+  aboutMe: string;
+  lastSeen: Date;
+};
