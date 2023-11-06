1eb66e4c0613535c7c621bdbac3d14a7f2f11948
bhc <behicsakar@gmail.com>
2023-11-07 02:01:36 +0300

    Add auth store feature module to SignupPage module.

--- src/app/pages/login/signup/signup.module.ts
+++ src/app/pages/login/signup/signup.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { StoreModule } from '@ngrx/store';
 
 import { SignupPageRoutingModule } from './signup-routing.module';
-
 import { SignupPage } from './signup.page';
+import { reducers } from 'src/app/store/reducers';
 
 @NgModule({
   imports: [
@@ -15,6 +15,7 @@ import { SignupPage } from './signup.page';
     ReactiveFormsModule,
     IonicModule,
     SignupPageRoutingModule,
+    StoreModule.forFeature('auth', reducers),
   ],
   declarations: [SignupPage],
 })
