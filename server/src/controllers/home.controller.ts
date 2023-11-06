d3f4b3f10c0e644e8ab43c01ce58cdc7691c67d7
bhc <behicsakar@gmail.com>
2023-11-06 05:49:14 +0300

    Remove /server subtree and files

--- server/src/controllers/home.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-
-export function welcome(req: Request, res: Response): Response {
-  return res.status(200).json({ message: 'Welcome to languageXchange API.' });
-}
