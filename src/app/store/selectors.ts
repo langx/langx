63f31daae371ac5e72cb64ac087eb74eb5f5c16e
bhc <behicsakar@gmail.com>
2023-11-07 19:36:07 +0300

    Add isLoadingSelector to authFeatureSelector in selectors.ts

--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -1,5 +1,11 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import { AuthStateInterface } from '../models/types/states/authState.interface';
 
-export const authFeatureSelector = createFeatureSelector<AuthStateInterface>('auth');
+export const authFeatureSelector =
+  createFeatureSelector<AuthStateInterface>('auth');
+
+export const isLoadingSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface) => authState.isLoading
+);
