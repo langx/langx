84b88e52eef086c3cce46b167ed08f9e0c01fc34
bhc <behicsakar@gmail.com>
2023-11-07 22:06:10 +0300

    Add registerSuccessAction and registerFailureAction to actions.ts

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -2,8 +2,16 @@ import { createAction, props } from '@ngrx/store';
 
 import { ActionTypes } from './actionTypes';
 import { RegisterRequestInterface } from '../models/types/requests/registerRequest.interface';
+import { Account } from '../models/Account';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
   props<{ request: RegisterRequestInterface }>()
 );
+
+export const registerSuccessAction = createAction(
+  ActionTypes.REGISTER_SUCCESS,
+  props<{ currentUser: Account }>()
+);
+
+export const registerFailureAction = createAction(ActionTypes.REGISTER_FAILURE);
