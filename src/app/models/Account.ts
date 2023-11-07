03a62424ecf5535cbec0acec1509a38be51f69aa
bhc <behicsakar@gmail.com>
2023-11-07 21:58:36 +0300

    Refactor Account model to use User with Preferences

--- src/app/models/Account.ts
+++ src/app/models/Account.ts
@@ -1,14 +1,3 @@
 import { Models } from 'appwrite';
 
-export type Account = Models.Document & {
-  name: string;
-  country: string;
-  countyCode: string;
-  gender: string;
-  birthdate: Date;
-  languageArray?: string[];
-  profilePhoto?: URL;
-  otherPhotos?: URL[];
-  aboutMe?: string;
-  lastSeen?: Date;
-};
+export type Account = Models.User<Models.Preferences>;
