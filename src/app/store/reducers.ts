bb5d758cc477e55b413fd15e570f89351fc4364a
bhc <behicsakar@gmail.com>
2023-11-08 03:03:59 +0300

    Add currentUser and isLoggedIn properties to AuthStateInterface, handle register actions in loadingReducer

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,18 +1,46 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
-import { registerAction } from './actions/register.action';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from './actions/register.action';
 import { AuthStateInterface } from '../models/types/states/authState.interface';
 
 const initialState: AuthStateInterface = {
   isLoading: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationError: null,
 };
 
 const loadingReducer = createReducer(
   initialState,
-  on(registerAction, (state: AuthStateInterface) => ({
-    ...state,
-    isLoading: true,
-  }))
+  on(
+    registerAction,
+    (state): AuthStateInterface => ({
+      ...state,
+      isLoading: true,
+      validationError: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isLoading: false,
+      isLoggedIn: true,
+      currentUser: action.payload,
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isLoading: false,
+      validationError: action.error,
+    })
+  )
 );
 
 export function reducers(state: AuthStateInterface, action: Action) {
