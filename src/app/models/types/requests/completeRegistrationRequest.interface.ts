8f549981ed7d79334ca51070516605e643092e6e
bhc <behicsakar@gmail.com>
2023-11-08 04:41:11 +0300

    Add CompleteRegistrationRequestInterface to handle registration data

---
+++ src/app/models/types/requests/completeRegistrationRequest.interface.ts
@@ -0,0 +1,8 @@
+export interface CompleteRegistrationRequestInterface {
+  birthdate: string;
+  birthdateWithDateFormat: Date;
+  country: string;
+  countryCode: string;
+  gender: string;
+  genderValue: string;
+}
