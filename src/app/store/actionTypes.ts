c6c99ad0fec26739c3aa8c528f95762810097791
bhc <behicsakar@gmail.com>
2023-11-08 04:19:09 +0300

    Add registration and language selection actions to ActionTypes

--- src/app/store/actionTypes.ts
+++ src/app/store/actionTypes.ts
@@ -2,4 +2,10 @@ export enum ActionTypes {
   REGISTER = '[Auth] Register',
   REGISTER_SUCCESS = '[Auth] Register Success',
   REGISTER_FAILURE = '[Auth] Register Failure',
+  COMPLETE_REGISTRATION = '[Auth] Complete Registration',
+  COMPLETE_REGISTRATION_SUCCESS = '[Auth] Complete Registration Success',
+  COMPLETE_REGISTRATION_FAILURE = '[Auth] Complete Registration Failure',
+  LANGUAGE_SELECTION = '[Auth] Language Selection',
+  LANGUAGE_SELECTION_SUCCESS = '[Auth] Language Selection Success',
+  LANGUAGE_SELECTION_FAILURE = '[Auth] Language Selection Failure',
 }
