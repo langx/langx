9fec85c92cc56d5aafe5c989a1f7e99b4ca19b51
bhc <behicsakar@gmail.com>
2023-11-08 03:03:59 +0300

    Add currentUser and isLoggedIn properties to AuthStateInterface, handle register actions in loadingReducer

--- src/app/models/types/states/authState.interface.ts
+++ src/app/models/types/states/authState.interface.ts
@@ -1,3 +1,9 @@
+import { Account } from '../../Account';
+import { ErrorInterface } from '../errors/error.interface';
+
 export interface AuthStateInterface {
   isLoading: boolean;
+  currentUser: Account | null;
+  isLoggedIn: boolean | null;
+  validationError: ErrorInterface | null;
 }
