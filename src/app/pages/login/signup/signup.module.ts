cd2b251c32c8bf156c19515e0f25bf992f40d01a
bhc <behicsakar@gmail.com>
2023-11-09 19:06:45 +0300

    Add auth feature and register effect to app module and signup module, create auth reducer

--- src/app/pages/login/signup/signup.module.ts
+++ src/app/pages/login/signup/signup.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { SignupPageRoutingModule } from './signup-routing.module';
 import { SignupPage } from './signup.page';
-import { reducers } from 'src/app/store/reducers/register.reducer';
-import { RegisterEffect } from 'src/app/store/effects/register.effects';
 
 @NgModule({
   imports: [
@@ -17,8 +13,8 @@ import { RegisterEffect } from 'src/app/store/effects/register.effects';
     ReactiveFormsModule,
     IonicModule,
     SignupPageRoutingModule,
-    StoreModule.forFeature('auth', reducers),
-    EffectsModule.forFeature([RegisterEffect]),
+    // StoreModule.forFeature('auth', reducers),
+    // EffectsModule.forFeature([RegisterEffect]),
   ],
   declarations: [SignupPage],
 })
