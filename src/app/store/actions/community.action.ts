34fe5482086a010fcb2bf73816f7f0a06941d2a3
bhc <behicsakar@gmail.com>
2023-11-10 19:59:54 +0300

    Add community actions for getting users and handling errors

---
+++ src/app/store/actions/community.action.ts
@@ -0,0 +1,18 @@
+import { createAction, props } from '@ngrx/store';
+
+import { ActionTypes } from 'src/app/store/actions/community.actiontypes';
+import { User } from 'src/app/models/User';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+
+// Get Users
+export const getUsersAction = createAction(ActionTypes.GETUSERS);
+
+export const getUsersSuccessAction = createAction(
+  ActionTypes.GETUSERS_SUCCESS,
+  props<{ payload: User[] }>()
+);
+
+export const isLoggedInFailureAction = createAction(
+  ActionTypes.GETUSERS_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
