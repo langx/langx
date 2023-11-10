f57e6b3ff940f76fde10efd4209c8ba7c4002d74
bhc <behicsakar@gmail.com>
2023-11-10 20:45:17 +0300

    Add community store feature module to CommunityPageModule.

--- src/app/pages/home/community/community.module.ts
+++ src/app/pages/home/community/community.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { CommunityPageRoutingModule } from './community-routing.module';
-
 import { CommunityPage } from './community.page';
 import { UserListComponent } from 'src/app/components/user-list/user-list.component';
+import { communityReducers } from 'src/app/store/reducers/community.reducer';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, CommunityPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CommunityPageRoutingModule,
+    StoreModule.forFeature('community', communityReducers),
+    // EffectsModule.forFeature([]),
+  ],
   declarations: [CommunityPage, UserListComponent],
 })
 export class CommunityPageModule {}
