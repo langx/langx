c59e3dca13c3e1370d45f2e6431829317a1e44d5
bhc <behicsakar@gmail.com>
2023-11-11 22:45:21 +0300

    Add room-related actions to community actiontypes

--- src/app/store/actions/community.actiontypes.ts
+++ src/app/store/actions/community.actiontypes.ts
@@ -5,4 +5,10 @@ export enum ActionTypes {
   GETUSERS_WITH_OFFSET = '[Community] Get Users With Offset',
   GETUSERS_WITH_OFFSET_SUCCESS = '[Community] Get Users With Offset Success',
   GETUSERS_WITH_OFFSET_FAILURE = '[Community] Get Users With Offset Failure',
+  GET_ROOM = '[Community] Get Room',
+  GET_ROOM_SUCCESS = '[Community] Get Room Success',
+  GET_ROOM_FAILURE = '[Community] Get Room Failure',
+  CREATE_ROOM = '[Community] Create Room',
+  CREATE_ROOM_SUCCESS = '[Community] Create Room Success',
+  CREATE_ROOM_FAILURE = '[Community] Create Room Failure',
 }
