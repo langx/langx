076a64977b3a72e61a17c3723b9b5cbaba3b8646
bhc <behicsakar@gmail.com>
2023-11-11 18:17:08 +0300

    Add new action types for getting users with offset in Community module

--- src/app/store/actions/community.actiontypes.ts
+++ src/app/store/actions/community.actiontypes.ts
@@ -2,4 +2,7 @@ export enum ActionTypes {
   GETUSERS = '[Community] Get Users',
   GETUSERS_SUCCESS = '[Community] Get Users Success',
   GETUSERS_FAILURE = '[Community] Get Users Failure',
+  GETUSERS_WITH_OFFSET = '[Community] Get Users With Offset',
+  GETUSERS_WITH_OFFSET_SUCCESS = '[Community] Get Users With Offset Success',
+  GETUSERS_WITH_OFFSET_FAILURE = '[Community] Get Users With Offset Failure',
 }
