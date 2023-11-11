82ec18c891c2fc5d08b0e435c75aba48fd0dc375
bhc <behicsakar@gmail.com>
2023-11-12 02:07:13 +0300

    Add room service and get room functionality to community effect

--- src/app/store/effects/community.effect.ts
+++ src/app/store/effects/community.effect.ts
@@ -4,9 +4,15 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { catchError, map, of, switchMap } from 'rxjs';
 
 import { UserService } from 'src/app/services/user/user.service';
+import { RoomService } from 'src/app/services/chat/room.service';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { getUsersResponseInterface } from 'src/app/models/types/responses/getUsersResponse.interface';
+import { getRoomsResponseInterface } from 'src/app/models/types/responses/getRoomsResponse.interface';
 import {
+  createRoomAction,
+  getRoomAction,
+  getRoomFailureAction,
+  getRoomSuccessAction,
   getUsersAction,
   getUsersFailureAction,
   getUsersSuccessAction,
@@ -57,5 +63,33 @@ export class CommunityEffects {
     )
   );
 
-  constructor(private actions$: Actions, private userService: UserService) {}
+  getRoom$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getRoomAction),
+      switchMap(({ currentUserId, userId }) =>
+        this.roomService.getRoom2(currentUserId, userId).pipe(
+          map((payload: getRoomsResponseInterface) => {
+            if (payload.total === 1) {
+              return getRoomSuccessAction({ payload });
+            } else {
+              return createRoomAction({ currentUserId, userId });
+            }
+          }),
+
+          catchError((errorResponse: HttpErrorResponse) => {
+            const error: ErrorInterface = {
+              message: errorResponse.message,
+            };
+            return of(getRoomFailureAction({ error }));
+          })
+        )
+      )
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private userService: UserService,
+    private roomService: RoomService
+  ) {}
 }
