47c64080df30b6488e5dcadfff6bf1632de9a166
bhc <behicsakar@gmail.com>
2023-11-12 14:13:58 +0300

    Add room selectors to store.

---
+++ src/app/store/selectors/room.selector.ts
@@ -0,0 +1,26 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { RoomStateInterface } from 'src/app/models/types/states/roomState.interface';
+
+export const roomFeatureSelector =
+  createFeatureSelector<RoomStateInterface>('room');
+
+export const isLoadingSelector = createSelector(
+  roomFeatureSelector,
+  (roomState: RoomStateInterface) => roomState.isLoading
+);
+
+export const roomsSelector = createSelector(
+  roomFeatureSelector,
+  (roomState: RoomStateInterface) => roomState.rooms
+);
+
+export const totalSelector = createSelector(
+  roomFeatureSelector,
+  (roomState: RoomStateInterface) => roomState.total
+);
+
+export const errorSelector = createSelector(
+  roomFeatureSelector,
+  (roomState: RoomStateInterface) => roomState.error
+);
