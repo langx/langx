7b3c03ad93b3ffab612898bb312d6e42bd3a8392
bhc <behicsakar@gmail.com>
2023-11-12 14:14:11 +0300

    Add roomsSelector to MessagesPage component

--- src/app/pages/home/messages/messages.page.ts
+++ src/app/pages/home/messages/messages.page.ts
@@ -4,12 +4,14 @@ import { Store, select } from '@ngrx/store';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { lastSeen } from 'src/app/extras/utils';
+import { Room } from 'src/app/models/Room';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { RoomService } from 'src/app/services/chat/room.service';
 import { FcmService } from 'src/app/services/fcm/fcm.service';
 import { getRoomsAction } from 'src/app/store/actions/room.action';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import { roomsSelector } from 'src/app/store/selectors/room.selector';
 
 @Component({
   selector: 'app-messages',
@@ -21,6 +23,7 @@ export class MessagesPage implements OnInit {
   isLoading: boolean = false;
 
   currentUser$: Observable<User | null>;
+  rooms$: Observable<Room[] | null>;
 
   model = {
     icon: 'chatbubbles-outline',
@@ -47,6 +50,7 @@ export class MessagesPage implements OnInit {
 
   initValues() {
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.rooms$ = this.store.pipe(select(roomsSelector));
   }
 
   listRooms2() {
