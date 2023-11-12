a9751a32ae9bcc019b5673ba2b911f11d30ed5c2
bhc <behicsakar@gmail.com>
2023-11-12 04:13:11 +0300

    Add room reducer for managing room state

---
+++ src/app/store/reducers/room.reducer.ts
@@ -0,0 +1,48 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import { RoomStateInterface } from 'src/app/models/types/states/roomState.interface';
+import {
+  getRoomsAction,
+  getRoomsFailureAction,
+  getRoomsSuccessAction,
+} from 'src/app/store/actions/room.action';
+
+const initialState: RoomStateInterface = {
+  isLoading: false,
+  total: null,
+  rooms: null,
+  error: null,
+};
+
+const roomReducer = createReducer(
+  initialState,
+  on(
+    getRoomsAction,
+    (state): RoomStateInterface => ({
+      ...state,
+      isLoading: true,
+      error: null,
+    })
+  ),
+  on(
+    getRoomsSuccessAction,
+    (state, action): RoomStateInterface => ({
+      ...state,
+      isLoading: false,
+      total: action.payload?.total,
+      rooms: action.payload?.documents,
+    })
+  ),
+  on(
+    getRoomsFailureAction,
+    (state, action): RoomStateInterface => ({
+      ...state,
+      isLoading: false,
+      error: action.error,
+    })
+  )
+);
+
+export function roomReducers(state: RoomStateInterface, action: Action) {
+  return roomReducer(state, action);
+}
