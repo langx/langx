45498301fb30ce5a5fcb6e3c1fc8cd433117adb1
bhc <behicsakar@gmail.com>
2023-11-12 14:48:04 +0300

    Add error handling and toast notification to MessagesPage

--- src/app/pages/home/messages/messages.page.ts
+++ src/app/pages/home/messages/messages.page.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { lastSeen } from 'src/app/extras/utils';
 import { Room } from 'src/app/models/Room';
 import { User } from 'src/app/models/User';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { RoomService } from 'src/app/services/chat/room.service';
 import { FcmService } from 'src/app/services/fcm/fcm.service';
 import { getRoomsAction } from 'src/app/store/actions/room.action';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
-import { roomsSelector } from 'src/app/store/selectors/room.selector';
+import {
+  errorSelector,
+  isLoadingSelector,
+  roomsSelector,
+  totalSelector,
+} from 'src/app/store/selectors/room.selector';
 
 @Component({
   selector: 'app-messages',
@@ -23,7 +30,9 @@ export class MessagesPage implements OnInit {
   isLoading: boolean = false;
 
   currentUser$: Observable<User | null>;
+  isLoading$: Observable<boolean>;
   rooms$: Observable<Room[] | null>;
+  total$: Observable<number | null> = null;
 
   model = {
     icon: 'chatbubbles-outline',
@@ -36,7 +45,8 @@ export class MessagesPage implements OnInit {
     private router: Router,
     private authService: AuthService,
     private roomService: RoomService,
-    private fcmService: FcmService
+    private fcmService: FcmService,
+    private toastController: ToastController
   ) {}
 
   async ngOnInit() {
@@ -50,7 +60,18 @@ export class MessagesPage implements OnInit {
 
   initValues() {
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.rooms$ = this.store.pipe(select(roomsSelector));
+    this.total$ = this.store.pipe(select(totalSelector));
+
+    // Present Toast if error
+    this.store
+      .pipe(select(errorSelector))
+      .subscribe((error: ErrorInterface) => {
+        if (error) {
+          this.presentToast(error.message, 'danger');
+        }
+      });
   }
 
   listRooms2() {
@@ -93,5 +114,20 @@ export class MessagesPage implements OnInit {
     let time = lastSeen(d);
     if (time === 'online') time = 'just now';
     return time;
+  }
+
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
   }
 }
