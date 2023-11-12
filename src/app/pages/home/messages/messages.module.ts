4057c6c190bee2a83222a9b0b9f72a15170f32f3
bhc <behicsakar@gmail.com>
2023-11-12 14:09:01 +0300

    Add module room state management

--- src/app/pages/home/messages/messages.module.ts
+++ src/app/pages/home/messages/messages.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { MessagesPageRoutingModule } from './messages-routing.module';
-
 import { MessagesPage } from './messages.page';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { roomReducers } from 'src/app/store/reducers/room.reducer';
+import { RoomEffects } from 'src/app/store/effects/room.effect';
 
 @NgModule({
   imports: [
@@ -16,6 +18,8 @@ import { ComponentsModule } from 'src/app/components/components.module';
     IonicModule,
     MessagesPageRoutingModule,
     ComponentsModule,
+    StoreModule.forFeature('room', roomReducers),
+    EffectsModule.forFeature([RoomEffects]),
   ],
   declarations: [MessagesPage],
 })
