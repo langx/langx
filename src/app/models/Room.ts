04596c14e3bc3826120898b5295cead8101138ef
bhc <behicsakar@gmail.com>
2023-11-12 15:38:37 +0300

    Add RoomWithUserData to Room model

--- src/app/models/Room.ts
+++ src/app/models/Room.ts
@@ -1,7 +1,12 @@
 import { Models } from 'appwrite';
 import { Message } from './Message';
+import { User } from './User';
 
 export type Room = Models.Document & {
   users: string[];
   messages: Message[];
+};
+
+export type RoomWithUserData = Room & {
+  userData: User;
 };
