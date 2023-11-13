3b3ec3024ad1006282e8dc1a15cef5e44f423cb9
bhc <behicsakar@gmail.com>
2023-11-13 16:38:40 +0300

    Add message actions to room action types

--- src/app/store/actions/room.actiontypes.ts
+++ src/app/store/actions/room.actiontypes.ts
@@ -5,4 +5,10 @@ export enum ActionTypes {
   GETROOMS_WITH_OFFSET = '[Room] Get Room With Offset',
   GETROOMS_WITH_OFFSET_SUCCESS = '[Room] Get Room With Offset Success',
   GETROOMS_WITH_OFFSET_FAILURE = '[Room] Get Room With Offset Failure',
+  GETMESSAGES = '[Message] Get Messages',
+  GETMESSAGES_SUCCESS = '[Message] Get Messages Success',
+  GETMESSAGES_FAILURE = '[Message] Get Messages Failure',
+  GETMESSAGES_WITH_OFFSET = '[Message] Get Messages With Offset',
+  GETMESSAGES_WITH_OFFSET_SUCCESS = '[Message] Get Messages With Offset Success',
+  GETMESSAGES_WITH_OFFSET_FAILURE = '[Message] Get Messages With Offset Failure',
 }
