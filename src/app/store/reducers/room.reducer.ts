b037781148876d4263bfb897c29460cb9933959f
bhc <behicsakar@gmail.com>
2023-11-13 15:19:09 +0300

    Add getRoomsWithOffsetAction to room reducer

--- src/app/store/reducers/room.reducer.ts
+++ src/app/store/reducers/room.reducer.ts
@@ -5,6 +5,7 @@ import {
   getRoomsAction,
   getRoomsFailureAction,
   getRoomsSuccessAction,
+  getRoomsWithOffsetAction,
 } from 'src/app/store/actions/room.action';
 
 const initialState: RoomStateInterface = {
@@ -18,6 +19,31 @@ const roomReducer = createReducer(
   initialState,
   on(
     getRoomsAction,
+    (state): RoomStateInterface => ({
+      ...state,
+      isLoading: true,
+      error: null,
+    })
+  ),
+  on(
+    getRoomsSuccessAction,
+    (state, action): RoomStateInterface => ({
+      ...state,
+      isLoading: false,
+      total: action.payload?.total,
+      rooms: action.payload?.documents,
+    })
+  ),
+  on(
+    getRoomsFailureAction,
+    (state, action): RoomStateInterface => ({
+      ...state,
+      isLoading: false,
+      error: action.error,
+    })
+  ),
+  on(
+    getRoomsWithOffsetAction,
     (state): RoomStateInterface => ({
       ...state,
       isLoading: true,
