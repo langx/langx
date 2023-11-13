49e021c77a1ad6b27bde9c4f74afe2cbc3b2fd9c
bhc <behicsakar@gmail.com>
2023-11-13 22:57:06 +0300

    Refactor imports in community, home, and messages modules.

--- src/app/pages/home/community/community.module.ts
+++ src/app/pages/home/community/community.module.ts
@@ -2,24 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { CommunityPageRoutingModule } from './community-routing.module';
 import { CommunityPage } from './community.page';
 import { UserListComponent } from 'src/app/components/user-list/user-list.component';
-import { userReducers } from 'src/app/store/reducers/user.reducer';
-import { UserEffects } from 'src/app/store/effects/user.effect';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CommunityPageRoutingModule,
-    StoreModule.forFeature('user', userReducers),
-    EffectsModule.forFeature([UserEffects]),
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, CommunityPageRoutingModule],
   declarations: [CommunityPage, UserListComponent],
 })
 export class CommunityPageModule {}
