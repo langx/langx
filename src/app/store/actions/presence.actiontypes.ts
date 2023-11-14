30a3d19c92d9388c1205978e8eea7fd75d13ee90
bhc <behicsakar@gmail.com>
2023-11-14 16:36:30 +0300

    Add presence action types.

---
+++ src/app/store/actions/presence.actiontypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  UPDATE_PRESENCE = '[Presence] Update Presence',
+  UPDATE_PRESENCE_SUCCESS = '[Presence] Update Presence Success',
+  UPDATE_PRESENCE_FAILURE = '[Presence] Update Presence Failure',
+}
