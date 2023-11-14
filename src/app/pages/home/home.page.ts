b69984f7f41a41928b042ef5a801ad6f2b8c6810
bhc <behicsakar@gmail.com>
2023-11-14 17:49:41 +0300

    Add presence ping and currentUser observable to HomePage component, and remove presence ping from NotificationService

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
-import { combineLatest, map } from 'rxjs';
+import { Observable, combineLatest, map } from 'rxjs';
 
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { isLoadingSelector as isLoadingUser } from 'src/app/store/selectors/user.selector';
 import { isLoadingSelector as isLoadingRoom } from 'src/app/store/selectors/room.selector';
+import { User } from 'src/app/models/User';
+import { updatePresenceAction } from 'src/app/store/actions/presence.action';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +18,9 @@ import { isLoadingSelector as isLoadingRoom } from 'src/app/store/selectors/room
 export class HomePage implements OnInit {
   loadingOverlay: HTMLIonLoadingElement;
   isLoadingOverlayActive = false;
+  refreshIntervalId: any;
+
+  currentUser$: Observable<User>;
 
   constructor(
     private store: Store,
@@ -24,13 +30,17 @@ export class HomePage implements OnInit {
 
   async ngOnInit() {
     this.initValues();
+    this.presencePing();
   }
 
   ngOnDestroy() {
     this.unsubscribeListener();
+    // Kill presence ping
+    clearInterval(this.refreshIntervalId);
   }
 
   initValues() {
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
     // Loading Controller
     combineLatest([
       this.store.pipe(select(isLoadingUser)),
@@ -47,6 +57,33 @@ export class HomePage implements OnInit {
   unsubscribeListener() {
     this.notification.unsubscribe();
     console.log('Notification Service stopped');
+  }
+
+  //
+  // Presence Ping
+  //
+
+  presencePing() {
+    // Update user in user collection lastSeen attribute
+    // with timeout of every 60 seconds
+    // Start with first ping
+    this.refreshIntervalId = setInterval(() => {
+      this.dispatchUpdatePresence();
+    }, 60000);
+    this.dispatchUpdatePresence();
+  }
+
+  dispatchUpdatePresence() {
+    this.currentUser$
+      .subscribe((user) => {
+        this.store.dispatch(
+          updatePresenceAction({
+            currentUserId: user.$id,
+            request: { lastSeen: new Date() },
+          })
+        );
+      })
+      .unsubscribe();
   }
 
   //
