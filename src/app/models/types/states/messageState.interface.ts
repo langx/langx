3f730dbca47be1e36f99b35fafe68bfb598bf59a
bhc <behicsakar@gmail.com>
2023-11-16 20:00:46 +0300

    Refactor message state to include room information

--- src/app/models/types/states/messageState.interface.ts
+++ src/app/models/types/states/messageState.interface.ts
@@ -1,11 +1,10 @@
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { Message } from 'src/app/models/Message';
 import { User } from 'src/app/models/User';
+import { RoomExtendedInterface } from '../roomExtended.interface';
 
 export interface MessageStateInterface {
   isLoading: boolean;
-  messages: Message[] | null;
-  userData: User | null;
-  total: number;
+  room: RoomExtendedInterface | null;
   error: ErrorInterface | null;
 }
