2a7f2adcc96846a7afdcc950012061ec023841aa
bhc <behicsakar@gmail.com>
2023-11-16 08:56:02 +0300

    Reorder getRoomsWithOffset effect to RoomsEffects

--- src/app/store/effects/rooms.effect.ts
+++ src/app/store/effects/rooms.effect.ts
@@ -31,6 +31,49 @@ import {
 
 @Injectable()
 export class RoomsEffects {
+  getRooms$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getRoomsAction),
+      switchMap(({ currentUserId }) =>
+        this.roomService.listRooms(currentUserId).pipe(
+          map((payload: getRoomsResponseInterface) =>
+            getRoomsSuccessAction({ payload })
+          ),
+
+          catchError((errorResponse: HttpErrorResponse) => {
+            const error: ErrorInterface = {
+              message: errorResponse.message,
+            };
+            return of(getRoomsFailureAction({ error }));
+          })
+        )
+      )
+    )
+  );
+
+  getRoomsWithOffset$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getRoomsWithOffsetAction),
+      switchMap(({ currentUserId, offset }) =>
+        this.roomService.listRooms(currentUserId, offset).pipe(
+          map((payload: getRoomsResponseInterface) =>
+            // TODO: #248 Before dispatch getRoomsWithOffsetSuccessAction,
+            // It may checked first all cureent rooms array,
+            // Then order all of them by last message timestamp
+            getRoomsWithOffsetSuccessAction({ payload })
+          ),
+
+          catchError((errorResponse: HttpErrorResponse) => {
+            const error: ErrorInterface = {
+              message: errorResponse.message,
+            };
+            return of(getRoomsWithOffsetFailureAction({ error }));
+          })
+        )
+      )
+    )
+  );
+
   getRoomById$ = createEffect(() =>
     this.actions$.pipe(
       ofType(getRoomByIdAction),
@@ -118,49 +161,6 @@ export class RoomsEffects {
         })
       ),
     { dispatch: false }
-  );
-
-  getRooms$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(getRoomsAction),
-      switchMap(({ currentUserId }) =>
-        this.roomService.listRooms(currentUserId).pipe(
-          map((payload: getRoomsResponseInterface) =>
-            getRoomsSuccessAction({ payload })
-          ),
-
-          catchError((errorResponse: HttpErrorResponse) => {
-            const error: ErrorInterface = {
-              message: errorResponse.message,
-            };
-            return of(getRoomsFailureAction({ error }));
-          })
-        )
-      )
-    )
-  );
-
-  getRoomsWithOffset$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(getRoomsWithOffsetAction),
-      switchMap(({ currentUserId, offset }) =>
-        this.roomService.listRooms(currentUserId, offset).pipe(
-          map((payload: getRoomsResponseInterface) =>
-            // TODO: #248 Before dispatch getRoomsWithOffsetSuccessAction,
-            // It may checked first all cureent rooms array,
-            // Then order all of them by last message timestamp
-            getRoomsWithOffsetSuccessAction({ payload })
-          ),
-
-          catchError((errorResponse: HttpErrorResponse) => {
-            const error: ErrorInterface = {
-              message: errorResponse.message,
-            };
-            return of(getRoomsWithOffsetFailureAction({ error }));
-          })
-        )
-      )
-    )
   );
 
   constructor(
