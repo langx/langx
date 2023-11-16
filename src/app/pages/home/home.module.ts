c866236ccc80b10719ac6f99cefda0ad5c6b2694
bhc <behicsakar@gmail.com>
2023-11-16 16:01:35 +0300

    Update RoomsEffects to RoomEffects

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -11,7 +11,7 @@ import { ComponentsModule } from 'src/app/components/components.module';
 import { userReducers } from 'src/app/store/reducers/user.reducer';
 import { UserEffects } from 'src/app/store/effects/user.effect';
 import { roomsReducers } from 'src/app/store/reducers/rooms.reducer';
-import { RoomsEffects } from 'src/app/store/effects/rooms.effect';
+import { RoomEffects } from 'src/app/store/effects/room.effect';
 
 @NgModule({
   imports: [
@@ -22,7 +22,7 @@ import { RoomsEffects } from 'src/app/store/effects/rooms.effect';
     ComponentsModule,
     StoreModule.forFeature('user', userReducers),
     StoreModule.forFeature('rooms', roomsReducers),
-    EffectsModule.forFeature([UserEffects, RoomsEffects]),
+    EffectsModule.forFeature([UserEffects, RoomEffects]),
   ],
   declarations: [HomePage],
 })
