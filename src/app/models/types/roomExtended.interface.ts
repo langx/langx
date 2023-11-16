815e944464aadc2e1e18014b0cd53ea07e3aef11
bhc <behicsakar@gmail.com>
2023-11-16 08:52:09 +0300

    Update RoomExtendedInterface to include lastMessage field

--- src/app/models/types/roomExtended.interface.ts
+++ src/app/models/types/roomExtended.interface.ts
@@ -4,6 +4,6 @@ import { User } from 'src/app/models/User';
 
 export interface RoomExtendedInterface extends Room {
   messages: Message[];
-  userData: User;
   lastMessage: Message;
+  userData: User;
 }
