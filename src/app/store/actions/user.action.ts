5a327cecaec4da3c10db54132a49d01e3a27083c
bhc <behicsakar@gmail.com>
2023-11-16 09:47:00 +0300

    Add user actions to store

---
+++ src/app/store/actions/user.action.ts
@@ -0,0 +1,23 @@
+import { createAction, props } from '@ngrx/store';
+
+import { ActionTypes } from 'src/app/store/actions/user.actiontypes';
+import { listUsersResponseInterface } from 'src/app/models/types/responses/listUsersResponse.interface';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
+
+// Get Users Actions
+export const getUserAction = createAction(
+  ActionTypes.GET_USER,
+  // TODO: Create interface for this request
+  props<{ filterData: FilterDataInterface }>()
+);
+
+export const getUserSuccessAction = createAction(
+  ActionTypes.GET_USER_SUCCESS,
+  props<{ payload: listUsersResponseInterface }>()
+);
+
+export const getUserFailureAction = createAction(
+  ActionTypes.GET_USER_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
