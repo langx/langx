e06e86283154055e2dc9b635975d527b16e0d540
bhc <behicsakar@gmail.com>
2023-11-18 02:49:34 +0300

    Add profile actions

---
+++ src/app/store/actions/profile.action.ts
@@ -0,0 +1,20 @@
+import { createAction, props } from '@ngrx/store';
+
+import { ActionTypes } from 'src/app/store/actions/types/profile.actiontypes';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+
+// Get Profile Actions
+export const getProfileAction = createAction(
+  ActionTypes.GET_PROFILE,
+  props<{ userId: string }>()
+);
+
+export const getProfileSuccessAction = createAction(
+  ActionTypes.GET_PROFILE_SUCCESS,
+  props<{ payload: any }>()
+);
+
+export const getProfileFailureAction = createAction(
+  ActionTypes.GET_PROFILE_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
