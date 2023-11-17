b172cf09709f3c807c28f69c0b12c1a0d883043d
bhc <behicsakar@gmail.com>
2023-11-18 02:49:15 +0300

    Add profile action types

---
+++ src/app/store/actions/types/profile.actiontypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  GET_PROFILE= '[Profile] Get Profile',
+  GET_PROFILE_SUCCESS = '[Profile] Get Profile Success',
+  GET_PROFILE_FAILURE = '[Profile] Get Profile Failure',
+}
