3950a755f362875437ef946b6daeaa3baf77d5c6
bhc <behicsakar@gmail.com>
2023-11-19 01:57:20 +0300

    Fix user action types formatting

--- src/app/store/actions/types/user.actiontypes.ts
+++ src/app/store/actions/types/user.actiontypes.ts
@@ -1,6 +1,6 @@
 export enum ActionTypes {
-  GET_USER= '[User] Get Profile',
-  GET_USER_SUCCESS= '[User] Get Profile Success',
+  GET_USER = '[User] Get Profile',
+  GET_USER_SUCCESS = '[User] Get Profile Success',
   GET_USER_FAILURE = '[User] Get Profile Failure',
   UPDATE_USER = '[User] Update User',
   UPDATE_USER_SUCCESS = '[User] Update User Success',
