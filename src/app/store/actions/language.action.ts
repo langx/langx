704990ae47cb5606265771fcd2ee2f5f3a519922
bhc <behicsakar@gmail.com>
2023-11-18 20:53:57 +0300

    Add create language actions and update language actions

--- src/app/store/actions/language.action.ts
+++ src/app/store/actions/language.action.ts
@@ -4,8 +4,27 @@ import { ActionTypes } from 'src/app/store/actions/types/language.actiontypes';
 import { Language } from 'src/app/models/Language';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 
+// Create Language Actions
+export const createLanguageAction = createAction(
+  ActionTypes.CREATE_LANGUAGE,
+  // TODO: Add request interface here
+  props<{ request: any }>()
+);
+
+export const createLanguageSuccessAction = createAction(
+  ActionTypes.CREATE_LANGUAGE_SUCCESS,
+  props<{ payload: Language }>()
+);
+
+export const createLanguageFailureAction = createAction(
+  ActionTypes.CREATE_LANGUAGE_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
+
+// Update Language Actions
 export const updateLanguageAction = createAction(
   ActionTypes.UPDATE_LANGUAGE,
+  // TODO: Add request interface here
   props<{ request: { id: string; data: { level: number } } }>()
 );
 
