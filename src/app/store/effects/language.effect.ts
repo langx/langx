684473b0390d0836a116114b155daded604434cb
bhc <behicsakar@gmail.com>
2023-11-18 20:32:52 +0300

    Seperate effects due to action types

---
+++ src/app/store/effects/language.effect.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { createEffect, ofType, Actions } from '@ngrx/effects';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, of, switchMap } from 'rxjs';
+
+import { LanguageService } from 'src/app/services/user/language.service';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+import { Language } from 'src/app/models/Language';
+
+import {
+  updateLanguageAction,
+  updateLanguageFailureAction,
+  updateLanguageSuccessAction,
+} from 'src/app/store/actions/language.action';
+
+@Injectable()
+export class LanguageEffects {
+  updateLanguage$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(updateLanguageAction),
+      switchMap(({ request }) => {
+        return this.languageService
+          .updateLanguageDoc(request.id, request.data)
+          .pipe(
+            map((payload: Language) =>
+              updateLanguageSuccessAction({ payload })
+            ),
+
+            catchError((errorResponse: HttpErrorResponse) => {
+              const error: ErrorInterface = {
+                message: errorResponse.message,
+              };
+              return of(updateLanguageFailureAction({ error }));
+            })
+          );
+      })
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private languageService: LanguageService
+  ) {}
+}
