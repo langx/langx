b6f8b0a8bacd7799cf5b069fa9892990b470adfc
bhc <behicsakar@gmail.com>
2023-11-18 18:28:49 +0300

    Update user action types

--- src/app/store/actions/types/user.actiontypes.ts
+++ src/app/store/actions/types/user.actiontypes.ts
@@ -1,5 +1,8 @@
 export enum ActionTypes {
-  GET_USER = '[User] Get Profile',
-  GET_USER_SUCCESS = '[User] Get Profile Success',
+  GET_USER= '[User] Get Profile',
+  GET_USER_SUCCESS= '[User] Get Profile Success',
   GET_USER_FAILURE = '[User] Get Profile Failure',
+  UPDATE_USER = '[User] Update User',
+  UPDATE_USER_SUCCESS = '[User] Update User Success',
+  UPDATE_USER_FAILURE = '[User] Update User Failure',
 }
