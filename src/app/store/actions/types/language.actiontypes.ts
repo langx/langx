bbfeb457f4aaa85d22ff9778e91358c758d718b2
bhc <behicsakar@gmail.com>
2023-11-18 20:53:57 +0300

    Add create language actions and update language actions

--- src/app/store/actions/types/language.actiontypes.ts
+++ src/app/store/actions/types/language.actiontypes.ts
@@ -1,4 +1,7 @@
 export enum ActionTypes {
+  CREATE_LANGUAGE = '[Language] Create Language',
+  CREATE_LANGUAGE_SUCCESS = '[Language] Create Language Success',
+  CREATE_LANGUAGE_FAILURE = '[Language] Create Language Failure',
   UPDATE_LANGUAGE= '[Language] Update Language',
   UPDATE_LANGUAGE_SUCCESS = '[Language] Update Language Success',
   UPDATE_LANGUAGE_FAILURE = '[Language] Update Language Failure',
