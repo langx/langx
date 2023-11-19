72fc7e2fc1f30246f68fd5eb345d5250dc972092
bhc <behicsakar@gmail.com>
2023-11-19 18:20:43 +0300

    Update chat-box component to display and track message status

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 import { lastSeen } from 'src/app/extras/utils';
 import { Message } from 'src/app/models/Message';
@@ -8,14 +15,40 @@ import { Message } from 'src/app/models/Message';
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.scss'],
 })
-export class ChatBoxComponent implements OnInit {
+export class ChatBoxComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() chat: Message;
   @Input() current_user_id: string;
 
-  constructor() {}
+  private observer: IntersectionObserver;
 
-  ngOnInit() {}
+  msg: Message = null;
 
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.msg = { ...this.chat };
+  }
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => this.handleIntersect(entry));
+    });
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
+
+  handleIntersect(entry) {
+    if (entry.isIntersecting) {
+      if (this.msg.to === this.current_user_id && this.msg.seen === false) {
+        console.log(entry.target, ' seen');
+        // this.msg.seen = true;
+        // Dispatch action to update message seen status
+      }
+    }
+  }
   messageTime(d: any) {
     if (!d) return null;
     let time = lastSeen(d);
