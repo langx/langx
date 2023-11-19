c5e40642520c110a429a7ce9a148af5c7a00482e
bhc <behicsakar@gmail.com>
2023-11-19 21:49:31 +0300

    Refactor code and remove unused functions

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,59 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ID, Query } from 'appwrite';
-import { BehaviorSubject, Observable, from, of, switchMap } from 'rxjs';
+import { Observable, from, of, switchMap } from 'rxjs';
 
-import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
-import { ApiService } from 'src/app/services/api/api.service';
+// Environment and Services Imports
 import { environment } from 'src/environments/environment';
+import { ApiService } from 'src/app/services/api/api.service';
 import { StorageService } from 'src/app/services/storage/storage.service';
+
+// Interface Imports
 import { User } from 'src/app/models/User';
-import { listUsersResponseInterface } from 'src/app/models/types/responses/listUsersResponse.interface';
 import { BucketFile } from 'src/app/models/BucketFile';
+import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
+import { listUsersResponseInterface } from 'src/app/models/types/responses/listUsersResponse.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  //TODO : Add model for user here
-  private userDoc = new BehaviorSubject<any>(null);
-
   constructor(private api: ApiService, private storage: StorageService) {}
-
-  getEvent(): BehaviorSubject<any> {
-    return this.userDoc;
-  }
-
-  setEvent(user: any): void {
-    this.userDoc.next(user);
-  }
-
-  // TODO: It may be moved to notification service.
-  // Listen to user
-  listenUserDoc(userid: string) {
-    const client = this.api.client$();
-    return client.subscribe(
-      'databases.' +
-        environment.appwrite.APP_DATABASE +
-        '.collections.' +
-        environment.appwrite.USERS_COLLECTION +
-        '.documents.' +
-        userid,
-      (response) => {
-        // console.log(response.payload);
-        this.setEvent(response.payload);
-      }
-    );
-  }
 
   getUserDoc(uid: string): Observable<any> {
     return from(
       this.api.getDocument(environment.appwrite.USERS_COLLECTION, uid)
     );
-  }
-
-  // TODO: Delete this function
-  getUserDoc2(uid: string): Promise<any> {
-    return this.api.getDocument(environment.appwrite.USERS_COLLECTION, uid);
   }
 
   createUserDoc(uid: string, data: any): Observable<User> {
@@ -65,15 +34,6 @@ export class UserService {
   updateUserDoc(uid: string, data: any): Observable<User> {
     return from(
       this.api.updateDocument(environment.appwrite.USERS_COLLECTION, uid, data)
-    );
-  }
-
-  // TODO: Delete this function
-  updateUserDoc2(uid: string, data: any): Promise<any> {
-    return this.api.updateDocument(
-      environment.appwrite.USERS_COLLECTION,
-      uid,
-      data
     );
   }
 
