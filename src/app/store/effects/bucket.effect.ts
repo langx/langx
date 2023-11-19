746b4a3df908bc156672e15eb6f2cfe630511006
bhc <behicsakar@gmail.com>
2023-11-19 03:02:34 +0300

    Add uploadOtherPhotosAction to EditPage component

--- src/app/store/effects/bucket.effect.ts
+++ src/app/store/effects/bucket.effect.ts
@@ -7,6 +7,9 @@ import { UserService } from 'src/app/services/user/user.service';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 
 import {
+  uploadOtherPhotosAction,
+  uploadOtherPhotosFailureAction,
+  uploadOtherPhotosSuccessAction,
   uploadProfilePictureAction,
   uploadProfilePictureFailureAction,
   uploadProfilePictureSuccessAction,
@@ -30,6 +33,37 @@ export class BucketEffects {
             return of(uploadProfilePictureFailureAction({ error }));
           })
         );
+      })
+    )
+  );
+
+  uploadOtherPhotos$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(uploadOtherPhotosAction),
+      switchMap(({ request, currentUserId, otherPhotos }) => {
+        return this.userService.uploadFile(request).pipe(
+          map((payload: URL) => {
+            return payload;
+          }),
+          switchMap((payload) => {
+            const updatedOtherPhotos = [...otherPhotos, payload];
+            return this.userService
+              .updateUserDoc(currentUserId, {
+                otherPhotos: updatedOtherPhotos,
+              })
+              .pipe(
+                map((payload) => {
+                  return uploadOtherPhotosSuccessAction({ payload });
+                })
+              );
+          })
+        );
+      }),
+      catchError((errorResponse: HttpErrorResponse) => {
+        const error: ErrorInterface = {
+          message: errorResponse.message,
+        };
+        return of(uploadOtherPhotosFailureAction({ error }));
       })
     )
   );
