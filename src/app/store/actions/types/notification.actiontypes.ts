7d52935264ae073a40bc800e5fb5bd63ed8a10fd
bhc <behicsakar@gmail.com>
2023-11-19 19:09:28 +0300

    Rename notification action types

--- src/app/store/actions/types/notification.actiontypes.ts
+++ src/app/store/actions/types/notification.actiontypes.ts
@@ -1,5 +1,5 @@
 export enum ActionTypes {
   FIND_AND_UPDATE_ROOM_UPDATED_AT = '[Notification] Find And Update Room Updated At',
-  FIND_AND_UPDATE_ROOM_MESSAGE = '[Notification] Find And Update Room Message',
-  FIND_AND_UPDATE_ACTIVE_ROOM_MESSAGE = '[Notification] Find And Update Active Room Message',
+  FIND_ROOM_AND_ADD_MESSAGE = '[Notification] Find Room And Add Message',
+  FIND_ACTIVE_ROOM_AND_ADD_MESSAGE = '[Notification] Find Active Room And Add Message',
 }
