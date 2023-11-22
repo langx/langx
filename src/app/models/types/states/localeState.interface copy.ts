20e213884dc1214f03ba0bf3ad199a3159181ca1
bhc <behicsakar@gmail.com>
2023-11-22 17:52:49 +0300

    Refactor localeState.interface and locale.action.ts with models

--- src/app/models/types/states/localeState.interface copy.ts
+++ src/app/models/types/states/localeState.interface copy.ts
@@ -1,8 +1,10 @@
+import { Countries } from 'src/app/models/locale/Countries';
+import { Languages } from 'src/app/models/locale/Languages';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 
 export interface LocaleStateInterface {
   isLoading: boolean;
-  countries: any[] | null;
-  languages: any[] | null;
+  countries: Countries | null;
+  languages: Languages | null;
   error: ErrorInterface | null;
 }
