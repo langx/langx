3ffb2d88f59f94c208d51d8e36ff7e48f07afb2e
bhc <behicsakar@gmail.com>
2023-11-23 03:00:08 +0300

    Fix birthdate input in signup form. While registering, and chosing birthdate, after complete, it opens modal again #201

--- src/app/pages/login/signup/complete/complete.page.ts
+++ src/app/pages/login/signup/complete/complete.page.ts
@@ -4,13 +4,13 @@ import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
-import { birthdateData } from 'src/app/extras/localeData'; // TODO: Delete it
 
-import { getAge, nameParts } from 'src/app/extras/utils';
+import { getAge, nameParts, dateValidator } from 'src/app/extras/utils';
 import { CompleteRegistrationRequestInterface } from 'src/app/models/types/requests/completeRegistrationRequest.interface';
 import { completeRegistrationAction } from 'src/app/store/actions/auth.action';
 import { Account } from 'src/app/models/Account';
 import { Countries } from 'src/app/models/locale/Countries';
+import { Country } from 'src/app/models/locale/Country';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { countriesSelector } from 'src/app/store/selectors/locale.selector';
 import {
@@ -18,7 +18,6 @@ import {
   isLoadingSelector,
   registerValidationErrorSelector,
 } from 'src/app/store/selectors/auth.selector';
-import { Country } from 'src/app/models/locale/Country';
 
 @Component({
   selector: 'app-complete',
@@ -80,60 +79,25 @@ export class CompletePage implements OnInit, OnDestroy {
     );
 
     // Present Toast if error
-    this.store
-      .pipe(select(registerValidationErrorSelector))
-      .subscribe((error: ErrorInterface) => {
-        if (error) this.presentToast(error.message, 'danger');
-      });
+    this.subscriptions.add(
+      this.store
+        .pipe(select(registerValidationErrorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) this.presentToast(error.message, 'danger');
+        })
+    );
   }
 
   initForm() {
     this.form = new FormGroup({
-      birthdate: new FormControl('', { validators: [Validators.required] }),
-      birthdateWithDateFormat: new FormControl('', {
-        validators: [Validators.required],
+      birthdate: new FormControl('', {
+        validators: [Validators.required, dateValidator],
       }),
       gender: new FormControl('', { validators: [Validators.required] }),
       country: new FormControl('', { validators: [Validators.required] }),
       countryCode: new FormControl('', { validators: [Validators.required] }),
     });
   }
-
-  public birthdatePickerColumns = [
-    {
-      name: 'day',
-      options: birthdateData.day,
-    },
-    {
-      name: 'month',
-      options: birthdateData.month,
-    },
-    {
-      name: 'year',
-      options: birthdateData.year,
-    },
-  ];
-
-  public birthdatePickerButtons = [
-    { text: 'Cancel', role: 'cancel' },
-    {
-      text: 'Confirm',
-      handler: (value) => {
-        let val =
-          value.day.text + '/' + value.month.value + '/' + value.year.text;
-        let newDate = new Date(val);
-        if (getAge(newDate) < 13) {
-          this.presentToast(
-            'You must be at least 13 years old to use this app',
-            'danger'
-          );
-        } else {
-          this.form.controls['birthdate'].setValue(val);
-          this.form.controls['birthdateWithDateFormat'].setValue(newDate);
-        }
-      },
-    },
-  ];
 
   genderChange(event) {
     this.form.controls['gender'].setValue(event.detail.value);
@@ -149,10 +113,23 @@ export class CompletePage implements OnInit, OnDestroy {
 
   async onSubmit() {
     console.log('form.value:', this.form.value);
+
+    // Check form is valid
     if (!this.form.valid) {
       this.presentToast('Please fill all the required fields', 'danger');
       return;
     }
+
+    // Check age is at least 13
+    if (getAge(new Date(this.form.value.birthdate)) < 13) {
+      this.presentToast(
+        'You must be at least 13 years old to use this app',
+        'danger'
+      );
+      return;
+    }
+
+    // Complete registration
     this.complete(this.form);
   }
 
@@ -167,7 +144,7 @@ export class CompletePage implements OnInit, OnDestroy {
         }
         const request: CompleteRegistrationRequestInterface = {
           name: nameParts(account?.name),
-          birthdate: form.value.birthdateWithDateFormat,
+          birthdate: new Date(form.value.birthdate),
           country: form.value.country,
           countryCode: form.value.countryCode,
           gender: form.value.gender,
