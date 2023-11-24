fca309a7cde97254f98f022747834cde22e9e931
bhc <behicsakar@gmail.com>
2023-11-24 03:24:26 +0300

    Add image support to chat messages

--- src/app/models/types/messageExtended.interface.ts
+++ src/app/models/types/messageExtended.interface.ts
@@ -2,9 +2,11 @@ import { Models } from 'appwrite';
 import { Room } from 'src/app/models/Room';
 
 export interface MessageExtendedInterface extends Models.Document {
-  body: string;
-  seen: boolean | false;
+  body?: string;
   sender: string;
+  seen: boolean | false;
   to: string;
+  isImage: boolean;
+  image?: URL;
   roomId: Room;
 }
