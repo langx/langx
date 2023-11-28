a73d90b6babfa387688d64025fc5c2088acc1179
bhc <behicsakar@gmail.com>
2023-11-28 21:51:18 +0300

    Fix audio play button color in chat box component

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -25,17 +25,19 @@ export class ChatBoxComponent implements OnInit, AfterViewInit, OnDestroy {
   private observer: IntersectionObserver;
 
   msg: Message = null;
-  storedFileNames: FileInfo[] = [];
+
   audioRef: HTMLAudioElement;
+  audioUrl: URL;
+  audioId: string = null;
 
   constructor(private store: Store, private el: ElementRef) {}
 
   ngOnInit() {
-    this.msg = { ...this.chat };
+    this.initValues();
   }
 
   ngAfterViewInit() {
-    // This is for the seen action when the message is in view
+    // This is for the seen action when the message is in viewc
     this.observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => this.handleIntersect(entry));
     });
@@ -46,25 +48,59 @@ export class ChatBoxComponent implements OnInit, AfterViewInit, OnDestroy {
     this.observer.disconnect();
   }
 
-  handleIntersect(entry) {
-    if (entry.isIntersecting) {
-      if (this.msg.to === this.current_user_id && this.msg.seen === false) {
-        this.msg.seen = true;
-        // Dispatch action to update message seen status
-        this.store.dispatch(updateMessageSeenAction({ request: this.msg }));
-      }
+  initValues() {
+    this.msg = { ...this.chat };
+
+    // Check if the message is an audio
+    if (this.msg.type === 'audio') {
+      this.audioId = this.msg?.$id + '.mp3';
+      this.audioUrl = this.msg?.audio;
+      this.readFiles(this.msg?.$id);
     }
-  }
-  messageTime(d: any) {
-    if (!d) return null;
-    let time = lastSeen(d);
-    if (time === 'online') time = 'now';
-    return time;
   }
 
   //
   // Utils for audio
   //
+
+  private async readFiles(id: string) {
+    try {
+      const ret = await Filesystem.readFile({
+        path: id + '.mp3',
+        directory: Directory.Data,
+      });
+      console.log('File found');
+    } catch (e) {
+      console.log('File not found, fetching from server');
+    }
+    // const audioFile = await Filesystem.readFile({
+    //   path: id + '.mp31',
+    //   directory: Directory.Data,
+    // });
+    // console.log('Audio file', audioFile);
+  }
+
+  // Write a function that downloads audio file from the server to save Filesystem with writeFile
+  async downloadFile(url: URL) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    console.log('Downloaded blob', blob);
+
+    const fileName = new Date().getTime() + '.mp3';
+
+    await Filesystem.writeFile({
+      path: fileName,
+      data: blob,
+      directory: Directory.Data,
+    });
+    console.log('Download complete');
+    // Read file from the filesystem
+    const ret = await Filesystem.readdir({
+      path: fileName,
+      directory: Directory.Data,
+    });
+    console.log('Read directory', ret);
+  }
 
   async play(fileName: string) {
     const audioFile = await Filesystem.readFile({
@@ -93,15 +129,40 @@ export class ChatBoxComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  async togglePlayStop(fileName: string) {
+  async togglePlayStop() {
     if (this.isPlaying()) {
       this.stop();
     } else {
-      await this.play(fileName);
+      await this.play(this.audioId);
     }
   }
 
   isPlaying(): boolean {
     return this.audioRef ? !this.audioRef.paused : false;
+  }
+
+  //
+  // Utils for seen
+  //
+
+  handleIntersect(entry) {
+    if (entry.isIntersecting) {
+      if (this.msg.to === this.current_user_id && this.msg.seen === false) {
+        this.msg.seen = true;
+        // Dispatch action to update message seen status
+        this.store.dispatch(updateMessageSeenAction({ request: this.msg }));
+      }
+    }
+  }
+
+  //
+  // Utils for time
+  //
+
+  messageTime(d: any) {
+    if (!d) return null;
+    let time = lastSeen(d);
+    if (time === 'online') time = 'now';
+    return time;
   }
 }
