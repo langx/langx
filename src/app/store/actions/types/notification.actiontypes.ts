6297fc54f24888dfb127809aaee0bea0e0a16e57
bhc <behicsakar@gmail.com>
2023-12-02 15:54:36 +0300

    🐛 [BUG] : Deactivate Room updatedAt$ update Fixes #254 New action FIND_AND_UPDATE_ACTIVE_ROOM_UPDATED_AT

--- src/app/store/actions/types/notification.actiontypes.ts
+++ src/app/store/actions/types/notification.actiontypes.ts
@@ -3,6 +3,7 @@ export enum ActionTypes {
   FIND_OR_ADD_ROOM_SUCCESS = '[Notification] Find Or Add Room Success',
   FIND_OR_ADD_ROOM_FAILURE = '[Notification] Find Or Add Room Failure',
   FIND_AND_UPDATE_ROOM_UPDATED_AT = '[Notification] Find And Update Room Updated At',
+  FIND_AND_UPDATE_ACTIVE_ROOM_UPDATED_AT = '[Notification] Find And Update Active Room Updated At',
   FIND_ROOM_AND_ADD_MESSAGE = '[Notification] Find Room And Add Message',
   FIND_ACTIVE_ROOM_AND_ADD_MESSAGE = '[Notification] Find Active Room And Add Message',
   FIND_ROOM_AND_UPDATE_MESSAGE_SEEN = '[Notification] Find Room And Update Message Seen',
