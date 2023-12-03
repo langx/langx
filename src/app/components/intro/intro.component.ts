faf365940c8af792e24e4a553f9a2781a45bf9ac
bhc <behicsakar@gmail.com>
2023-12-03 18:58:01 +0300

    Add ViewChild to intro.component.ts

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
   @Input() onFinish: () => void;
+  @ViewChild('slides', { read: ElementRef }) slides: ElementRef;
 
   constructor() {}
 
   ngOnInit() {}
+
+  nextSlide() {
+    (this.slides.nativeElement as any).swiper.slideNext();
+  }
 
   close() {
     this.onFinish();
