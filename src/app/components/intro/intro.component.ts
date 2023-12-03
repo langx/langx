6b7c80834a88ec3b4b6b6eb7a04b84af5a47dc05
bhc <behicsakar@gmail.com>
2023-12-03 18:03:32 +0300

    Add onFinish callback to IntroComponent

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent  implements OnInit {
+  @Input() onFinish: () => void;
 
   constructor() { }
 
