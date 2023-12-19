37f6a74890723249cd3718f4211429301dab6e1a
bhc <behicsakar@gmail.com>
2023-12-19 06:40:51 -0500

    Add Oauth2Component to ComponentsModule and LoginPage

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { EmptyScreenComponent } from 'src/app/components/empty-screen/empty-screen.component';
+import { Oauth2Component } from 'src/app/components/oauth2/oauth2.component';
 
 @NgModule({
-  declarations: [EmptyScreenComponent],
+  declarations: [EmptyScreenComponent, Oauth2Component],
   imports: [CommonModule, IonicModule],
-  exports: [EmptyScreenComponent],
+  exports: [EmptyScreenComponent, Oauth2Component],
 })
 export class ComponentsModule {}
