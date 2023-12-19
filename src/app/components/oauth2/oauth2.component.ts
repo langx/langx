13da37a8f6ea21dda0b9c2682bfee03fd98c13d7
bhc <behicsakar@gmail.com>
2023-12-19 06:33:00 -0500

    Add OAuth2 component generated

---
+++ src/app/components/oauth2/oauth2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oauth2',
+  templateUrl: './oauth2.component.html',
+  styleUrls: ['./oauth2.component.scss'],
+})
+export class Oauth2Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
