013a8df03582bd2c7c3dc488f0bed3797c34968a
bhc <behicsakar@gmail.com>
2023-12-26 13:57:07 -0500

    Remove unused import statement

--- src/app/components/oauth2/oauth2.component.ts
+++ src/app/components/oauth2/oauth2.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-oauth2',
