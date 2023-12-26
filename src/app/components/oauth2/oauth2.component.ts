0ef5117684128cf077954c6f934621444b748f0d
bhc <behicsakar@gmail.com>
2023-12-26 14:05:50 -0500

    Add platform parameter to sign-in methods

--- src/app/components/oauth2/oauth2.component.ts
+++ src/app/components/oauth2/oauth2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
 
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -10,20 +11,23 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class Oauth2Component implements OnInit {
   accessToken: string;
+  platform: string;
 
   constructor(private api: ApiService, private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.platform = Capacitor.getPlatform();
+  }
 
   signInWithGoogle() {
-    this.authService.signInWithGoogle();
+    this.authService.signInWithGoogle(this.platform);
   }
 
   signInWithFacebook() {
-    this.authService.signInWithFacebook();
+    this.authService.signInWithFacebook(this.platform);
   }
 
   signInWithApple() {
-    this.authService.signInWithApple();
+    this.authService.signInWithApple(this.platform);
   }
 }
