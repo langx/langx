66caf137cb1391166f3aa506bb3af75003d7632c
bhc <behicsakar@gmail.com>
2023-12-27 21:56:04 -0500

    Add CapacitorCookies and CookieService imports and update ngOnInit() method

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// TODO: Test Cookies
+import { CapacitorCookies } from '@capacitor/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-oauth2-callback',
@@ -6,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oauth2-callback.component.scss'],
 })
 export class Oauth2CallbackComponent implements OnInit {
-  constructor() {}
+  constructor(private cookieService: CookieService) {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.setCapacitorCookie();
+    this.getCookies();
+    let a = this.cookieService.getAll()
+    console.log('cookie-service', a);
+  }
+
+  async setCapacitorCookie() {
+    await CapacitorCookies.setCookie({
+      url: 'app.languagexchange.net',
+      key: 'language1',
+      value: 'en2',
+    });
+  }
+
+  getCookies() {
+    console.log(document.cookie);
+  }
 }
