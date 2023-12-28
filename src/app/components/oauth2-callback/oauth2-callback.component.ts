35ad9d763506f71fb46fde120cb2b607d433919f
bhc <behicsakar@gmail.com>
2023-12-28 00:16:43 -0500

    Remove ngx-cookie-service and CapacitorCookies

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-// TODO: Test Cookies
-import { CapacitorCookies } from '@capacitor/core';
-import { CookieService } from 'ngx-cookie-service';
-
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-oauth2-callback',
@@ -14,31 +11,14 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class Oauth2CallbackComponent implements OnInit {
   constructor(
-    private cookieService: CookieService,
     private route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private api: ApiService
   ) {}
 
   async ngOnInit() {
-    await this.setCapacitorCookie();
-    this.getCookies();
-    let a = this.cookieService.getAll();
-    console.log('cookie-service', a);
-
-    const params = this.route.snapshot.queryParams;
-
-    this.authService.getCookies(params);
-  }
-
-  async setCapacitorCookie() {
-    await CapacitorCookies.setCookie({
-      url: 'app.languagexchange.net',
-      key: 'language1',
-      value: 'en2',
+    this.api.account.createJWT().then((res) => {
+      console.log('createJWT', res);
     });
-  }
-
-  getCookies() {
-    console.log(document.cookie);
   }
 }
