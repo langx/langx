bdb1f7be7ed81bb5daee69f0fe8415dd75c1bcbf
bhc <behicsakar@gmail.com>
2023-12-27 22:37:18 -0500

    Add ActivatedRoute and AuthService dependencies

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 // TODO: Test Cookies
 import { CapacitorCookies } from '@capacitor/core';
 import { CookieService } from 'ngx-cookie-service';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-oauth2-callback',
@@ -9,13 +13,21 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./oauth2-callback.component.scss'],
 })
 export class Oauth2CallbackComponent implements OnInit {
-  constructor(private cookieService: CookieService) {}
+  constructor(
+    private cookieService: CookieService,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   async ngOnInit() {
     await this.setCapacitorCookie();
     this.getCookies();
-    let a = this.cookieService.getAll()
+    let a = this.cookieService.getAll();
     console.log('cookie-service', a);
+
+    const params = this.route.snapshot.queryParams;
+
+    this.authService.getCookies(params);
   }
 
   async setCapacitorCookie() {
