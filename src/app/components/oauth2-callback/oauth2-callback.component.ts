eebf60bb69a084074c76ec3cc7ddd0f33c224295
bhc <behicsakar@gmail.com>
2023-12-28 00:34:02 -0500

    Add ApiService dependency and implement session checking in Oauth2CallbackComponent

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -13,7 +14,8 @@ export class Oauth2CallbackComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private api: ApiService
   ) {}
 
   async ngOnInit() {
@@ -23,6 +25,30 @@ export class Oauth2CallbackComponent implements OnInit {
   initValues() {
     // Set Params
     this.params = this.route.snapshot.queryParams;
+    console.log('params: ', this.params);
+
+    if (this.params['jwt']) {
+      console.log('jwt: ', this.params['jwt']);
+      console.log('redirect to mobile app');
+    } else {
+      // Check session
+      this.checkSession()
+        .then((response) => {
+          console.log('session: ', response);
+          const jwt = this.createJWT();
+          console.log('creating new jwt..');
+          console.log('jwt: ', jwt);
+          console.log('redirect to web app');
+        })
+        .catch((error) => {
+          console.log('error: ', error);
+          console.log('There is no user session. Redirect to login page.');
+        });
+    }
+  }
+
+  checkSession() {
+    return this.api.account.getSession('current');
   }
 
   createJWT() {
