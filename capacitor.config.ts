0bae262c8d79c629808b5fb7e051180f52aced33
bhc <behicsakar@gmail.com>
2023-12-29 03:45:06 -0500

    Remove CapacitorCookies from capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -14,9 +14,6 @@ const config: CapacitorConfig = {
     PushNotifications: {
       presentationOptions: ['badge', 'alert', 'sound'],
     },
-    CapacitorCookies: {
-      enabled: true,
-    },
   },
 };
 
