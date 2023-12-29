4e49bcca2a291c863cf056e350f39e933d2f7704
bhc <behicsakar@gmail.com>
2023-12-29 03:56:22 -0500

    Refactor Oauth2CallbackComponent constructor and remove unnecessary code

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -7,14 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./oauth2-callback.component.scss'],
 })
 export class Oauth2CallbackComponent implements OnInit {
-  // TODO: Delete following lines
-  // params: Params;
-  // token: string = null;
-
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   async ngOnInit() {
     await this.initValues();
@@ -36,74 +29,12 @@ export class Oauth2CallbackComponent implements OnInit {
     const cookieValue = JSON.stringify(cookieFallback);
     console.log('cookieValue: ', cookieValue);
 
+    // TODO: Dispatch to somewhere because
     localStorage.setItem('cookieFallback', cookieValue);
-    this.router.navigateByUrl('/home');
 
-    // this.storageService.setValue(
-    //   'cookieFallback',
-    //   JSON.stringify(cookieFallback)
-    // );
-    // await CapacitorCookies.setCookie({
-    //   url: params.get('domain') || null,
-    //   key: params.get('key') || null,
-    //   value: params.get('secret') || null,
-    // });
-
-    // await CapacitorCookies.setCookie({
-    //   url: '192.168.0.104',
-    //   key: '1db.languagexchange.net',
-    //   value: '1db.languagexchange.net',
-    // });
-
-    // console.log('cookies:', document.cookie);
-
-    // await this.api.account.getSession('current').then((response) => {
-    //   console.log('session: ', response);
-    // });
-
-    // TODO: Delete following lines
-    // if (this.params['jwt']) {
-    //   this.token = this.params['jwt'];
-    //   console.log('jwt: ', this.token);
-    //   console.log('redirect to mobile app');
-
-    //   this.navigateWithToken();
-    // } else {
-    //   // Check session
-    //   this.checkSession()
-    //     .then(async (response) => {
-    //       console.log('session: ', response);
-    //       console.log('creating new jwt..');
-    //       this.token = (await this.createJWT()).jwt;
-    //       console.log('jwt: ', this.token);
-    //       console.log('redirect to web app');
-
-    //       this.navigateWithToken();
-    //     })
-    //     .catch((error) => {
-    //       console.log('error: ', error);
-    //       console.log('There is no user session. Redirect to login page.');
-    //     });
-    // }
+    // TODO: There is a bug here, it loads the home page before the cookie is set
+    setTimeout(() => {
+      this.router.navigateByUrl('/');
+    }, 200);
   }
-
-  // checkSession() {
-  //   return this.api.account.getSession('current');
-  // }
-
-  // createJWT() {
-  //   return this.authService.createJWT();
-  // }
-
-  // navigateWithToken() {
-  //   // TODO: If there is no app installed in the device, redirect to web app
-  //   // window.location.href = `tech.newchapter.languagexchange:/login/oauth2/${this.token}`;
-  //   // this.navigateToHome();
-  // }
-
-  // navigateToHome() {
-  //   // this.router.navigateByUrl('/home');
-  //   // TODO: Remove this line
-  //   this.router.navigateByUrl(`/login/oauth2/${this.token}`);
-  // }
 }
