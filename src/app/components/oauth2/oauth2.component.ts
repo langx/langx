60dbe94263b886ba6d759f336078fef6b5076437
bhc <behicsakar@gmail.com>
2023-12-29 03:56:33 -0500

    Refactor OAuth2 authentication in Oauth2Component

--- src/app/components/oauth2/oauth2.component.ts
+++ src/app/components/oauth2/oauth2.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  OAuth2AuthenticateOptions,
-  OAuth2Client,
-} from '@byteowls/capacitor-oauth2';
 
-import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -17,83 +12,16 @@ export class Oauth2Component implements OnInit {
 
   ngOnInit() {}
 
-  oauth2State: string = JSON.stringify({
-    success: environment.url.SUCCESS_OAUTH2,
-    failure: environment.url.FAILURE_OAUTH2,
-  });
-
-  googleOptions: OAuth2AuthenticateOptions = {
-    appId: environment.oauth.google.appID,
-    authorizationBaseUrl: environment.oauth.google.base,
-    redirectUrl: environment.oauth.google.uri,
-    state: this.oauth2State,
-    scope: 'email profile',
-    responseType: 'code',
-    logsEnabled: false,
-    web: {
-      windowTarget: '_self',
-    },
-    android: {},
-    ios: {},
-  };
-
-  facebookOptions: OAuth2AuthenticateOptions = {
-    appId: environment.oauth.facebook.appID,
-    authorizationBaseUrl: environment.oauth.facebook.base,
-    redirectUrl: environment.oauth.facebook.uri,
-    state: this.oauth2State,
-    scope: 'email',
-    responseType: 'code',
-    logsEnabled: true,
-    web: {
-      windowTarget: '_self',
-    },
-    android: {},
-    ios: {},
-  };
-
-  appleOptions: OAuth2AuthenticateOptions = {
-    appId: environment.oauth.apple.appID,
-    authorizationBaseUrl: environment.oauth.apple.base,
-    redirectUrl: environment.oauth.apple.uri,
-    state: this.oauth2State,
-    responseType: 'code',
-    // responseMode: 'form_post',
-    // scope: 'name email',
-    logsEnabled: true,
-    web: {
-      windowTarget: '_self',
-    },
-    android: {},
-    ios: {
-      siwaUseScope: true,
-    },
-  };
-
   signInWithGoogle() {
-    // OAuth2Client.authenticate(this.googleOptions)
-    //   .then((result) => {
-    //     console.log('signInWithGoogle result', result);
-    //   })
-    //   .catch((error) => {
-    //     console.log('signInWithGoogle error', error);
-    //   });
     this.authService.signInWithGoogle();
   }
 
   signInWithFacebook() {
-    // OAuth2Client.authenticate(this.facebookOptions);
     this.authService.signInWithFacebook();
   }
 
   signInWithApple() {
-    // OAuth2Client.authenticate(this.appleOptions)
-    //   .then((result) => {
-    //     console.log('signInWithApple result', result);
-    //   })
-    //   .catch((error) => {
-    //     console.log('signInWithApple error', error);
-    //   });
+    // TODO: It may be better usage
     this.authService.signInWithApple();
   }
 }
