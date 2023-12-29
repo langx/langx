d55decdaffdbac012b2ff922a04ea0809010bac1
bhc <behicsakar@gmail.com>
2023-12-29 03:35:24 -0500

    Refactor OAuth2 callback component

--- src/app/components/oauth2-callback/oauth2-callback.component.ts
+++ src/app/components/oauth2-callback/oauth2-callback.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
-import { ApiService } from 'src/app/services/api/api.service';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-oauth2-callback',
@@ -10,67 +9,104 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./oauth2-callback.component.scss'],
 })
 export class Oauth2CallbackComponent implements OnInit {
-  params: Params;
-  token: string = null;
+  // TODO: Delete following lines
+  // params: Params;
+  // token: string = null;
 
   constructor(
-    private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthService,
-    private api: ApiService
+    private router: Router,
+    private storageService: StorageService
   ) {}
 
   async ngOnInit() {
-    this.initValues();
+    await this.initValues();
   }
 
-  initValues() {
+  async initValues() {
     // Set Params
-    this.params = this.route.snapshot.queryParams;
-    console.log('params: ', this.params);
+    const params = this.route.snapshot.queryParamMap;
+    console.log('params: ', params);
 
-    if (this.params['jwt']) {
-      this.token = this.params['jwt'];
-      console.log('jwt: ', this.token);
-      console.log('redirect to mobile app');
+    const cookieFallback = {};
+    const key = params.get('key');
+    const secret = params.get('secret');
 
-      this.navigateWithToken();
-    } else {
-      // Check session
-      this.checkSession()
-        .then(async (response) => {
-          console.log('session: ', response);
-          console.log('creating new jwt..');
-          this.token = (await this.createJWT()).jwt;
-          console.log('jwt: ', this.token);
-          console.log('redirect to web app');
-
-          this.navigateWithToken();
-        })
-        .catch((error) => {
-          console.log('error: ', error);
-          console.log('There is no user session. Redirect to login page.');
-        });
+    if (key) {
+      cookieFallback[key] = secret;
     }
+
+    const cookieValue = JSON.stringify(cookieFallback);
+    console.log('cookieValue: ', cookieValue);
+
+    localStorage.setItem('cookieFallback', cookieValue);
+    this.router.navigateByUrl('/home');
+
+    // this.storageService.setValue(
+    //   'cookieFallback',
+    //   JSON.stringify(cookieFallback)
+    // );
+    // await CapacitorCookies.setCookie({
+    //   url: params.get('domain') || null,
+    //   key: params.get('key') || null,
+    //   value: params.get('secret') || null,
+    // });
+
+    // await CapacitorCookies.setCookie({
+    //   url: '192.168.0.104',
+    //   key: '1db.languagexchange.net',
+    //   value: '1db.languagexchange.net',
+    // });
+
+    // console.log('cookies:', document.cookie);
+
+    // await this.api.account.getSession('current').then((response) => {
+    //   console.log('session: ', response);
+    // });
+
+    // TODO: Delete following lines
+    // if (this.params['jwt']) {
+    //   this.token = this.params['jwt'];
+    //   console.log('jwt: ', this.token);
+    //   console.log('redirect to mobile app');
+
+    //   this.navigateWithToken();
+    // } else {
+    //   // Check session
+    //   this.checkSession()
+    //     .then(async (response) => {
+    //       console.log('session: ', response);
+    //       console.log('creating new jwt..');
+    //       this.token = (await this.createJWT()).jwt;
+    //       console.log('jwt: ', this.token);
+    //       console.log('redirect to web app');
+
+    //       this.navigateWithToken();
+    //     })
+    //     .catch((error) => {
+    //       console.log('error: ', error);
+    //       console.log('There is no user session. Redirect to login page.');
+    //     });
+    // }
   }
 
-  checkSession() {
-    return this.api.account.getSession('current');
-  }
+  // checkSession() {
+  //   return this.api.account.getSession('current');
+  // }
 
-  createJWT() {
-    return this.authService.createJWT();
-  }
+  // createJWT() {
+  //   return this.authService.createJWT();
+  // }
 
-  navigateWithToken() {
-    // TODO: If there is no app installed in the device, redirect to web app
-    // window.location.href = `tech.newchapter.languagexchange:/login/oauth2/${this.token}`;
-    // this.navigateToHome();
-  }
+  // navigateWithToken() {
+  //   // TODO: If there is no app installed in the device, redirect to web app
+  //   // window.location.href = `tech.newchapter.languagexchange:/login/oauth2/${this.token}`;
+  //   // this.navigateToHome();
+  // }
 
-  navigateToHome() {
-    // this.router.navigateByUrl('/home');
-    // TODO: Remove this line
-    this.router.navigateByUrl(`/login/oauth2/${this.token}`);
-  }
+  // navigateToHome() {
+  //   // this.router.navigateByUrl('/home');
+  //   // TODO: Remove this line
+  //   this.router.navigateByUrl(`/login/oauth2/${this.token}`);
+  // }
 }
