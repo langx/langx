8776402d93ec0520344c7b481fea0adb28330a95
bhc <behicsakar@gmail.com>
2023-12-30 22:27:41 -0500

    Add listSessionsAction to AccountPage component

--- src/app/pages/home/profile/settings/account/account.page.ts
+++ src/app/pages/home/profile/settings/account/account.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
+import { Models } from 'appwrite';
 
 import { lastSeen } from 'src/app/extras/utils';
 import { Account } from 'src/app/models/Account';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { listIdentitiesAction } from 'src/app/store/actions/auth.action';
+import {
+  listIdentitiesAction,
+  listSessionsAction,
+} from 'src/app/store/actions/auth.action';
 import {
   accountSelector,
   currentUserSelector,
   identitiesSelector,
+  sessionsSelector,
 } from 'src/app/store/selectors/auth.selector';
-import { Models } from 'appwrite';
 
 @Component({
   selector: 'app-account',
@@ -23,6 +27,7 @@ export class AccountPage implements OnInit {
   account$: Observable<Account | null> = null;
   currentUser$: Observable<User> = null;
   identities$: Observable<Models.Identity[]> = null;
+  sessions$: Observable<Models.Session[]> = null;
 
   constructor(private store: Store, private authService: AuthService) {}
 
@@ -33,11 +38,13 @@ export class AccountPage implements OnInit {
   initValues() {
     // Dispatch the action to list identities
     this.store.dispatch(listIdentitiesAction());
+    this.store.dispatch(listSessionsAction());
 
     // Get Selectors
     this.account$ = this.store.pipe(select(accountSelector));
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
     this.identities$ = this.store.pipe(select(identitiesSelector));
+    this.sessions$ = this.store.pipe(select(sessionsSelector));
   }
 
   lastSeen(d: any) {
