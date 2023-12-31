ca79bf772ea49c31d19acac2e9f2fd5d848a2150
bhc <behicsakar@gmail.com>
2023-12-31 05:29:25 -0500

    Add imports and implement email verification functionality

--- src/app/pages/auth/verify-email/verify-email.page.ts
+++ src/app/pages/auth/verify-email/verify-email.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { verifyEmailConfirmationRequestInterface } from 'src/app/models/types/requests/verifyEmailConfirmationRequest.interface';
+import { verifyEmailConfirmationAction } from 'src/app/store/actions/auth.action';
 
 @Component({
   selector: 'app-verify-email',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verify-email.page.scss'],
 })
 export class VerifyEmailPage implements OnInit {
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private toastController: ToastController
+  ) {}
 
-  constructor() { }
+  async ngOnInit() {
+    const params = this.route.snapshot.queryParamMap;
 
-  ngOnInit() {
+    if (!params.get('userId') || !params.get('secret')) {
+      // Present Toast if error
+      this.presentToast('Invalid URL', 'danger');
+      return;
+    }
+
+    const request: verifyEmailConfirmationRequestInterface = {
+      userId: params.get('userId'),
+      secret: params.get('secret'),
+    };
+
+    this.store.dispatch(verifyEmailConfirmationAction({ request }));
   }
 
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
 }
