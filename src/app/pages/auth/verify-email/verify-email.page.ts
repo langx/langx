6fb084d1225e1569b84faf72f4cf46fb567501ad
bhc <behicsakar@gmail.com>
2023-12-31 05:56:01 -0500

    Refactor verify-email page to use @ngrx/store selectors and add subscription management

--- src/app/pages/auth/verify-email/verify-email.page.ts
+++ src/app/pages/auth/verify-email/verify-email.page.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+
 import { verifyEmailConfirmationRequestInterface } from 'src/app/models/types/requests/verifyEmailConfirmationRequest.interface';
 import { verifyEmailConfirmationAction } from 'src/app/store/actions/auth.action';
+import {
+  verifyEmailConfirmationErrorSelector,
+  verifyEmailConfirmationSuccessSelector,
+} from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-verify-email',
@@ -11,6 +18,8 @@ import { verifyEmailConfirmationAction } from 'src/app/store/actions/auth.action
   styleUrls: ['./verify-email.page.scss'],
 })
 export class VerifyEmailPage implements OnInit {
+  subscription: Subscription;
+
   constructor(
     private store: Store,
     private route: ActivatedRoute,
@@ -18,6 +27,44 @@ export class VerifyEmailPage implements OnInit {
   ) {}
 
   async ngOnInit() {
+    this.initValues();
+  }
+
+  ionViewWillEnter() {
+    this.subscription = new Subscription();
+
+    // Present Toast if error
+    this.subscription.add(
+      this.store
+        .pipe(select(verifyEmailConfirmationErrorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) {
+            this.presentToast(error.message, 'danger');
+          }
+        })
+    );
+
+    // Present Toast if verifyEmailConfirmationSuccess
+    this.subscription.add(
+      this.store
+        .pipe(select(verifyEmailConfirmationSuccessSelector))
+        .subscribe((verifyEmailConfirmationSuccess: boolean) => {
+          if (verifyEmailConfirmationSuccess) {
+            this.presentToast(
+              'Email has been successfully verified!',
+              'success'
+            );
+          }
+        })
+    );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
+  initValues() {
     const params = this.route.snapshot.queryParamMap;
 
     if (!params.get('userId') || !params.get('secret')) {
