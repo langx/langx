1eaca23203a79a898c9294945aca96abd2f52820
bhc <behicsakar@gmail.com>
2024-01-02 18:40:06 -0500

    🐛 [BUG] : Login with oauth2 redirect page better UI Fixes #315

---
+++ src/app/components/redirect/redirect.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redirect',
+  templateUrl: './redirect.component.html',
+  styleUrls: ['./redirect.component.scss'],
+})
+export class RedirectComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
