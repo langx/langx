9a3504064cdc1c9afff3b5fb0ef7f638c109a276
bhc <behicsakar@gmail.com>
2024-01-02 20:05:24 -0500

    Add RedirectComponent to ComponentsModule and use it in VerifyEmailPage

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { IonicModule } from '@ionic/angular';
 
 import { EmptyScreenComponent } from 'src/app/components/empty-screen/empty-screen.component';
 import { Oauth2Component } from 'src/app/components/oauth2/oauth2.component';
+import { RedirectComponent } from 'src/app/components/redirect/redirect.component';
 
 @NgModule({
-  declarations: [EmptyScreenComponent, Oauth2Component],
+  declarations: [EmptyScreenComponent, Oauth2Component, RedirectComponent],
   imports: [CommonModule, IonicModule],
-  exports: [EmptyScreenComponent, Oauth2Component],
+  exports: [EmptyScreenComponent, Oauth2Component, RedirectComponent],
 })
 export class ComponentsModule {}
--- src/app/pages/auth/verify-email/verify-email.module.ts
+++ src/app/pages/auth/verify-email/verify-email.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { VerifyEmailPageRoutingModule } from './verify-email-routing.module';
-
 import { VerifyEmailPage } from './verify-email.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    VerifyEmailPageRoutingModule
+    VerifyEmailPageRoutingModule,
+    ComponentsModule,
   ],
-  declarations: [VerifyEmailPage]
+  declarations: [VerifyEmailPage],
 })
 export class VerifyEmailPageModule {}
