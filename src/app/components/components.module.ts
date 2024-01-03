b4b7d1a8e703c6608fd0e2d4f72ec3eea6319667
bhc <behicsakar@gmail.com>
2024-01-02 20:49:17 -0500

    Add SpinnerComponent to ComponentsModule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,10 +5,21 @@ import { IonicModule } from '@ionic/angular';
 import { EmptyScreenComponent } from 'src/app/components/empty-screen/empty-screen.component';
 import { Oauth2Component } from 'src/app/components/oauth2/oauth2.component';
 import { RedirectComponent } from 'src/app/components/redirect/redirect.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [EmptyScreenComponent, Oauth2Component, RedirectComponent],
+  declarations: [
+    EmptyScreenComponent,
+    Oauth2Component,
+    RedirectComponent,
+    SpinnerComponent,
+  ],
   imports: [CommonModule, IonicModule],
-  exports: [EmptyScreenComponent, Oauth2Component, RedirectComponent],
+  exports: [
+    EmptyScreenComponent,
+    Oauth2Component,
+    RedirectComponent,
+    SpinnerComponent,
+  ],
 })
 export class ComponentsModule {}
