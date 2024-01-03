72cac7ff306970a0a38b9efd694574e8cac7b070
bhc <behicsakar@gmail.com>
2024-01-02 20:05:24 -0500

    Add RedirectComponent to ComponentsModule and use it in VerifyEmailPage

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-redirect',
   templateUrl: './redirect.component.html',
   styleUrls: ['./redirect.component.scss'],
 })
-export class RedirectComponent  implements OnInit {
+export class RedirectComponent implements OnInit {
+  @Input() model: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
