152b869444a7a4a34738ebcd28982559df70aeba
bhc <behicsakar@gmail.com>
2024-01-05 03:39:35 -0500

    Refactor notifications page and add toast notifications

--- src/app/pages/home/profile/settings/notifications/notifications.page.ts
+++ src/app/pages/home/profile/settings/notifications/notifications.page.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Capacitor } from '@capacitor/core';
+import { ToastController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { FcmService } from 'src/app/services/fcm/fcm.service';
-import { Account } from 'src/app/models/Account';
-import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import { User } from 'src/app/models/User';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+import { updateCurrentUserAction } from 'src/app/store/actions/user.action';
+import {
+  currentUserSelector,
+  editProfileErrorSelector,
+} from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-notifications',
@@ -16,7 +22,15 @@ import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 export class NotificationsPage implements OnInit {
   form: FormGroup;
 
-  constructor(private store: Store, private fcmService: FcmService) {}
+  subscription: Subscription;
+  currentUser$: Observable<User>;
+  currentUser: User;
+
+  constructor(
+    private store: Store,
+    private fcmService: FcmService,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -24,21 +38,15 @@ export class NotificationsPage implements OnInit {
 
   ionViewWillEnter() {
     this.initValues();
-  }
+    this.subscription = new Subscription();
 
-  initForm() {
-    this.form = new FormGroup({
-      pushNotifications: new FormControl(false),
-      emailNotifications: new FormControl(false),
-    });
-  }
-
-  initValues() {
-    // Get Account Preferences
-    this.store
-      .pipe(select(currentUserSelector))
-      .subscribe((currentUser) => {
-        console.log(currentUser);
+    // Present Toast if success and set form values
+    this.subscription.add(
+      this.currentUser$.subscribe((currentUser) => {
+        if (currentUser?.notifications !== this.currentUser?.notifications) {
+          this.presentToast('Notification settings updated!', 'success');
+        }
+        this.currentUser = currentUser;
         if (currentUser) {
           const notifications = currentUser?.notifications;
 
@@ -49,6 +57,39 @@ export class NotificationsPage implements OnInit {
             .get('emailNotifications')
             .setValue(notifications.includes('email'));
         }
+      })
+    );
+
+    // Present Toast if verifyEmailSuccess
+    this.subscription.add(
+      this.store
+        .pipe(select(editProfileErrorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) {
+            this.presentToast(error.message, 'danger');
+          }
+        })
+    );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      pushNotifications: new FormControl(false),
+      emailNotifications: new FormControl(false),
+    });
+  }
+
+  initValues() {
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.currentUser$
+      .subscribe((currentUser) => {
+        // Set initial values
+        this.currentUser = currentUser;
       })
       .unsubscribe();
   }
@@ -69,10 +110,26 @@ export class NotificationsPage implements OnInit {
       notifications.push('email');
     }
 
-    // const account: Account = {
-    //   notifications,
-    // };
+    // Dispatch updateCurrentUserAction
+    const request = {
+      userId: this.currentUser?.$id,
+      data: { notifications },
+    };
+    this.store.dispatch(updateCurrentUserAction({ request }));
+  }
 
-    // await this.fcmService.updateNotifications(account);
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
   }
 }
