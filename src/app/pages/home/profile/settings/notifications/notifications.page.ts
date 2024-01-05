5cb1c3107577cf9259dd5f0422e09e6b65b70616
bhc <behicsakar@gmail.com>
2024-01-05 02:45:44 -0500

    Refactor notifications page to use reactive forms and retrieve account preferences

--- src/app/pages/home/profile/settings/notifications/notifications.page.ts
+++ src/app/pages/home/profile/settings/notifications/notifications.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Capacitor } from '@capacitor/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { FcmService } from 'src/app/services/fcm/fcm.service';
+import { Account } from 'src/app/models/Account';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-notifications',
@@ -6,7 +14,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
-  constructor() {}
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(private store: Store, private fcmService: FcmService) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  ionViewWillEnter() {
+    this.initValues();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      pushNotifications: new FormControl(false),
+      emailNotifications: new FormControl(false),
+    });
+  }
+
+  initValues() {
+    // Get Account Preferences
+    this.store
+      .pipe(select(currentUserSelector))
+      .subscribe((currentUser) => {
+        console.log(currentUser);
+        if (currentUser) {
+          const notifications = currentUser?.notifications;
+
+          this.form
+            .get('pushNotifications')
+            .setValue(notifications.includes('push'));
+          this.form
+            .get('emailNotifications')
+            .setValue(notifications.includes('email'));
+        }
+      })
+      .unsubscribe();
+  }
 }
