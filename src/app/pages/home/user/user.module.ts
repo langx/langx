066c7b0a5fef95cf86069f60421e3e50f1d6d9df
bhc <behicsakar@gmail.com>
2024-01-05 23:35:59 -0500

    Add ComponentsModule to UserModule imports

--- src/app/pages/home/user/user.module.ts
+++ src/app/pages/home/user/user.module.ts
@@ -1,15 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { UserPageRoutingModule } from './user-routing.module';
-
 import { UserPage } from './user.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, UserPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    UserPageRoutingModule,
+    ComponentsModule,
+  ],
   declarations: [UserPage],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
