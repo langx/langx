0a92d89134e057b77468c2925ce20469b50a4b04
bhc <behicsakar@gmail.com>
2024-01-08 18:25:50 -0500

    Refactor blocked-users page to improve user experience

--- src/app/pages/home/profile/settings/privacy/blocked-users/blocked-users.page.ts
+++ src/app/pages/home/profile/settings/privacy/blocked-users/blocked-users.page.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { User } from 'src/app/models/User';
-import { UserService } from 'src/app/services/user/user.service';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { getBlockedUsersAction } from 'src/app/store/actions/auth.action';
-import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  blockedUsersDataSelector,
+  blockedUsersErrorSelector,
+  currentUserSelector,
+  isLoadingSelector,
+} from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-blocked-users',
@@ -13,17 +19,42 @@ import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
   styleUrls: ['./blocked-users.page.scss'],
 })
 export class BlockedUsersPage implements OnInit {
+  subscription: Subscription;
+
+  isLoading$: Observable<boolean>;
   currentUser$: Observable<User | null>;
   blockedUsersData$: Observable<User[] | null>;
 
-  constructor(private store: Store, private userService: UserService) {}
+  constructor(private store: Store, private toastController: ToastController) {}
 
   ngOnInit() {
     this.initValues();
   }
 
+  ionViewWillEnter() {
+    this.subscription = new Subscription();
+
+    // User Errors
+    this.subscription.add(
+      this.store
+        .pipe(select(blockedUsersErrorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) {
+            this.presentToast(error.message, 'danger');
+          }
+        })
+    );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
   initValues() {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.blockedUsersData$ = this.store.pipe(select(blockedUsersDataSelector));
 
     // Get blocked users data
     this.currentUser$
@@ -37,11 +68,21 @@ export class BlockedUsersPage implements OnInit {
 
   getBlockedUsers(blockedUsers: string[]) {
     const request = { blockedUsers };
-    // this.store.dispatch(getBlockedUsersAction({ request }));
+    this.store.dispatch(getBlockedUsersAction({ request }));
+  }
 
-    // TODO: Test here, delete later
-    this.blockedUsersData$ = this.userService.getBlockedUsers(
-      request.blockedUsers
-    );
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
   }
 }
