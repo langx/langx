1cf036c69103cc9b8720230bbac650addec3329b
bhc <behicsakar@gmail.com>
2024-01-08 18:30:37 -0500

    Refactor blocked user list component

--- src/app/components/blocked-user-list/blocked-user-list.component.ts
+++ src/app/components/blocked-user-list/blocked-user-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { lastSeen } from 'src/app/extras/utils';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-blocked-user-list',
@@ -6,7 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blocked-user-list.component.scss'],
 })
 export class BlockedUserListComponent implements OnInit {
-  constructor() {}
+  @Input() item: User;
+  @Output() onClick: EventEmitter<any> = new EventEmitter();
+
+  constructor(private route: Router) {}
 
   ngOnInit() {}
+
+  redirect() {
+    this.onClick.emit(this.item);
+  }
+
+  goProfile() {
+    this.route.navigateByUrl('/home/user/' + this.item.$id);
+  }
+
+  getStudyLanguages() {
+    let studyLanguages = this.item?.languages
+      .filter((language) => !language?.motherLanguage)
+      .map((language) => language?.name);
+    return studyLanguages.join(', ');
+  }
+
+  //
+  // Utils
+  //
+
+  messageTime(d: any) {
+    if (!d) return null;
+    let time = lastSeen(d);
+    if (time === 'online') time = 'now';
+    return time;
+  }
+
+  lastSeen(d: any) {
+    if (!d) return null;
+    return lastSeen(d);
+  }
 }
