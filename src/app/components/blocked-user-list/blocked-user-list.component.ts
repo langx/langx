e9bb766865a0def1ed2be243d0d73946d55b60b1
bhc <behicsakar@gmail.com>
2024-01-08 18:28:22 -0500

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ src/app/components/blocked-user-list/blocked-user-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blocked-user-list',
+  templateUrl: './blocked-user-list.component.html',
+  styleUrls: ['./blocked-user-list.component.scss'],
+})
+export class BlockedUserListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
