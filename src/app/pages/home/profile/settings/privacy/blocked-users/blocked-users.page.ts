00780f4bafa96cfa236b99848ab8a18208400186
bhc <behicsakar@gmail.com>
2024-01-08 01:37:39 -0500

    Refactor blocked-users page to display blocked users list

--- src/app/pages/home/profile/settings/privacy/blocked-users/blocked-users.page.ts
+++ src/app/pages/home/profile/settings/privacy/blocked-users/blocked-users.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { User } from 'src/app/models/User';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-blocked-users',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blocked-users.page.scss'],
 })
 export class BlockedUsersPage implements OnInit {
-
-  constructor() { }
+  currentUser$: Observable<User | null>;
+  constructor(private store: Store) {}
 
   ngOnInit() {
+    this.initValues();
   }
 
+  initValues() {
+    // Get blocked users data
+    // this.store.dispatch();
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+  }
 }
