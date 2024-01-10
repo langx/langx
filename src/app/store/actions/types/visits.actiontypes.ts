6b5c9cc2af6970ac1f81f1e534bd7f833d5fb257
bhc <behicsakar@gmail.com>
2024-01-10 00:37:06 -0500

    Add actions for getting visits with offset

--- src/app/store/actions/types/visits.actiontypes.ts
+++ src/app/store/actions/types/visits.actiontypes.ts
@@ -2,4 +2,7 @@ export enum ActionTypes {
   GET_VISITS = '[Visits] Get Visits',
   GET_VISITS_SUCCESS = '[Visits] Get Visits Success',
   GET_VISITS_FAILURE = '[Visits] Get Visits Failure',
+  GET_VISITS_WITH_OFFSET = '[Visits] Get Visits With Offset',
+  GET_VISITS_WITH_OFFSET_SUCCESS = '[Visits] Get Visits With Offset Success',
+  GET_VISITS_WITH_OFFSET_FAILURE = '[Visits] Get Visits With Offset Failure',
 }
--- src/app/store/actions/visits.action.ts
+++ src/app/store/actions/visits.action.ts
@@ -4,6 +4,7 @@ import { ActionTypes } from 'src/app/store/actions/types/visits.actiontypes';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { listVisitsResponseInterface } from 'src/app/models/types/responses/listVisitsResponse.interface';
 
+// Get Visits Actions
 export const getVisitsAction = createAction(ActionTypes.GET_VISITS);
 
 export const getVisitsSuccessAction = createAction(
@@ -13,5 +14,21 @@ export const getVisitsSuccessAction = createAction(
 
 export const getVisitsFailureAction = createAction(
   ActionTypes.GET_VISITS_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
+
+// Get Visits With Offset Actions
+export const getVisitsWithOffsetAction = createAction(
+  ActionTypes.GET_VISITS_WITH_OFFSET,
+  props<{ offset: number }>()
+);
+
+export const getVisitsWithOffsetSuccessAction = createAction(
+  ActionTypes.GET_VISITS_WITH_OFFSET_SUCCESS,
+  props<{ payload: listVisitsResponseInterface }>()
+);
+
+export const getVisitsWithOffsetFailureAction = createAction(
+  ActionTypes.GET_VISITS_WITH_OFFSET_FAILURE,
   props<{ error: ErrorInterface }>()
 );
