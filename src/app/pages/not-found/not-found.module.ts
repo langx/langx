39b17ad719bbbfe8b24c36830dcc7b2c27bc4783
bhc <behicsakar@gmail.com>
2024-01-10 02:10:16 -0500

    Add ComponentsModule to NotFoundPageModule imports

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -7,9 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { NotFoundPageRoutingModule } from './not-found-routing.module';
 
 import { NotFoundPage } from './not-found.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, NotFoundPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NotFoundPageRoutingModule,
+    ComponentsModule,
+  ],
   declarations: [NotFoundPage],
 })
 export class NotFoundPageModule {}
