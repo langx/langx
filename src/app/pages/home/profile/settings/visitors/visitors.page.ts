a9132f7494adeb26f96b5ed2bcf2a1367bfc020f
bhc <behicsakar@gmail.com>
2024-01-10 00:29:23 -0500

    Add toast functionality to VisitorsPage

--- src/app/pages/home/profile/settings/visitors/visitors.page.ts
+++ src/app/pages/home/profile/settings/visitors/visitors.page.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { User } from 'src/app/models/User';
 import { Visit } from 'src/app/models/Visit';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { getVisitsAction } from 'src/app/store/actions/visits.action';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 import {
+  errorSelector,
   isLoadingSelector,
   totalSelector,
   visitsSelector,
@@ -18,12 +21,14 @@ import {
   styleUrls: ['./visitors.page.scss'],
 })
 export class VisitorsPage implements OnInit {
+  subscription: Subscription;
+
   currentUser$: Observable<User | null> = null;
   isLoading$: Observable<boolean> = null;
   visits$: Observable<Visit[] | null> = null;
   total$: Observable<number | null> = null;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private toastController: ToastController) {}
 
   ngOnInit() {
     this.initValues();
@@ -31,9 +36,29 @@ export class VisitorsPage implements OnInit {
     this.listVisits();
   }
 
+  ionViewWillEnter() {
+    this.subscription = new Subscription();
+
+    // User Errors
+    this.subscription.add(
+      this.store
+        .pipe(select(errorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) {
+            this.presentToast(error.message, 'danger');
+          }
+        })
+    );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
   initValues() {
-    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.visits$ = this.store.pipe(select(visitsSelector));
     this.total$ = this.store.pipe(select(totalSelector));
   }
@@ -50,5 +75,20 @@ export class VisitorsPage implements OnInit {
   handleRefresh(event) {
     this.listVisits();
     if (event) event.target.complete();
+  }
+
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
   }
 }
