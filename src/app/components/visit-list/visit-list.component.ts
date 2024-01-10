62060631c2bccf369b6967bda5af80e0ae4cd80b
bhc <behicsakar@gmail.com>
2024-01-09 23:03:52 -0500

    Add visit-list component

---
+++ src/app/components/visit-list/visit-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-list',
+  templateUrl: './visit-list.component.html',
+  styleUrls: ['./visit-list.component.scss'],
+})
+export class VisitListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
