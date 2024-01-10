a54117e926958e7e1e9a366ed478932862e40af8
bhc <behicsakar@gmail.com>
2024-01-10 00:00:34 -0500

    Refactor visitors page to display visit list

--- src/app/pages/home/profile/settings/visitors/visitors.page.ts
+++ src/app/pages/home/profile/settings/visitors/visitors.page.ts
@@ -3,9 +3,15 @@ import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { User } from 'src/app/models/User';
+import { Visit } from 'src/app/models/Visit';
 import { UserService } from 'src/app/services/user/user.service';
 import { getVisitsAction } from 'src/app/store/actions/visits.action';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  isLoadingSelector,
+  totalSelector,
+  visitsSelector,
+} from 'src/app/store/selectors/visits.selector';
 
 @Component({
   selector: 'app-visitors',
@@ -14,6 +20,9 @@ import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 })
 export class VisitorsPage implements OnInit {
   currentUser$: Observable<User | null> = null;
+  isLoading$: Observable<boolean> = null;
+  visits$: Observable<Visit[] | null> = null;
+  total$: Observable<number | null> = null;
 
   constructor(private store: Store, private userService: UserService) {}
 
@@ -24,12 +33,19 @@ export class VisitorsPage implements OnInit {
   }
 
   initValues() {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.visits$ = this.store.pipe(select(visitsSelector));
+    this.total$ = this.store.pipe(select(totalSelector));
   }
 
   listVisits() {
     // Dispatch action to get all visits
     this.store.dispatch(getVisitsAction());
+  }
+
+  getRoom(user: any) {
+    console.log('getRoom', user);
   }
 
   //
