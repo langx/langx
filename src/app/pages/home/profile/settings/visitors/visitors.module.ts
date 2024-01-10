e54a9433cf3ebadd88765a5a530f79f975f19a01
bhc <behicsakar@gmail.com>
2024-01-09 23:01:48 -0500

    Add StoreModule for visits feature

--- src/app/pages/home/profile/settings/visitors/visitors.module.ts
+++ src/app/pages/home/profile/settings/visitors/visitors.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { VisitorsPageRoutingModule } from './visitors-routing.module';
 import { VisitorsPage } from './visitors.page';
+
 import { ComponentsModule } from 'src/app/components/components.module';
 import { VisitsEffects } from 'src/app/store/effects/visits.effect';
+import { visitsReducers } from 'src/app/store/reducers/visits.reducer';
 
 @NgModule({
   imports: [
@@ -16,7 +19,7 @@ import { VisitsEffects } from 'src/app/store/effects/visits.effect';
     IonicModule,
     VisitorsPageRoutingModule,
     ComponentsModule,
-    //StoreModule.forFeature('visit', visitReducers),
+    StoreModule.forFeature('visit', visitsReducers),
     EffectsModule.forFeature([VisitsEffects]),
   ],
   declarations: [VisitorsPage],
