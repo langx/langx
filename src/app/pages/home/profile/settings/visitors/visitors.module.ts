ef839ba3c01aef04b693de92cdf84bd0869481aa
bhc <behicsakar@gmail.com>
2024-01-09 22:20:52 -0500

    Add components and effects to Visitors module

--- src/app/pages/home/profile/settings/visitors/visitors.module.ts
+++ src/app/pages/home/profile/settings/visitors/visitors.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { EffectsModule } from '@ngrx/effects';
 
 import { VisitorsPageRoutingModule } from './visitors-routing.module';
-
 import { VisitorsPage } from './visitors.page';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { VisitsEffects } from 'src/app/store/effects/visits.effect';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    VisitorsPageRoutingModule
+    VisitorsPageRoutingModule,
+    ComponentsModule,
+    //StoreModule.forFeature('visit', visitReducers),
+    EffectsModule.forFeature([VisitsEffects]),
   ],
-  declarations: [VisitorsPage]
+  declarations: [VisitorsPage],
 })
 export class VisitorsPageModule {}
