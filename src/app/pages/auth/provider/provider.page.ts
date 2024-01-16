872d24cf715a51b35ee27aecbe17a3b9177b74e9
bhc <behicsakar@gmail.com>
2024-01-15 22:59:45 -0500

    Update provider page with login title and redirect model

--- src/app/pages/auth/provider/provider.page.ts
+++ src/app/pages/auth/provider/provider.page.ts
@@ -8,10 +8,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProviderPage implements OnInit {
   provider: string;
+  title: string;
+
+  model = {
+    success: true,
+    title: '',
+    color: 'medium',
+    icon: 'shield-checkmark-outline',
+  };
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.provider = this.route.snapshot.paramMap.get('name') || null;
+    this.title = this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
+    this.model.title = 'Redirecting ' + this.title + ' authentication...';
   }
 }
