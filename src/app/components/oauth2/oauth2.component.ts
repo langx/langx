8057167e19b1bb4c5544d697a86ef0c620ca1985
bhc <behicsakar@gmail.com>
2024-01-16 02:22:23 -0500

    Refactor OAuth2Component to remove unused code and add browser event listeners

--- src/app/components/oauth2/oauth2.component.ts
+++ src/app/components/oauth2/oauth2.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InAppBrowser, UrlEvent } from '@capgo/inappbrowser';
 
 import { environment } from 'src/environments/environment';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-oauth2',
@@ -15,13 +14,42 @@ export class Oauth2Component implements OnInit {
     'user-agent': `Mozilla/5.0 (iPhone; CPU iPhone OS16_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.2 Mobile/15E148 Safari/604.1`,
   };
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  signInWithGoogle() {
+    InAppBrowser.openWebView({
+      url: `${environment.url.AUTH_PROVIDER_URL}/google`,
+      headers: this.HEADERS,
+    });
+    this.addBrowserListeners();
+  }
+
+  signInWithFacebook() {
+    InAppBrowser.openWebView({
+      url: `${environment.url.AUTH_PROVIDER_URL}/facebook`,
+      headers: this.HEADERS,
+    });
+    this.addBrowserListeners();
+  }
+
+  signInWithApple() {
+    // TODO: #313  🚀 [Feature] : Native sing-in-with-apple
+    InAppBrowser.openWebView({
+      url: `${environment.url.AUTH_PROVIDER_URL}/apple`,
+      headers: this.HEADERS,
+    });
+    this.addBrowserListeners();
+  }
+
+  addBrowserListeners() {
+    console.log('InAppBrowser listeners started');
+
+    // urlChangeEvent
     InAppBrowser.addListener('urlChangeEvent', (res: UrlEvent) => {
       if (res.url.startsWith(environment.url.SUCCESS_OAUTH2)) {
         // Remove delete
-        InAppBrowser.removeAllListeners();
         InAppBrowser.close();
 
         const redirectUrl = res.url.split(environment.url.HOMEPAGE_URL)[1];
@@ -30,7 +58,6 @@ export class Oauth2Component implements OnInit {
       }
       if (res.url.startsWith(environment.url.FAILURE_OAUTH2)) {
         // Remove delete
-        InAppBrowser.removeAllListeners();
         InAppBrowser.close();
 
         const redirectUrl = res.url.split(environment.url.HOMEPAGE_URL)[1];
@@ -38,31 +65,13 @@ export class Oauth2Component implements OnInit {
         this.router.navigateByUrl(redirectUrl);
       }
     });
-  }
 
-  ngOnDestroy() {
-    InAppBrowser.removeAllListeners();
-  }
-
-  signInWithGoogle() {
-    InAppBrowser.open({
-      url: `${environment.url.AUTH_PROVIDER_URL}/google`,
-      headers: this.HEADERS,
-      preventDeeplink: true,
+    // Close event
+    InAppBrowser.addListener('closeEvent', () => {
+      console.log('InAppBrowser closed');
+      // Remove delete
+      InAppBrowser.removeAllListeners();
+      console.log('all listeners removed');
     });
-  }
-
-  signInWithFacebook() {
-    InAppBrowser.open({
-      url: `${environment.url.AUTH_PROVIDER_URL}/facebook`,
-      headers: this.HEADERS,
-      preventDeeplink: true,
-    });
-    // this.authService.signInWithFacebook();
-  }
-
-  signInWithApple() {
-    // TODO: #313  🚀 [Feature] : Native sing-in-with-apple
-    this.authService.signInWithApple();
   }
 }
