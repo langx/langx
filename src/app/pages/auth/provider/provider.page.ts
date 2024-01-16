0d70c228715aab81c238052868315d98611b8c78
bhc <behicsakar@gmail.com>
2024-01-15 23:15:07 -0500

    Add sign-in functionality for different providers

--- src/app/pages/auth/provider/provider.page.ts
+++ src/app/pages/auth/provider/provider.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-provider',
@@ -13,15 +14,41 @@ export class ProviderPage implements OnInit {
   model = {
     success: true,
     title: '',
+    subtitle: '',
     color: 'medium',
     icon: 'shield-checkmark-outline',
   };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.provider = this.route.snapshot.paramMap.get('name') || null;
     this.title = this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
     this.model.title = 'Redirecting ' + this.title + ' authentication...';
+
+    // Trigger Sign-in with provider
+    this.signInWithProvider(this.provider);
+  }
+
+  signInWithProvider(provider: string) {
+    switch (provider) {
+      case 'google':
+        this.authService.signInWithGoogle();
+        break;
+      case 'facebook':
+        this.authService.signInWithFacebook();
+        break;
+      case 'apple':
+        this.authService.signInWithApple();
+        break;
+      default:
+        this.model.success = false;
+        this.model.title = 'Please close this window.';
+        this.model.color = 'danger';
+        this.model.icon = 'close-outline';
+    }
   }
 }
