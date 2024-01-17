2cc936e3a4cef3248587f679de17c405ee6e1bd9
bhc <behicsakar@gmail.com>
2024-01-17 01:35:24 -0500

    Update ServiceWorker registration strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,8 +37,8 @@ import { AuthEffect } from 'src/app/store/effects/auth.effect';
     ServiceWorkerModule.register('combined-sw.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
+      // or after 5 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:5000',
     }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
