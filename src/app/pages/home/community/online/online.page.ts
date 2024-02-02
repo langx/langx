9e0559f4c6b1e90b2ac30c2317e9b3d009072ee8
bhc <behicsakar@gmail.com>
2024-02-02 10:50:53 +0300

    Add ComponentsModule to imports in OnlinePageModule

--- src/app/pages/home/community/online/online.page.ts
+++ src/app/pages/home/community/online/online.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+
+import { User } from 'src/app/models/User';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  totalSelector,
+  usersSelector,
+  isLoadingSelector,
+} from 'src/app/store/selectors/user.selector';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-online',
@@ -6,7 +17,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./online.page.scss'],
 })
 export class OnlinePage implements OnInit {
-  constructor() {}
+  isLoading$: Observable<boolean>;
+  currentUser$: Observable<User>;
+  users$: Observable<User[] | null> = null;
+  total$: Observable<number | null> = null;
 
-  ngOnInit() {}
+  constructor(private store: Store, private router: Router) {}
+
+  ngOnInit() {
+    this.initValues();
+  }
+
+  initValues(): void {
+    // Set values from selectors
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.users$ = this.store.pipe(select(usersSelector));
+    this.total$ = this.store.pipe(select(totalSelector));
+  }
+
+  //
+  // Routes
+  //
+
+  getUserPage(id: string): void {
+    this.router.navigate([`/home/user/${id}`]);
+  }
 }
