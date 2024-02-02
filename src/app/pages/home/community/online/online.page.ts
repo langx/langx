f0fbb34378dcc5eb3b07b03b74108c5043a116cc
bhc <behicsakar@gmail.com>
2024-02-02 10:56:30 +0300

    Refactor online page component

--- src/app/pages/home/community/online/online.page.ts
+++ src/app/pages/home/community/online/online.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 
 import { User } from 'src/app/models/User';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import { getUsersAction, getUsersWithOffsetAction } from 'src/app/store/actions/users.action';
+import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
+import { FilterService } from 'src/app/services/filter/filter.service';
+import { StorageService } from 'src/app/services/storage/storage.service';
 import {
   totalSelector,
   usersSelector,
   isLoadingSelector,
 } from 'src/app/store/selectors/user.selector';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-online',
@@ -17,15 +21,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./online.page.scss'],
 })
 export class OnlinePage implements OnInit {
+  filter$: any;
+  filterData: FilterDataInterface;
+
   isLoading$: Observable<boolean>;
   currentUser$: Observable<User>;
   users$: Observable<User[] | null> = null;
   total$: Observable<number | null> = null;
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private filterService: FilterService,
+    private storageService: StorageService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.initValues();
+
+    // Check Local Storage for filters
+    await this.checkLocalStorage();
+    await this.checkFilter();
   }
 
   initValues(): void {
@@ -37,10 +53,112 @@ export class OnlinePage implements OnInit {
   }
 
   //
+  // Get Users
+  //
+
+  listUsers() {
+    const filterData = this.filterData;
+    this.store.dispatch(getUsersAction({ request: { filterData } }));
+  }
+
+  //
+  // Check Filter
+  //
+
+  async checkFilter() {
+    this.filter$ = this.filterService
+      .getEvent()
+      .subscribe((filterData: FilterDataInterface) => {
+        this.filterData = filterData;
+        // console.log('Subscribed filter: ', filterData);
+
+        // List Users
+        this.listUsers();
+      });
+  }
+
+  // TODO: #246 Save filterData with JSON.stringify();
+  async checkLocalStorage() {
+    // Getting the filter data from Capacitor Preferences
+    let languagesString =
+      (await this.storageService.getValue('languages')) || [];
+    const gender = (await this.storageService.getValue('gender')) || null;
+    const country = (await this.storageService.getValue('country')) || null;
+    const minAgeString = (await this.storageService.getValue('minAge')) || null;
+    const maxAgeString = (await this.storageService.getValue('maxAge')) || null;
+
+    let minAge = Number(minAgeString) || null;
+    let maxAge = Number(maxAgeString) || null;
+
+    // TODO: Do better logic here
+    let languages: Array<any> = [];
+    if (languagesString) {
+      languages = languagesString.toLocaleString().split(',');
+      if (languages.length === 1 && languages[0] === '') {
+        languages = [];
+      }
+    }
+
+    let filterData: FilterDataInterface = {
+      languages: languages,
+      gender: gender,
+      country: country,
+      minAge: minAge,
+      maxAge: maxAge,
+    };
+
+    // console.log('checkLocalStorage', filterData);
+    this.filterService.setEvent(filterData);
+  }
+
+  //
   // Routes
   //
 
   getUserPage(id: string): void {
     this.router.navigate([`/home/user/${id}`]);
+  }
+
+  //
+  // Pull to refresh
+  //
+
+  handleRefresh(event) {
+    this.listUsers();
+    if (event) event.target.complete();
+  }
+
+    //
+  // Infinite Scroll
+  //
+
+  loadMore(event) {
+    // Offset is the number of users already loaded
+    let offset: number = 0;
+    this.users$
+      .subscribe((users) => {
+        offset = users.length;
+        this.total$
+          .subscribe((total) => {
+            if (offset < total) {
+              const filterData = this.filterData;
+              this.store.dispatch(
+                getUsersWithOffsetAction({
+                  request: {
+                    filterData,
+                    offset,
+                  },
+                })
+              );
+            } else {
+              console.log('All users loaded');
+            }
+          })
+          .unsubscribe();
+      })
+      .unsubscribe();
+
+    // this.getUsers(this.filterData);
+    event.target.complete();
   }
 }
