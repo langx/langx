02083f505b50e75a34dcf8f2ffa42baa1bb5664c
bhc <behicsakar@gmail.com>
2024-02-03 12:53:40 +0300

    Remove unused import in new.module.ts

--- src/app/pages/home/community/new/new.module.ts
+++ src/app/pages/home/community/new/new.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
