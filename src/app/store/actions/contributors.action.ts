c2132dff17ac7f2f92c45d6c5243daea2b8ab9aa
bhc <behicsakar@gmail.com>
2024-02-12 17:52:27 +0300

    Update contributors action to use listUsersResponseInterface

--- src/app/store/actions/contributors.action.ts
+++ src/app/store/actions/contributors.action.ts
@@ -1,15 +1,15 @@
 import { createAction, props } from '@ngrx/store';
 
 import { ActionTypes } from 'src/app/store/actions/types/contributors.actiontypes';
-import { User } from 'src/app/models/User';
 import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+import { listUsersResponseInterface } from 'src/app/models/types/responses/listUsersResponse.interface';
 
 // Get Contributors Actions
 export const getContributorsAction = createAction(ActionTypes.GET_CONTRIBUTORS);
 
 export const getContributorsSuccessAction = createAction(
   ActionTypes.GET_CONTRIBUTORS_SUCCESS,
-  props<{ payload: User[] }>()
+  props<{ payload: listUsersResponseInterface }>()
 );
 
 export const getContributorsFailureAction = createAction(
