90db4b6eb2d864faeef84590e366582e04b98f29
bhc <behicsakar@gmail.com>
2024-02-12 17:52:45 +0300

    Add contributors selector to store

---
+++ src/app/store/selectors/contributors.selector.ts
@@ -0,0 +1,25 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { ContributorsStateInterface } from 'src/app/models/types/states/contributorsState.interface';
+
+export const contributorsFeatureSelector =
+  createFeatureSelector<ContributorsStateInterface>('visit');
+
+export const isLoadingSelector = createSelector(
+  contributorsFeatureSelector,
+  (contributorsState: ContributorsStateInterface) => contributorsState.isLoading
+);
+export const usersSelector = createSelector(
+  contributorsFeatureSelector,
+  (contributorsState: ContributorsStateInterface) => contributorsState.users
+);
+
+export const totalSelector = createSelector(
+  contributorsFeatureSelector,
+  (contributorsState: ContributorsStateInterface) => contributorsState.total
+);
+
+export const errorSelector = createSelector(
+  contributorsFeatureSelector,
+  (contributorsState: ContributorsStateInterface) => contributorsState.error
+);
