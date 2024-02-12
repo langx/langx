fbdfd481a88932734362de3e1c8e10577b146519
bhc <behicsakar@gmail.com>
2024-02-12 17:52:41 +0300

    Add contributors reducer

---
+++ src/app/store/reducers/contributors.reducer.ts
@@ -0,0 +1,53 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import { ContributorsStateInterface } from 'src/app/models/types/states/contributorsState.interface';
+import {
+  getContributorsAction,
+  getContributorsSuccessAction,
+  getContributorsFailureAction,
+} from 'src/app/store/actions/contributors.action';
+
+const initialState: ContributorsStateInterface = {
+  isLoading: false,
+  total: null,
+  users: null,
+  error: null,
+};
+
+const contributorsReducer = createReducer(
+  initialState,
+
+  // Get Contributors Reducers
+  on(
+    getContributorsAction,
+    (state): ContributorsStateInterface => ({
+      ...state,
+      isLoading: true,
+      error: null,
+    })
+  ),
+  on(
+    getContributorsSuccessAction,
+    (state, action): ContributorsStateInterface => ({
+      ...state,
+      isLoading: false,
+      total: action.payload.total,
+      users: action.payload.documents,
+    })
+  ),
+  on(
+    getContributorsFailureAction,
+    (state, action): ContributorsStateInterface => ({
+      ...state,
+      isLoading: false,
+      error: action.error,
+    })
+  )
+);
+
+export function contributorsReducers(
+  state: ContributorsStateInterface,
+  action: Action
+) {
+  return contributorsReducer(state, action);
+}
