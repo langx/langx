ba2708fee55b02b6972896e903e96a47dd197f9d
bhc <behicsakar@gmail.com>
2024-02-25 00:44:42 +0300

    Add sponsors actions and action types

---
+++ src/app/store/actions/types/sponsors.actiontypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  GET_SPONSORS = '[Sponsors] Get Sponsors',
+  GET_SPONSORS_SUCCESS = '[Sponsors] Get Sponsors Success',
+  GET_SPONSORS_FAILED = '[Sponsors] Get Sponsors Failed',
+}
