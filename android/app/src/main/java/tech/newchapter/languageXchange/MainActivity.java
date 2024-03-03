0deedc0be5334121f0c445b090238fb053834537
bhc <behicsakar@gmail.com>
2024-03-03 15:38:16 +0300

    Remove unused code in MainActivity.java

--- android/app/src/main/java/tech/newchapter/languageXchange/MainActivity.java
+++ android/app/src/main/java/tech/newchapter/languageXchange/MainActivity.java
@@ -2,22 +2,4 @@ package tech.newchapter.languageXchange;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(
-        savedInstanceState,
-        new ArrayList<Class<? extends Plugin>>() {
-          {
-            // Additional plugins you've installed go here
-            // Ex: add(TotallyAwesomePlugin.class);
-            add(NativeMarket.class);
-          }
-        }
-      );
-  }
-}
+public class MainActivity extends BridgeActivity {}
