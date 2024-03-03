db25ee3b7c221a095259ea6d6badbf554cd6024f
bhc <behicsakar@gmail.com>
2024-03-03 13:32:58 +0300

    Add NativeMarket plugin initialization in MainActivity

--- android/app/src/main/java/tech/newchapter/languageXchange/MainActivity.java
+++ android/app/src/main/java/tech/newchapter/languageXchange/MainActivity.java
@@ -2,4 +2,22 @@ package tech.newchapter.languageXchange;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(
+        savedInstanceState,
+        new ArrayList<Class<? extends Plugin>>() {
+          {
+            // Additional plugins you've installed go here
+            // Ex: add(TotallyAwesomePlugin.class);
+            add(NativeMarket.class);
+          }
+        }
+      );
+  }
+}
