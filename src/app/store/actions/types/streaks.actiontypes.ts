32c76dc012fcc981089c366cfb065c3f8361648d
bhc <behicsakar@gmail.com>
2024-03-05 22:37:50 +0300

    Add listStreaksResponseInterface and streaks actions

---
+++ src/app/store/actions/types/streaks.actiontypes.ts
@@ -0,0 +1,8 @@
+export enum ActionTypes {
+  GET_STREAKS = '[Streaks] Get Visits',
+  GET_STREAKS_SUCCESS = '[Streaks] Get Visits Success',
+  GET_STREAKS_FAILURE = '[Streaks] Get Visits Failure',
+  GET_STREAKS_WITH_OFFSET = '[Streaks] Get Visits With Offset',
+  GET_STREAKS_WITH_OFFSET_SUCCESS = '[Streaks] Get Visits With Offset Success',
+  GET_STREAKS_WITH_OFFSET_FAILURE = '[Streaks] Get Visits With Offset Failure',
+}
