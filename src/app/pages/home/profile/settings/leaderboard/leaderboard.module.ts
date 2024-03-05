264ce6340dda27c56a73c4c58b006c3f2b270be4
bhc <behicsakar@gmail.com>
2024-03-05 23:20:13 +0300

    Add leaderboard store and effects

--- src/app/pages/home/profile/settings/leaderboard/leaderboard.module.ts
+++ src/app/pages/home/profile/settings/leaderboard/leaderboard.module.ts
@@ -8,6 +8,10 @@ import { LeaderboardPageRoutingModule } from './leaderboard-routing.module';
 
 import { LeaderboardPage } from './leaderboard.page';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { StoreModule } from '@ngrx/store';
+import { streaksReducers } from 'src/app/store/reducers/streaks.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { StreaksEffects } from 'src/app/store/effects/streaks.effect';
 
 @NgModule({
   imports: [
@@ -16,6 +20,8 @@ import { ComponentsModule } from 'src/app/components/components.module';
     IonicModule,
     LeaderboardPageRoutingModule,
     ComponentsModule,
+    StoreModule.forFeature('streak', streaksReducers),
+    EffectsModule.forFeature([StreaksEffects]),
   ],
   declarations: [LeaderboardPage],
 })
