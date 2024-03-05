a5765d494bdf1a4aa03a2b77d33460dd48fa3647
bhc <behicsakar@gmail.com>
2024-03-05 23:50:58 +0300

    Refactor streak-list component to display user information and profile photo

--- src/app/components/streak-list/streak-list.component.ts
+++ src/app/components/streak-list/streak-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { exactDateAndTime, getFlagEmoji, lastSeen, onlineStatus } from 'src/app/extras/utils';
+import { Streak } from 'src/app/models/Streaks';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-streak-list',
@@ -6,7 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./streak-list.component.scss'],
 })
 export class StreakListComponent implements OnInit {
-  constructor() {}
+  @Input() item: Streak;
 
-  ngOnInit() {}
+  user: User;
+
+  constructor(private route: Router) {}
+
+  ngOnInit() {
+    this.user = this.item.userId;
+  }
+
+  goProfile() {
+    this.route.navigateByUrl('/home/user/' + this.user.$id);
+  }
+
+  //
+  // Utils
+  //
+
+  exactDateAndTime(d: any) {
+    if (!d) return null;
+    return exactDateAndTime(d);
+  }
+
+  lastSeen(d: any) {
+    if (!d) return null;
+    return lastSeen(d);
+  }
+
+  onlineStatus(d: any) {
+    if (!d) return null;
+    return onlineStatus(d);
+  }
+
+  getFlagEmoji(item: User) {
+    return getFlagEmoji(item);
+  }
 }
