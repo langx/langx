2bb1717b149c5ab1dca419bc8129bfeaf256b92e
bhc <behicsakar@gmail.com>
2024-04-02 13:46:06 +0300

    Add update-user function files

---
+++ functions/update-user/src/main.js
@@ -0,0 +1,20 @@
+import { Client, Databases } from 'node-appwrite';
+
+export default async ({ req, res, log, error }) => {
+  // Init SDK
+  const client = new Client()
+    .setEndpoint(process.env.APP_ENDPOINT)
+    .setProject(process.env.APP_PROJECT)
+    .setKey(process.env.API_KEY);
+
+  const db = new Databases(client);
+
+  log('Update User function called');
+  log(req.body);
+
+  try {
+    res.json({ ok: true, error: null });
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+};
