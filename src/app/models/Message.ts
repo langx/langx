9fff2d8c3cc7391c1a88c4d9ba280e3f151fdab8
bhc <behicsakar@gmail.com>
2024-04-04 11:46:25 +0300

    Add 'deleted' property to Message model

--- src/app/models/Message.ts
+++ src/app/models/Message.ts
@@ -4,6 +4,7 @@ import { Models } from 'src/app/extras/sdk/src';
 export type Message = Models.Document & {
   sender: string;
   seen: boolean | false;
+  deleted: boolean | false;
   to: string;
   roomId: string;
   replyTo?: string;
