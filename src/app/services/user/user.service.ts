95d729219471da76dc34180f1f43cf8f759d0383
bhc <behicsakar@gmail.com>
2024-04-10 02:11:11 +0300

    Refactor profile photo URL and update image URLs in pre-chat-box component

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -151,9 +151,7 @@ export class UserService {
 
     // Query for users with completed profiles
     queries.push(Query.notEqual('aboutMe', ''));
-    queries.push(
-      Query.notEqual('profilePic', environment.defaultAssets.DEFAULT_PP_ID)
-    );
+    queries.push(Query.notEqual('profilePic', environment.defaultAssets.PP_ID));
 
     // Query for users descending by last seen
     queries.push(Query.orderDesc('lastSeen'));
