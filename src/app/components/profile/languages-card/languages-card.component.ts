a931ddc8105a5935ba7a6c150c398c3cae30eb36
bhc <behicsakar@gmail.com>
2024-04-22 20:29:10 +0300

    Add LanguagesCardComponent to profile module

---
+++ src/app/components/profile/languages-card/languages-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-languages-card',
+  templateUrl: './languages-card.component.html',
+  styleUrls: ['./languages-card.component.scss'],
+})
+export class LanguagesCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
