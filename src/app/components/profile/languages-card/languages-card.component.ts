b36de38bc3c2d0692924d0493e9a3720fead9648
bhc <behicsakar@gmail.com>
2024-04-22 20:54:56 +0300

    Remove console.log statement in LanguagesCardComponent

--- src/app/components/profile/languages-card/languages-card.component.ts
+++ src/app/components/profile/languages-card/languages-card.component.ts
@@ -15,8 +15,6 @@ export class LanguagesCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    console.log(this.languages);
-
     this.studyLanguages = this.languages?.filter(
       (lang) => !lang.motherLanguage
     );
