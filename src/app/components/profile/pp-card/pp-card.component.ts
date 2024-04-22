050ecfba3d69ae9bc2ec319326082dceab502656
bhc <behicsakar@gmail.com>
2024-04-22 14:28:23 +0300

    🐛 : Profile Page Jumping Up When `lastSeen` Updated Fixes #686

---
+++ src/app/components/profile/pp-card/pp-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pp-card',
+  templateUrl: './pp-card.component.html',
+  styleUrls: ['./pp-card.component.scss'],
+})
+export class PpCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
