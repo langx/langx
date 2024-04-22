d5c952fe1895dc35025e05a9b4663369207bb90f
bhc <behicsakar@gmail.com>
2024-04-22 17:22:47 +0300

    Refactor OtherPhotosCardComponent to use Swiper library for displaying other photos

--- src/app/components/profile/other-photos-card/other-photos-card.component.ts
+++ src/app/components/profile/other-photos-card/other-photos-card.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { IonModal, ModalController } from '@ionic/angular';
 import { Observable, forkJoin, of } from 'rxjs';
 
 import { UserService } from 'src/app/services/user/user.service';
+
+import { PreviewPhotoComponent } from 'src/app/components/preview-photo/preview-photo.component';
 
 @Component({
   selector: 'app-other-photos-card',
@@ -10,14 +13,34 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class OtherPhotosCardComponent implements OnInit {
   @Input() otherPics: string[];
+  @ViewChild(IonModal) modal: IonModal;
 
   otherPics$: Observable<URL[]> = of([]);
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
     this.otherPics$ = forkJoin(
       (this.otherPics || []).map((id) => this.userService.getUserFileView(id))
     );
+  }
+
+  async openPreview(photos$: Observable<URL | URL[]>): Promise<void> {
+    photos$.subscribe(async (photos) => {
+      const modal = await this.modalCtrl.create({
+        component: PreviewPhotoComponent,
+        componentProps: {
+          photos: Array.isArray(photos) ? photos : [photos],
+        },
+      });
+      modal.present();
+    });
+  }
+
+  dismissModal() {
+    this.modal.dismiss();
   }
 }
