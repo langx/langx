5933f88f541c799acd8f0d1414fd0554c8c20086
bhc <behicsakar@gmail.com>
2024-04-22 20:04:47 +0300

    Add AboutmeCardComponent to profile module

---
+++ src/app/components/profile/aboutme-card/aboutme-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme-card',
+  templateUrl: './aboutme-card.component.html',
+  styleUrls: ['./aboutme-card.component.scss'],
+})
+export class AboutmeCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
