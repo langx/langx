ee0b3ba0cc679ae068389c629d4cd3a79e6beba4
bhc <behicsakar@gmail.com>
2024-04-22 20:30:27 +0300

    Refactor profile module to include LanguagesCardComponent

--- src/app/components/profile/profile.components.module.ts
+++ src/app/components/profile/profile.components.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 // Profile Components
-import { PpCardComponent } from './pp-card/pp-card.component';
-import { OtherPhotosCardComponent } from './other-photos-card/other-photos-card.component';
-import { AboutmeCardComponent } from './aboutme-card/aboutme-card.component';
+import { PpCardComponent } from 'src/app/components/profile/pp-card/pp-card.component';
+import { OtherPhotosCardComponent } from 'src/app/components/profile/other-photos-card/other-photos-card.component';
+import { AboutmeCardComponent } from 'src/app/components/profile/aboutme-card/aboutme-card.component';
+import { LanguagesCardComponent } from 'src/app/components/profile/languages-card/languages-card.component';
 
 // Pipes
-import { AppExtrasModule } from '../../app.extras.module';
+import { AppExtrasModule } from 'src/app/app.extras.module';
 
 @NgModule({
-  declarations: [PpCardComponent, AboutmeCardComponent],
+  declarations: [PpCardComponent, AboutmeCardComponent, LanguagesCardComponent],
   // declarations: [PpCardComponent, OtherPhotosCardComponent],
   imports: [CommonModule, IonicModule, AppExtrasModule],
-  exports: [PpCardComponent, AboutmeCardComponent],
+  exports: [PpCardComponent, AboutmeCardComponent, LanguagesCardComponent],
   // exports: [PpCardComponent, OtherPhotosCardComponent],
 })
 export class ProfileComponentsModule {}
