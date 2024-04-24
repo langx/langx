aa9f487ff2b517173361e00be4dc297c925a661b
bhc <behicsakar@gmail.com>
2024-04-24 23:14:31 +0300

    Refactor OtherPhotosCardComponent in profile module

--- src/app/components/profile/other-photos-card/other-photos-card.component.ts
+++ src/app/components/profile/other-photos-card/other-photos-card.component.ts
@@ -1,6 +1,13 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IonModal, ModalController } from '@ionic/angular';
 import { Observable, forkJoin, of } from 'rxjs';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -11,7 +18,7 @@ import { PreviewPhotoComponent } from 'src/app/components/preview-photo/preview-
   templateUrl: './other-photos-card.component.html',
   styleUrls: ['./other-photos-card.component.scss'],
 })
-export class OtherPhotosCardComponent implements OnInit {
+export class OtherPhotosCardComponent implements OnInit, OnChanges {
   @Input() otherPics: string[];
   @ViewChild(IonModal) modal: IonModal;
 
@@ -26,6 +33,15 @@ export class OtherPhotosCardComponent implements OnInit {
     this.otherPics$ = forkJoin(
       (this.otherPics || []).map((id) => this.userService.getUserFileView(id))
     );
+    console.log('ProfilePage has been initialized');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['otherPics']) {
+      this.otherPics$ = forkJoin(
+        (this.otherPics || []).map((id) => this.userService.getUserFileView(id))
+      );
+    }
   }
 
   async openPreview(photos$: Observable<URL | URL[]>): Promise<void> {
