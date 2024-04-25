c91cd3399ef911dcb910a9579121195e773047b8
bhc <behicsakar@gmail.com>
2024-04-25 11:55:05 +0300

    Refactor profile module to remove OtherPhotosCardComponent and update dependencies

--- src/app/components/profile/profile.components.module.ts
+++ src/app/components/profile/profile.components.module.ts
@@ -4,7 +4,6 @@ import { IonicModule } from '@ionic/angular';
 
 // Profile Components
 import { PpCardComponent } from 'src/app/components/profile/pp-card/pp-card.component';
-import { OtherPhotosCardComponent } from 'src/app/components/profile/other-photos-card/other-photos-card.component';
 import { AboutmeCardComponent } from 'src/app/components/profile/aboutme-card/aboutme-card.component';
 import { LanguagesCardComponent } from 'src/app/components/profile/languages-card/languages-card.component';
 import { BadgesCardComponent } from './badges-card/badges-card.component';
@@ -21,7 +20,6 @@ import { AppExtrasModule } from 'src/app/app.extras.module';
     BadgesCardComponent,
     StreakCardComponent,
   ],
-  // declarations: [PpCardComponent, OtherPhotosCardComponent],
   imports: [CommonModule, IonicModule, AppExtrasModule],
   exports: [
     PpCardComponent,
@@ -30,6 +28,5 @@ import { AppExtrasModule } from 'src/app/app.extras.module';
     BadgesCardComponent,
     StreakCardComponent,
   ],
-  // exports: [PpCardComponent, OtherPhotosCardComponent],
 })
 export class ProfileComponentsModule {}
