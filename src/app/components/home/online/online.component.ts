6948328bd9dcad8cdba51310c77c38386455704c
bhc <behicsakar@gmail.com>
2024-05-08 15:36:36 +0300

    feat: Improve performance and remove unused code in OnlineComponent

--- src/app/components/home/online/online.component.ts
+++ src/app/components/home/online/online.component.ts
@@ -1,14 +1,131 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+
+import { User } from 'src/app/models/User';
+import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
+import { FilterService } from 'src/app/services/filter/filter.service';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  getUsersByLastSeenAction,
+  getUsersByLastSeenWithOffsetAction,
+} from 'src/app/store/actions/users.action';
+import {
+  totalByLastSeenSelector,
+  usersByLastSeenSelector,
+  isLoadingByLastSeenSelector,
+} from 'src/app/store/selectors/user.selector';
 
 @Component({
   selector: 'app-online',
   templateUrl: './online.component.html',
   styleUrls: ['./online.component.scss'],
 })
-export class OnlineComponent  implements OnInit {
+export class OnlineComponent implements OnInit {
+  filter$: any;
+  filterData: FilterDataInterface;
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  currentUser$: Observable<User>;
+  usersByLastSeen$: Observable<User[] | null> = null;
+  totalByLastSeen$: Observable<number | null> = null;
 
-  ngOnInit() {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private filterService: FilterService
+  ) {}
 
+  async ngOnInit() {
+    this.initValues();
+
+    // Check Local Storage for filters
+    await this.checkFilter();
+  }
+
+  initValues(): void {
+    // Set values from selectors
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+
+    this.isLoading$ = this.store.pipe(select(isLoadingByLastSeenSelector));
+    this.usersByLastSeen$ = this.store.pipe(select(usersByLastSeenSelector));
+    this.totalByLastSeen$ = this.store.pipe(select(totalByLastSeenSelector));
+  }
+
+  //
+  // Get Users
+  //
+
+  listUsers() {
+    const filterData = this.filterData;
+    this.store.dispatch(getUsersByLastSeenAction({ request: { filterData } }));
+  }
+
+  //
+  // Check Filter
+  //
+
+  async checkFilter() {
+    this.filter$ = this.filterService
+      .getEvent()
+      .subscribe((filterData: FilterDataInterface) => {
+        this.filterData = filterData;
+        // console.log('Subscribed filter: ', filterData);
+
+        // List Users
+        this.listUsers();
+      });
+  }
+
+  //
+  // Routes
+  //
+
+  getUserPage(id: string): void {
+    this.router.navigate([`/home/user/${id}`]);
+  }
+
+  //
+  // Pull to refresh
+  //
+
+  handleRefresh(event) {
+    this.listUsers();
+    if (event) event.target.complete();
+  }
+
+  //
+  // Infinite Scroll
+  //
+
+  loadMore(event) {
+    // Offset is the number of users already loaded
+    let offset: number = 0;
+    this.usersByLastSeen$
+      .subscribe((users) => {
+        offset = users.length;
+        this.totalByLastSeen$
+          .subscribe((total) => {
+            if (offset < total) {
+              const filterData = this.filterData;
+              this.store.dispatch(
+                getUsersByLastSeenWithOffsetAction({
+                  request: {
+                    filterData,
+                    offset,
+                  },
+                })
+              );
+            } else {
+              console.log('All users loaded');
+            }
+          })
+          .unsubscribe();
+      })
+      .unsubscribe();
+
+    // this.getUsers(this.filterData);
+    event.target.complete();
+  }
 }
