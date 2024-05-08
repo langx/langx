12743148f7d83c496094911b0d79289ac73ed1ae
bhc <behicsakar@gmail.com>
2024-05-08 15:32:39 +0300

    feat: Improve performance and remove unused code in NewComponent
    This commit improves the performance of the NewComponent by removing skeleton loading states and unnecessary infinite scrolling. It also removes unused code for displaying language mates. These changes were made based on recent user commits and repository commits.

--- src/app/components/home/new/new.component.ts
+++ src/app/components/home/new/new.component.ts
@@ -1,14 +1,131 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { User } from 'src/app/models/User';
+import { FilterDataInterface } from 'src/app/models/types/filterData.interface';
+import { FilterService } from 'src/app/services/filter/filter.service';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  getUsersByCreatedAtAction,
+  getUsersByCreatedAtWithOffsetAction,
+} from 'src/app/store/actions/users.action';
+import {
+  isLoadingByCreatedAtSelector,
+  totalByCreatedAtSelector,
+  usersByCreatedAtSelector,
+} from 'src/app/store/selectors/user.selector';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss'],
 })
-export class NewComponent  implements OnInit {
+export class NewComponent implements OnInit {
+  filter$: any;
+  filterData: FilterDataInterface;
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  currentUser$: Observable<User>;
+  usersByCreatedAt$: Observable<User[] | null> = null;
+  totalByCreatedAt$: Observable<number | null> = null;
 
-  ngOnInit() {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private filterService: FilterService
+  ) {}
 
+  async ngOnInit() {
+    this.initValues();
+
+    // Check Local Storage for filters
+    await this.checkFilter();
+  }
+
+  initValues(): void {
+    // Set values from selectors
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+
+    this.isLoading$ = this.store.pipe(select(isLoadingByCreatedAtSelector));
+    this.usersByCreatedAt$ = this.store.pipe(select(usersByCreatedAtSelector));
+    this.totalByCreatedAt$ = this.store.pipe(select(totalByCreatedAtSelector));
+  }
+
+  //
+  // Get Users
+  //
+
+  listUsers() {
+    const filterData = this.filterData;
+    this.store.dispatch(getUsersByCreatedAtAction({ request: { filterData } }));
+  }
+
+  //
+  // Check Filter
+  //
+
+  async checkFilter() {
+    this.filter$ = this.filterService
+      .getEvent()
+      .subscribe((filterData: FilterDataInterface) => {
+        this.filterData = filterData;
+        // console.log('Subscribed filter: ', filterData);
+
+        // List Users
+        this.listUsers();
+      });
+  }
+
+  //
+  // Routes
+  //
+
+  getUserPage(id: string): void {
+    this.router.navigate([`/home/user/${id}`]);
+  }
+
+  //
+  // Pull to refresh
+  //
+
+  handleRefresh(event) {
+    this.listUsers();
+    if (event) event.target.complete();
+  }
+
+  //
+  // Infinite Scroll
+  //
+
+  loadMore(event) {
+    // Offset is the number of users already loaded
+    let offset: number = 0;
+    this.usersByCreatedAt$
+      .subscribe((users) => {
+        offset = users.length;
+        this.totalByCreatedAt$
+          .subscribe((total) => {
+            if (offset < total) {
+              const filterData = this.filterData;
+              this.store.dispatch(
+                getUsersByCreatedAtWithOffsetAction({
+                  request: {
+                    filterData,
+                    offset,
+                  },
+                })
+              );
+            } else {
+              console.log('All users loaded');
+            }
+          })
+          .unsubscribe();
+      })
+      .unsubscribe();
+
+    // this.getUsers(this.filterData);
+    event.target.complete();
+  }
 }
