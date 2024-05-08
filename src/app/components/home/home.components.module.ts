d39f03ae93f3fd41169d3bfd5c016e0986e5c551
bhc <behicsakar@gmail.com>
2024-05-08 15:32:30 +0300

    feat: Add NewComponent to HomeComponentsModule
    This commit adds the NewComponent to the HomeComponentsModule. The NewComponent is responsible for displaying a new feature on the home page. It includes the HTML template, TypeScript component, and necessary imports.
    
    #changelog
    - Added NewComponent to HomeComponentsModule

--- src/app/components/home/home.components.module.ts
+++ src/app/components/home/home.components.module.ts
@@ -10,10 +10,11 @@ import { AppExtrasModule } from 'src/app/app.extras.module';
 // Home Components
 import { TargetLanguageComponent } from './target-language/target-language.component';
 import { EnthusiastComponent } from './enthusiast/enthusiast.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
-  declarations: [TargetLanguageComponent, EnthusiastComponent],
+  declarations: [TargetLanguageComponent, EnthusiastComponent, NewComponent],
   imports: [CommonModule, IonicModule, ComponentsModule, AppExtrasModule],
-  exports: [TargetLanguageComponent, EnthusiastComponent],
+  exports: [TargetLanguageComponent, EnthusiastComponent, NewComponent],
 })
 export class HomeComponentsModule {}
