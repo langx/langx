eeac44404d13c50c15118d1b39269e7fe4bd2260
bhc <behicsakar@gmail.com>
2024-05-08 16:08:55 +0300

    feat: Add VisitorsComponent to home page
    This commit adds the VisitorsComponent to the HomeComponentsModule. The VisitorsComponent is responsible for displaying the visitors section on the home page. It includes the HTML template, TypeScript component, and necessary imports.

--- src/app/components/home/visitors/visitors.component.ts
+++ src/app/components/home/visitors/visitors.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Visit } from 'src/app/models/Visit';
+
+import {
+  isLoadingSelector,
+  totalSelector,
+  visitsSelector,
+} from 'src/app/store/selectors/visits.selector';
 
 @Component({
   selector: 'app-visitors',
   templateUrl: './visitors.component.html',
   styleUrls: ['./visitors.component.scss'],
 })
-export class VisitorsComponent  implements OnInit {
+export class VisitorsComponent implements OnInit {
+  isLoadingVisits$: Observable<boolean>;
+  visits$: Observable<Visit[] | null> = null;
+  totalVisits$: Observable<number | null> = null;
+  model = {
+    icon: 'people-outline',
+    title: 'No Profile Visitors Yet',
+    color: 'warning',
+  };
 
-  constructor() { }
+  constructor(private store: Store, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Visits
+    this.isLoadingVisits$ = this.store.pipe(select(isLoadingSelector));
+    this.visits$ = this.store.pipe(select(visitsSelector));
+    this.totalVisits$ = this.store.pipe(select(totalSelector));
+  }
 
+  getVisitsPage() {
+    this.router.navigateByUrl('/home/visitors');
+  }
 }
