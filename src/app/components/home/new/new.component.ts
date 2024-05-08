278a17adf067fa006b2970053cbf59d7ef88a130
bhc <behicsakar@gmail.com>
2024-05-08 15:23:55 +0300

    feat: Add new component to home page
    This commit adds a new component to the home page. The new component displays a simple paragraph.

---
+++ src/app/components/home/new/new.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.scss'],
+})
+export class NewComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
