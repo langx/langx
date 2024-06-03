8e64c167339305137a90f66774ece99d3f900868
xue <behicsakar@gmail.com>
2024-06-03 05:30:01 +0300

    🚀 : Copilot Instructions Modal Fixes #759

---
+++ src/app/components/copilot-instructions/copilot-instructions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-copilot-instructions',
+  templateUrl: './copilot-instructions.component.html',
+  styleUrls: ['./copilot-instructions.component.scss'],
+})
+export class CopilotInstructionsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
