47633aa3b5915c270d2296941f81dd1739c8a038
xue <behicsakar@gmail.com>
2024-06-06 18:34:04 +0300

    refactor: Update event trigger names in token-baseamount function

--- functions/token-baseamount/src/main.js
+++ functions/token-baseamount/src/main.js
@@ -5,6 +5,17 @@ import { Client, Databases, Query } from 'node-appwrite';
 // + Users Collection:    databases.650750f16cd0c482bb83.collections.65103e2d3a6b4d9494c8.documents.*.update
 // + Streaks Collection:  databases.650750f16cd0c482bb83.collections.65e73985ef5ac00c186b.documents.*.update
 
+/**
+ * @typedef {Object} Token
+ * @property {number} [image]
+ * @property {number} [audio]
+ * @property {number} [text]
+ * @property {number} [onlineMin]
+ * @property {number} [streak]
+ * @property {number} [badges]
+ * @property {number} [baseAmount]
+ */
+
 export default async ({ req, res, log, error }) => {
   // Init SDK
   const client = new Client()
@@ -15,7 +26,6 @@ export default async ({ req, res, log, error }) => {
   const db = new Databases(client);
 
   log('Token Baseamount function called');
-  log(req);
 
   try {
     const tokenDoc = await db.listDocuments(
@@ -25,23 +35,36 @@ export default async ({ req, res, log, error }) => {
     );
 
     log(tokenDoc);
+
+    switch (req.body.$collectionId) {
+      case process.env.USERS_COLLECTION:
+        log('Users Collection Triggered');
+        log(req.body);
+        if (tokenDoc.length === 0) {
+          await db.createDocument(
+            process.env.APP_DATABASE,
+            process.env.TOKEN_COLLECTION,
+            req.body.$id,
+
+            {
+              $id: req.body.$id,
+            }
+          );
+        }
+        log('New document created for user in token collection.');
+        return res.json({ ok: true });
+      case process.env.MESSAGES_COLLECTION:
+        log('Messages Collection Triggered');
+        return res.json({ ok: true });
+      case process.env.STREAKS_COLLECTION:
+        log('Streaks Collection Triggered');
+        return res.json({ ok: true });
+      default:
+        log('Unknown Collection');
+        break;
+    }
   } catch (err) {
     log('Error occurred while searching for user: ', err.message);
     return res.json({ ok: false, error: err.message }, 400);
-  }
-
-  switch (req.body.$collectionId) {
-    case process.env.USERS_COLLECTION:
-      log('Users Collection Triggered');
-      return res.json({ ok: true });
-    case process.env.MESSAGES_COLLECTION:
-      log('Messages Collection Triggered');
-      return res.json({ ok: true });
-    case process.env.STREAKS_COLLECTION:
-      log('Streaks Collection Triggered');
-      return res.json({ ok: true });
-    default:
-      log('Unknown Collection');
-      break;
   }
 };
