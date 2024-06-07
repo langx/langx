e90ca60ea879f57f7cb3dd66f49dd4557cc4410b
xue <behicsakar@gmail.com>
2024-06-07 06:04:55 +0300

    feat: Add lazy loading for images in token details page

--- src/app/pages/home/profile/settings/token-details/token-details.page.ts
+++ src/app/pages/home/profile/settings/token-details/token-details.page.ts
@@ -1,7 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { Browser } from '@capacitor/browser';
+import { ToastController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
+import { User } from 'src/app/models/User';
+import { Checkout } from 'src/app/models/Checkout';
+import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
+import {
+  errorSelector,
+  isLoadingSelector,
+  totalSelector,
+  checkoutsSelector,
+} from 'src/app/store/selectors/checkouts.selector';
+import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
+import {
+  getCheckoutsAction,
+  getCheckoutsWithOffsetAction,
+} from 'src/app/store/actions/checkouts.action';
 
 @Component({
   selector: 'app-token-details',
@@ -13,9 +30,53 @@ export class TokenDetailsPage implements OnInit {
     environment.ext.token.LITEPAPER + '/litepaper/token/distibution#formula';
   twitter = environment.ext.token.TWITTER;
 
-  constructor() {}
+  subscription: Subscription;
 
-  ngOnInit() {}
+  currentUser$: Observable<User | null> = null;
+  isLoading$: Observable<boolean> = null;
+  checkouts$: Observable<Checkout[] | null> = null;
+  total$: Observable<number | null> = null;
+
+  constructor(private store: Store, private toastController: ToastController) {}
+
+  ngOnInit() {
+    this.initValues();
+    // Get all Streaks
+    this.listCheckouts();
+  }
+
+  ionViewWillEnter() {
+    this.subscription = new Subscription();
+
+    // User Errors
+    this.subscription.add(
+      this.store
+        .pipe(select(errorSelector))
+        .subscribe((error: ErrorInterface) => {
+          if (error) {
+            this.presentToast(error.message, 'danger');
+            // TODO: Clear error message if it will be shown
+          }
+        })
+    );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
+  initValues() {
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.checkouts$ = this.store.pipe(select(checkoutsSelector));
+    this.total$ = this.store.pipe(select(totalSelector));
+  }
+
+  listCheckouts() {
+    // Dispatch action to get all visits
+    this.store.dispatch(getCheckoutsAction());
+  }
 
   async openPage(pageURL: any) {
     // console.log(pageURL);
@@ -23,12 +84,59 @@ export class TokenDetailsPage implements OnInit {
   }
 
   //
+  // Infinite Scroll
+  //
+
+  loadMore(event) {
+    // Offset is the number of users already loaded
+    let offset: number = 0;
+    this.checkouts$
+      .subscribe((visits) => {
+        offset = visits.length;
+        this.total$
+          .subscribe((total) => {
+            if (offset < 100) {
+              // console.log('offset', offset);
+              // console.log('total', total);
+              this.store.dispatch(
+                getCheckoutsWithOffsetAction({
+                  request: {
+                    offset,
+                  },
+                })
+              );
+            } else {
+              console.log('All checkouts loaded');
+            }
+          })
+          .unsubscribe();
+      })
+      .unsubscribe();
+    event.target.complete();
+  }
+
+  //
   // Pull to refresh
   //
 
   handleRefresh(event) {
-    // this.listStreaks();
+    this.listCheckouts();
     console.log('refresh token details');
     if (event) event.target.complete();
+  }
+
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1000,
+      position: 'top',
+    });
+
+    await toast.present();
   }
 }
