2e0bf1703b4a4f375cafc0987e42a9157faf341c
xue <behicsakar@gmail.com>
2024-06-07 03:30:51 +0300

    refactor: Update TokenDistributionComponent to use correct image source

--- src/app/pages/home/profile/settings/token-details/token-details.page.ts
+++ src/app/pages/home/profile/settings/token-details/token-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Browser } from '@capacitor/browser';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-token-details',
@@ -6,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token-details.page.scss'],
 })
 export class TokenDetailsPage implements OnInit {
+  infoURL =
+    environment.ext.token.LITEPAPER + '/litepaper/token/distibution#formula';
+  twitter = environment.ext.token.TWITTER;
+
   constructor() {}
 
   ngOnInit() {}
+
+  async openPage(pageURL: any) {
+    // console.log(pageURL);
+    await Browser.open({ url: pageURL });
+  }
+
+  //
+  // Pull to refresh
+  //
+
+  handleRefresh(event) {
+    // this.listStreaks();
+    console.log('refresh token details');
+    if (event) event.target.complete();
+  }
 }
