8ae8c9acd5fb424c7e9ad35e2e90077cb9588e69
xue <behicsakar@gmail.com>
2024-06-07 05:57:19 +0300

    feat: Add StoreModule and EffectsModule for managing checkouts in token details module

--- src/app/pages/home/profile/settings/token-details/token-details.module.ts
+++ src/app/pages/home/profile/settings/token-details/token-details.module.ts
@@ -7,6 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { TokenDetailsPageRoutingModule } from './token-details-routing.module';
 
 import { TokenDetailsPage } from './token-details.page';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { checkoutsReducers } from 'src/app/store/reducers/checkouts.reducer';
+import { CheckoutsEffects } from 'src/app/store/effects/checkouts.effect';
 
 @NgModule({
   imports: [
@@ -14,6 +18,8 @@ import { TokenDetailsPage } from './token-details.page';
     FormsModule,
     IonicModule,
     TokenDetailsPageRoutingModule,
+    StoreModule.forFeature('checkout', checkoutsReducers),
+    EffectsModule.forFeature([CheckoutsEffects]),
   ],
   declarations: [TokenDetailsPage],
 })
