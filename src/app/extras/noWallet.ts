2d3e65ee5d8e77cf941dcfbf31c1957226d0e055
xue <behicsakar@gmail.com>
2024-06-07 04:46:02 +0300

    feat: Add noWallet constant to extras module

---
+++ src/app/extras/noWallet.ts
@@ -0,0 +1,13 @@
+import { Wallet } from '../models/Wallet';
+
+export const noWallet: Wallet = {
+  balance: 0,
+  $id: 'no-wallet',
+  $collectionId: null,
+  $databaseId: null,
+  $createdAt: null,
+  $updatedAt: null,
+  $permissions: null,
+  $owner: null,
+  $version: null,
+};
