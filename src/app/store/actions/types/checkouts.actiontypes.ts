1d95b62066fb40b5818e41161ad965e919cb5289
xue <behicsakar@gmail.com>
2024-06-07 05:22:05 +0300

    feat: Add ActionTypes for managing checkouts

---
+++ src/app/store/actions/types/checkouts.actiontypes.ts
@@ -0,0 +1,8 @@
+export enum ActionTypes {
+  GET_CHECKOUTS = '[Checkouts] Get Checkouts',
+  GET_CHECKOUTS_SUCCESS = '[Checkouts] Get Checkouts Success',
+  GET_CHECKOUTS_FAILURE = '[Checkouts] Get Checkouts Failure',
+  GET_CHECKOUTS_WITH_OFFSET = '[Checkouts] Get Checkouts With Offset',
+  GET_CHECKOUTS_WITH_OFFSET_SUCCESS = '[Checkouts] Get Checkouts With Offset Success',
+  GET_CHECKOUTS_WITH_OFFSET_FAILURE = '[Checkouts] Get Checkouts With Offset Failure',
+}
