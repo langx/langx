526492c8daa88b2f09063968fdcbb2ab2f33dd7b
xue <behicsakar@gmail.com>
2024-06-10 14:04:22 +0300

    refactor: Remove isLoadingSelector and errorSelector from filters.selector.ts 🚀 [Feature] : Move filters to store #265

--- src/app/store/selectors/filters.selector.ts
+++ src/app/store/selectors/filters.selector.ts
@@ -5,17 +5,7 @@ import { FiltersStateInterface } from 'src/app/models/types/states/filtersState.
 export const contributorsFeatureSelector =
   createFeatureSelector<FiltersStateInterface>('filter');
 
-export const isLoadingSelector = createSelector(
-  contributorsFeatureSelector,
-  (filtersState: FiltersStateInterface) => filtersState.isLoading
-);
-
 export const filterDataSelector = createSelector(
   contributorsFeatureSelector,
   (filtersState: FiltersStateInterface) => filtersState.filterData
-);
-
-export const errorSelector = createSelector(
-  contributorsFeatureSelector,
-  (filtersState: FiltersStateInterface) => filtersState.error
 );
