68804f743904c5399d0a669723c4886ba849378f
xue <behicsakar@gmail.com>
2024-06-11 14:47:59 +0300

    Update checkout list to open detailed checkout modal on click

--- src/app/components/checkout-list/checkout-list.component.ts
+++ src/app/components/checkout-list/checkout-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
 
-import { DetailedCheckoutModalComponent } from 'src/app/components//detailed-checkout-modal/detailed-checkout-modal.component';
 import { exactDateAndTime } from 'src/app/extras/utils';
 import { Checkout } from 'src/app/models/Checkout';
 
@@ -13,25 +11,9 @@ import { Checkout } from 'src/app/models/Checkout';
 export class CheckoutListComponent implements OnInit {
   @Input() item: Checkout;
 
-  constructor(private modalController: ModalController) {}
+  constructor() {}
 
   ngOnInit() {}
-
-  async openDetailModal(item: Checkout) {
-    const modal = await this.modalController.create({
-      component: DetailedCheckoutModalComponent,
-      componentProps: {
-        item,
-      },
-      breakpoints: [0, 0.5, 1],
-      initialBreakpoint: 0.5,
-      backdropBreakpoint: 0.5,
-    });
-
-    await modal.present();
-
-    const { data } = await modal.onWillDismiss();
-  }
 
   //
   // Utils
