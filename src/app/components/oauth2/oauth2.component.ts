68c2bfceb3726dd37ca813f851832ca1c0e88425
xue <behicsakar@gmail.com>
2024-06-15 01:45:39 +0300

    refactor: Update OAuth2 component to use OAuth2Service for authentication
    The code changes in the `oauth2.component.ts` file update the implementation of the OAuth2 component to use the `OAuth2Service` for authentication with various providers such as Discord, Google, Facebook, and Apple. This refactor improves the codebase by separating the authentication logic into a dedicated service, making it more modular and maintainable.

--- src/app/components/oauth2/oauth2.component.ts
+++ src/app/components/oauth2/oauth2.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Capacitor } from '@capacitor/core';
-import { BackgroundColor, InAppBrowser, UrlEvent } from '@capgo/inappbrowser';
 
-import { environment } from 'src/environments/environment';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { OAuth2Service } from 'src/app/services/auth/oauth2.service';
 
 @Component({
   selector: 'app-oauth2',
@@ -16,95 +12,23 @@ export class Oauth2Component implements OnInit {
     'user-agent': `Mozilla/5.0 (iPhone; CPU iPhone OS16_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.2 Mobile/15E148 Safari/604.1`,
   };
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private OAuth2Service: OAuth2Service) {}
 
   ngOnInit() {}
 
   signInWithDiscord() {
-    if (Capacitor.getPlatform() === 'web')
-      return this.authService.signInWithDiscord();
-
-    // For mobile
-    InAppBrowser.openWebView({
-      url: `${environment.url.AUTH_PROVIDER_URL}/discord`,
-      headers: this.HEADERS,
-      title: 'Sign in with Discord',
-      backgroundColor: BackgroundColor.WHITE,
-    });
-    this.addBrowserListeners();
+    this.OAuth2Service.signInWithDiscord();
   }
 
   signInWithGoogle() {
-    if (Capacitor.getPlatform() === 'web')
-      return this.authService.signInWithGoogle();
-
-    // For mobile
-    InAppBrowser.openWebView({
-      url: `${environment.url.AUTH_PROVIDER_URL}/google`,
-      headers: this.HEADERS,
-      title: 'Sign in with Google',
-      backgroundColor: BackgroundColor.WHITE,
-    });
-    this.addBrowserListeners();
+    this.OAuth2Service.signInWithGoogle();
   }
 
   signInWithFacebook() {
-    if (Capacitor.getPlatform() === 'web')
-      return this.authService.signInWithFacebook();
-
-    // For mobile
-    InAppBrowser.openWebView({
-      url: `${environment.url.AUTH_PROVIDER_URL}/facebook`,
-      headers: this.HEADERS,
-      title: 'Sign in with Facebook',
-      backgroundColor: BackgroundColor.WHITE,
-    });
-    this.addBrowserListeners();
+    this.OAuth2Service.signInWithFacebook();
   }
 
   signInWithApple() {
-    if (Capacitor.getPlatform() === 'web')
-      return this.authService.signInWithApple();
-
-    // For mobile
-    InAppBrowser.openWebView({
-      url: `${environment.url.AUTH_PROVIDER_URL}/apple`,
-      headers: this.HEADERS,
-      title: 'Sign in with Apple',
-      backgroundColor: BackgroundColor.WHITE,
-    });
-    this.addBrowserListeners();
-  }
-
-  addBrowserListeners() {
-    // console.log('InAppBrowser listeners started');
-
-    // urlChangeEvent
-    InAppBrowser.addListener('urlChangeEvent', (res: UrlEvent) => {
-      if (res.url.startsWith(environment.url.SUCCESS_OAUTH2)) {
-        // Remove delete
-        InAppBrowser.close();
-
-        const redirectUrl = res.url.split(environment.url.HOMEPAGE_URL)[1];
-        // console.log('redirectUrl', redirectUrl);
-        this.router.navigateByUrl(redirectUrl);
-      }
-      if (res.url.startsWith(environment.url.FAILURE_OAUTH2)) {
-        // Remove delete
-        InAppBrowser.close();
-
-        const redirectUrl = res.url.split(environment.url.HOMEPAGE_URL)[1];
-        // console.log('redirectUrl', redirectUrl);
-        this.router.navigateByUrl(redirectUrl);
-      }
-    });
-
-    // Close event
-    InAppBrowser.addListener('closeEvent', () => {
-      // console.log('InAppBrowser closed');
-      // Remove delete
-      InAppBrowser.removeAllListeners();
-      // console.log('all listeners removed');
-    });
+    this.OAuth2Service.signInWithApple();
   }
 }
