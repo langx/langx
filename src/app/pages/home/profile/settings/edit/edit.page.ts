5a7abbcf4adec5a2b959eea8fa4a401238a239e2
xue <behicsakar@gmail.com>
2024-06-14 16:37:41 +0300

    fix: typo

--- src/app/pages/home/profile/settings/edit/edit.page.ts
+++ src/app/pages/home/profile/settings/edit/edit.page.ts
@@ -393,7 +393,7 @@ export class EditPage implements OnInit {
         motherLanguage: true,
       };
 
-      // If it length is 6, then don't let the user to add one more study language.
+      // If it length is 6, then don't let the user to add one more mother language.
       if (this.currentUser.motherLanguages.length >= 5) {
         this.presentToast(
           'You can add max 5 Study Languages. Please remove at least one and try again.',
