4e0f48fb83b7679ee43bd66ef28c9b779c747047
xue <behicsakar@gmail.com>
2024-06-14 16:36:10 +0300

    fix: max number of languages limited to 5

--- src/app/pages/home/profile/settings/edit/edit.page.ts
+++ src/app/pages/home/profile/settings/edit/edit.page.ts
@@ -341,7 +341,7 @@ export class EditPage implements OnInit {
       };
 
       // If it length is 6, then don't let the user to add one more study language.
-      if (this.currentUser.studyLanguages.length > 5) {
+      if (this.currentUser.studyLanguages.length >= 5) {
         this.presentToast(
           'You can add max 5 Study Languages. Please remove at least one and try again.',
           'danger'
@@ -394,7 +394,7 @@ export class EditPage implements OnInit {
       };
 
       // If it length is 6, then don't let the user to add one more study language.
-      if (this.currentUser.motherLanguages.length > 5) {
+      if (this.currentUser.motherLanguages.length >= 5) {
         this.presentToast(
           'You can add max 5 Study Languages. Please remove at least one and try again.',
           'danger'
