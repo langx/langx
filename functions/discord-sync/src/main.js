5ee29d769637c0dc48f2a71fefa9c2eeb6bb6662
xue <behicsakar@gmail.com>
2024-06-15 18:38:38 +0300

    feat: Add user role synchronization in discord-sync function

--- functions/discord-sync/src/main.js
+++ functions/discord-sync/src/main.js
@@ -2,6 +2,15 @@ import { Client, Databases, Account } from 'node-appwrite';
 
 // execute = ["users"]
 
+const roles = {
+  moderator: '1211342347552563251',
+  'early-adopter': '1212395552193515551',
+  sponsor: '1222464830028386394',
+  creator: '1232679338969792573',
+  fundamental: '1223726462494838805',
+  pioneer: '1220823209381728348',
+};
+
 export default async ({ req, res, log, error }) => {
   // Init SDK
   const client = new Client()
@@ -10,13 +19,13 @@ export default async ({ req, res, log, error }) => {
     .setJWT(req.headers['x-appwrite-user-jwt'] || '');
 
   const account = new Account(client);
-  // const db = new Databases(client);
+  const db = new Databases(client);
 
-  // const userDoc = await db.getDocument(
-  //   process.env.APP_DATABASE,
-  //   process.env.USERS_COLLECTION,
-  //   req.body.to
-  // );
+  const userDoc = await db.getDocument(
+    process.env.APP_DATABASE,
+    process.env.USERS_COLLECTION,
+    req.headers['x-appwrite-user-id']
+  );
 
   // const admin = new Client()
   //   .setEndpoint(process.env.APP_ENDPOINT)
@@ -56,7 +65,19 @@ export default async ({ req, res, log, error }) => {
       log(memberData);
       const userRoles = memberData['roles'];
 
-      log(`User roles: ${userRoles}`);
+      log(`User roleIds: ${userRoles}`);
+
+      const userRolesNames = userRoles
+        .map((roleId) => {
+          const roleName = Object.keys(roles).find(
+            (name) => roles[name] === roleId
+          );
+          return roleName;
+        })
+        .filter(Boolean);
+
+      log(`User roles: ${userRolesNames.join(', ')}`);
+      log(`User badges ${userDoc.badges.join(', ')}`);
 
       return res.json({ newBadges: [] });
     } else {
