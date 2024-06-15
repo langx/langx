2bb75158e103ed00fbaa0b0e5b136dd3dc5ce297
xue <behicsakar@gmail.com>
2024-06-15 16:01:34 +0300

    refactor: Remove unused code in discord-sync function

--- functions/discord-sync/src/main.js
+++ functions/discord-sync/src/main.js
@@ -9,21 +9,19 @@ export default async ({ req, res, log, error }) => {
     .setProject(process.env.APP_PROJECT)
     .setJWT(req.headers['x-appwrite-user-jwt'] || '');
 
-  const admin = new Client()
-    .setEndpoint(process.env.APP_ENDPOINT)
-    .setProject(process.env.APP_PROJECT)
-    .setKey(process.env.APP_KEY);
-
-  const db = new Databases(client);
   const account = new Account(client);
-
-  // const accountDoc = await account.get();
+  // const db = new Databases(client);
 
   // const userDoc = await db.getDocument(
   //   process.env.APP_DATABASE,
   //   process.env.USERS_COLLECTION,
   //   req.body.to
   // );
+
+  // const admin = new Client()
+  //   .setEndpoint(process.env.APP_ENDPOINT)
+  //   .setProject(process.env.APP_PROJECT)
+  //   .setKey(process.env.APP_KEY);
 
   try {
     const identities = await account.listIdentities();
