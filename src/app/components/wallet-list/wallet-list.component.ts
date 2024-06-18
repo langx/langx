7ecc42a221e36696d9655287055607d016fd27e0
xue <behicsakar@gmail.com>
2024-06-18 05:48:39 +0300

    feat: Add wallet-list component

---
+++ src/app/components/wallet-list/wallet-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-list',
+  templateUrl: './wallet-list.component.html',
+  styleUrls: ['./wallet-list.component.scss'],
+})
+export class WalletListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
