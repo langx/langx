b02d1177ecf33de13c83674d810c25b68263fd0e
xue <behicsakar@gmail.com>
2024-06-18 04:08:45 +0300

    refactor: Update token-leaderboard page UI and functionality

--- src/app/pages/home/profile/settings/token-leaderboard/token-leaderboard.page.ts
+++ src/app/pages/home/profile/settings/token-leaderboard/token-leaderboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Browser } from '@capacitor/browser';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-token-leaderboard',
@@ -6,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token-leaderboard.page.scss'],
 })
 export class TokenLeaderboardPage implements OnInit {
+  infoURL =
+    environment.ext.token.LITEPAPER + '/litepaper/token/distibution#formula';
+  twitter = environment.ext.token.TWITTER;
+
   constructor() {}
 
   ngOnInit() {}
+
+  listWallets() {
+    // Dispatch action to get all visits
+    // this.store.dispatch(getStreaksAction());
+  }
+
+  //
+  // Pull to refresh
+  //
+
+  handleRefresh(event) {
+    this.listWallets();
+    if (event) event.target.complete();
+  }
+
+  //
+  // Utils
+  //
+
+  async openPage(pageURL: any) {
+    // console.log(pageURL);
+    await Browser.open({ url: pageURL });
+  }
 }
