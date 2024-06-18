4ae84aab7ab5b1f719e9df252b34ffc11cfc8a02
xue <behicsakar@gmail.com>
2024-06-18 04:49:24 +0300

    refactor: Add list wallets functionality and error handling

--- src/app/store/actions/types/wallet.actiontypes.ts
+++ src/app/store/actions/types/wallet.actiontypes.ts
@@ -2,4 +2,10 @@ export enum ActionTypes {
   GET_WALLET = '[Wallet] Get Wallet',
   GET_WALLET_SUCCESS = '[Wallet] Get Wallet Success',
   GET_WALLET_FAILURE = '[Wallet] Get Wallet Failure',
+
+  LIST_WALLETS = '[Wallet] List Wallets',
+  LIST_WALLETS_SUCCESS = '[Wallet] List Wallets Success',
+  LIST_WALLETS_FAILURE = '[Wallet] List Wallets Failure',
+
+  CLEAR_ERRORS = '[Wallet] Clear Errors',
 }
