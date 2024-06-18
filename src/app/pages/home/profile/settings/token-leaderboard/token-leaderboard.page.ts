f1696ef3d4cfc542d9130925f9a060151a915c8a
xue <behicsakar@gmail.com>
2024-06-18 04:14:32 +0300

    refactor: Update token-leaderboard page UI and functionality

--- src/app/pages/home/profile/settings/token-leaderboard/token-leaderboard.page.ts
+++ src/app/pages/home/profile/settings/token-leaderboard/token-leaderboard.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Browser } from '@capacitor/browser';
+import { Observable, Subscription } from 'rxjs';
+import { ToastController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
 
 import { environment } from 'src/environments/environment';
+import { Wallet } from 'src/app/models/Wallet';
 
 @Component({
   selector: 'app-token-leaderboard',
@@ -13,9 +17,46 @@ export class TokenLeaderboardPage implements OnInit {
     environment.ext.token.LITEPAPER + '/litepaper/token/distibution#formula';
   twitter = environment.ext.token.TWITTER;
 
-  constructor() {}
+  subscription: Subscription;
 
-  ngOnInit() {}
+  isLoading$: Observable<boolean> = null;
+  wallets$: Observable<Wallet[] | null> = null;
+  total$: Observable<number | null> = null;
+
+  constructor(private store: Store, private toastController: ToastController) {}
+
+  ngOnInit() {
+    this.initValues();
+    // Get all Streaks
+    this.listWallets();
+  }
+
+  ionViewWillEnter() {
+    this.subscription = new Subscription();
+
+    // User Errors
+    // this.subscription.add(
+    //   this.store
+    //     .pipe(select(errorSelector))
+    //     .subscribe((error: ErrorInterface) => {
+    //       if (error) {
+    //         this.presentToast(error.message, 'danger');
+    //         this.store.dispatch(clearErrorsAction());
+    //       }
+    //     })
+    // );
+  }
+
+  ionViewWillLeave() {
+    // Unsubscribe from all subscriptions
+    this.subscription.unsubscribe();
+  }
+
+  initValues() {
+    // this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    // this.streaks$ = this.store.pipe(select(streaksSelector));
+    // this.total$ = this.store.pipe(select(totalSelector));
+  }
 
   listWallets() {
     // Dispatch action to get all visits
@@ -38,5 +79,20 @@ export class TokenLeaderboardPage implements OnInit {
   async openPage(pageURL: any) {
     // console.log(pageURL);
     await Browser.open({ url: pageURL });
+  }
+
+  //
+  // Present Toast
+  //
+
+  async presentToast(msg: string, color?: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: color || 'primary',
+      duration: 1000,
+      position: 'top',
+    });
+
+    await toast.present();
   }
 }
