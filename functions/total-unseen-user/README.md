ea039f396184222819e603ce35eebc56016ca064
xue <behicsakar@gmail.com>
2024-06-19 12:05:28 +0300

    feat: Add total-unseen-user function for tracking unseen user updates

---
+++ functions/total-unseen-user/README.md
@@ -0,0 +1,1 @@
+# total-unseen-user
---
+++ functions/total-unseen-user/src/main.js
@@ -0,0 +1,62 @@
+import { Client, Databases, Query } from 'node-appwrite';
+
+// "events": [
+//   "databases.650750f16cd0c482bb83.collections.65075108a4025a4f5bd7.documents.*.create",
+//   "databases.650750f16cd0c482bb83.collections.65075108a4025a4f5bd7.documents.*.update"
+// ],
+
+export default async ({ req, res, log, error }) => {
+  // Init SDK
+  const client = new Client()
+    .setEndpoint(process.env.APP_ENDPOINT)
+    .setProject(process.env.APP_PROJECT)
+    .setKey(process.env.API_KEY);
+
+  const db = new Databases(client);
+
+  try {
+    const listMessages = await db.listDocuments(
+      process.env.APP_DATABASE,
+      process.env.MESSAGES_COLLECTION,
+      [
+        Query.equal('roomId', req.body.roomId.$id),
+        Query.orderDesc('$createdAt'),
+      ]
+    );
+
+    log(`Request: ${JSON.stringify(req.body)}`);
+    log(`List Messages: ${JSON.stringify(listMessages)}`);
+
+    // define unseen
+    let unseen = [0, 0];
+
+    for (const message of listMessages.documents) {
+      if (!message.seen) {
+        if (message.to > message.sender) {
+          unseen[0] += 1;
+        } else {
+          unseen[1] += 1;
+        }
+      }
+    }
+
+    log(`Before: ${JSON.stringify(req.body.roomId.unseen)}, After: ${unseen}`);
+    if (unseen.toString() !== req.body.roomId.unseen.toString()) {
+      log('Updating unseen');
+      const updatedRoom = await db.updateDocument(
+        process.env.APP_DATABASE,
+        process.env.ROOMS_COLLECTION,
+        req.body.roomId.$id,
+        {
+          unseen: unseen,
+        }
+      );
+      log(`Room Updated: ${JSON.stringify(updatedRoom)}`);
+    }
+
+    return res.json({ ok: true });
+  } catch (err) {
+    log(`Error: ${err.message}`);
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+};
