cb9001168664d66b2c2eba3f5417a34bfc7c55de
xue <behicsakar@gmail.com>
2024-06-21 04:13:19 +0300

    refactor: Update getRoomsAction and getRoomsWithOffsetAction props
    This commit updates the props of the getRoomsAction and getRoomsWithOffsetAction in the rooms.action.ts file. The request object now includes an optional "archived" property for filtering archived rooms. This change improves the functionality of retrieving rooms and rooms with offset.

--- src/app/store/actions/rooms.action.ts
+++ src/app/store/actions/rooms.action.ts
@@ -5,10 +5,7 @@ import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { listRoomsResponseInterface } from 'src/app/models/types/responses/listRoomsResponse.interface';
 
 // Get Rooms Actions
-export const getRoomsAction = createAction(
-  ActionTypes.GET_ROOMS,
-  props<{ request?: { archived?: boolean } }>()
-);
+export const getRoomsAction = createAction(ActionTypes.GET_ROOMS);
 
 export const getRoomsSuccessAction = createAction(
   ActionTypes.GET_ROOMS_SUCCESS,
@@ -23,7 +20,7 @@ export const getRoomsFailureAction = createAction(
 // Get Rooms With Offset Actions
 export const getRoomsWithOffsetAction = createAction(
   ActionTypes.GET_ROOMS_WITH_OFFSET,
-  props<{ request: { offset: number; archived?: boolean } }>()
+  props<{ request: { offset: number } }>()
 );
 
 export const getRoomsWithOffsetSuccessAction = createAction(
@@ -33,5 +30,36 @@ export const getRoomsWithOffsetSuccessAction = createAction(
 
 export const getRoomsWithOffsetFailureAction = createAction(
   ActionTypes.GET_ROOMS_WITH_OFFSET_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
+
+// Get Archived Rooms Actions
+export const getArchivedRoomsAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS
+);
+
+export const getArchivedRoomsSuccessAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS_SUCCESS,
+  props<{ payload: listRoomsResponseInterface }>()
+);
+
+export const getArchivedRoomsFailureAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS_FAILURE,
+  props<{ error: ErrorInterface }>()
+);
+
+// Get Archived Rooms With Offset Actions
+export const getArchivedRoomsWithOffsetAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS_WITH_OFFSET,
+  props<{ request: { offset: number } }>()
+);
+
+export const getArchivedRoomsWithOffsetSuccessAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS_WITH_OFFSET_SUCCESS,
+  props<{ payload: listRoomsResponseInterface }>()
+);
+
+export const getArchivedRoomsWithOffsetFailureAction = createAction(
+  ActionTypes.GET_ARCHIVED_ROOMS_WITH_OFFSET_FAILURE,
   props<{ error: ErrorInterface }>()
 );
