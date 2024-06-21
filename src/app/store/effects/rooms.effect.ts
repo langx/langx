b9c0e0f0ecebf9a4c4791792567528dbaca509ff
xue <behicsakar@gmail.com>
2024-06-21 04:33:16 +0300

    refactor: Update listRooms method to support filtering archived rooms

--- src/app/store/effects/rooms.effect.ts
+++ src/app/store/effects/rooms.effect.ts
@@ -9,6 +9,12 @@ import { ErrorInterface } from 'src/app/models/types/errors/error.interface';
 import { listRoomsResponseInterface } from 'src/app/models/types/responses/listRoomsResponse.interface';
 import { currentUserSelector } from 'src/app/store/selectors/auth.selector';
 import {
+  getArchivedRoomsAction,
+  getArchivedRoomsFailureAction,
+  getArchivedRoomsSuccessAction,
+  getArchivedRoomsWithOffsetAction,
+  getArchivedRoomsWithOffsetFailureAction,
+  getArchivedRoomsWithOffsetSuccessAction,
   getRoomsAction,
   getRoomsFailureAction,
   getRoomsSuccessAction,
@@ -60,6 +66,50 @@ export class RoomsEffects {
                 message: errorResponse.message,
               };
               return of(getRoomsWithOffsetFailureAction({ error }));
+            })
+          )
+      )
+    )
+  );
+
+  getArchivedRooms$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getArchivedRoomsAction),
+      withLatestFrom(this.store.pipe(select(currentUserSelector))),
+      switchMap(([_, currentUser]) =>
+        this.roomService.listRooms(currentUser, { archived: true }).pipe(
+          map((payload: listRoomsResponseInterface) => {
+            return getArchivedRoomsSuccessAction({ payload });
+          }),
+
+          catchError((errorResponse: HttpErrorResponse) => {
+            const error: ErrorInterface = {
+              message: errorResponse.message,
+            };
+            return of(getArchivedRoomsFailureAction({ error }));
+          })
+        )
+      )
+    )
+  );
+
+  getArchivedRoomsWithOffset$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getArchivedRoomsWithOffsetAction),
+      withLatestFrom(this.store.pipe(select(currentUserSelector))),
+      switchMap(([{ request }, currentUser]) =>
+        this.roomService
+          .listRooms(currentUser, { archived: true, offset: request.offset })
+          .pipe(
+            map((payload: listRoomsResponseInterface) => {
+              return getArchivedRoomsWithOffsetSuccessAction({ payload });
+            }),
+
+            catchError((errorResponse: HttpErrorResponse) => {
+              const error: ErrorInterface = {
+                message: errorResponse.message,
+              };
+              return of(getArchivedRoomsWithOffsetFailureAction({ error }));
             })
           )
       )
