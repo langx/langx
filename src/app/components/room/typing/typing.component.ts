58bb7f2256fbfe4844069d080c1fdc158fd4365a
xue <behicsakar@gmail.com>
2024-06-23 03:24:23 +0300

    feat: Add typing component to room
    The code changes include adding a new typing component to the room feature. This component consists of an HTML template, TypeScript file, and a corresponding spec file. The component is responsible for displaying the typing status of the user in the chat page.

---
+++ src/app/components/room/typing/typing.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typing',
+  templateUrl: './typing.component.html',
+  styleUrls: ['./typing.component.scss'],
+})
+export class TypingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
