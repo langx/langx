7f90de248276e3dc6101fb8df04b032ba041e6cb
xue <behicsakar@gmail.com>
2024-06-26 04:38:57 +0300

    refactor: Update file permission function to include permission check for room query in total-unseen-user function

--- functions/update-file-permission/src/main.js
+++ functions/update-file-permission/src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from 'node-appwrite';
+import { Client, Storage, Permission, Role } from 'node-appwrite';
 
 import { throwIfMissing } from './utils.js';
 
@@ -15,7 +15,14 @@ export default async ({ req, res, log, error }) => {
     .setProject(process.env.APP_PROJECT)
     .setKey(process.env.API_KEY);
 
-  const db = new Databases(client);
+  const storage = new Storage(client);
+
+  // const result = await storage.updateFile(
+  //     '<BUCKET_ID>', // bucketId
+  //     '<FILE_ID>', // fileId
+  //     '<NAME>', // name (optional)
+  //     ["read("any")"] // permissions (optional)
+  // );
 
   log('update-file-permission');
 
@@ -24,16 +31,42 @@ export default async ({ req, res, log, error }) => {
     const requestBody = JSON.parse(req.body);
     throwIfMissing(requestBody, ['to', 'fileId', 'type']);
 
-    return res.json({ ok: true });
-  } catch (err) {
-    return res.json({ ok: false, error: err.message }, 400);
-  }
+    const senderId = req.headers['x-appwrite-user-id'];
 
-  try {
-    log(req.body);
+    if (requestBody.type !== 'image' && requestBody.type !== 'audio') {
+      return res.json({ ok: false, error: 'type is not valid' }, 400);
+    } else if (requestBody.type === 'image') {
+      log('type: image');
+      // log(process.env.MESSAGE_BUCKET);
+      // log(requestBody.fileId);
+      // log(senderId);
+      // log(requestBody.to);
+      const updatedFileImage = await storage.updateFile(
+        process.env.MESSAGE_BUCKET,
+        requestBody.fileId,
+        undefined,
+        [
+          Permission.read(Role.user(requestBody.to)),
+          Permission.read(Role.user(senderId)),
+        ]
+      );
+      log(updatedFileImage.$id);
+    } else if (requestBody.type === 'audio') {
+      log('type: audio');
+      const updatedFileAudio = await storage.updateFile(
+        process.env.AUDIO_BUCKET,
+        requestBody.fileId,
+        undefined,
+        [
+          Permission.read(Role.user(requestBody.to)),
+          Permission.read(Role.user(senderId)),
+        ]
+      );
+      log(updatedFileAudio.$id);
+    }
+
     return res.json({ ok: true });
   } catch (err) {
-    log('Error occurred while searching for user: ', err.message);
     return res.json({ ok: false, error: err.message }, 400);
   }
 };
