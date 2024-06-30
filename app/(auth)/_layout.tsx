577c3fe2f9c71f5e41dcb85c6604c0fb842f2acf
xue <behicsakar@gmail.com>
2024-06-30 21:55:09 +0300

    feat: Add authentication layout and login/register components

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+
+const AuthLayout = () => {
+  return (
+    <ThemedView>
+      <ThemedText>AuthLayout</ThemedText>
+    </ThemedView>
+  );
+};
+
+export default AuthLayout;
