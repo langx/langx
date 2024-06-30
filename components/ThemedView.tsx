b1eb6f19d832dbad41e207187ad2a9882edcdaaf
xue <behicsakar@gmail.com>
2024-06-30 20:43:35 +0300

    chore: Refactor ThemedText and ThemedView components

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,33 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+import { styled } from "nativewind";
+const StyledView = styled(View);
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  className?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({
+  style,
+  lightColor,
+  darkColor,
+  className,
+  ...otherProps
+}: ThemedViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <StyledView
+      className={className ? className : ""}
+      style={[{ backgroundColor }, style]}
+      {...otherProps}
+    />
+  );
 }
