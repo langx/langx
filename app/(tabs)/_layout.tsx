857b4df8c4676812dbf9b41bbf4744132a287eab
xue <behicsakar@gmail.com>
2024-07-01 00:50:05 +0300

    refactor: Update tab screens and icons for better user experience

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,44 @@
-import { Tabs } from "expo-router";
 import React from "react";
+import { Tabs } from "expo-router";
+import { Image } from "react-native";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedView } from "@/components/ThemedView";
+
+import { styled } from "nativewind";
+const StyledImage = styled(Image);
+
+import icons from "@/constants/icons";
+import { ThemedText } from "@/components/ThemedText";
+
+type TabIconProps = {
+  icon: any;
+  color: string;
+  name: string;
+  focused: boolean;
+};
+
+const TabIcon: React.FC<TabIconProps> = ({ icon, color, name, focused }) => {
+  return (
+    <>
+      <ThemedView>
+        <StyledImage
+          source={icon}
+          resizeMode="contain"
+          tintColor={color}
+          className="w-6 h-6"
+        />
+        <ThemedText
+          className={`${focused ? "font-csemibold" : "font-cregular"} text-xs`}
+        >
+          {name}
+        </ThemedText>
+      </ThemedView>
+    </>
+  );
+};
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,6 +48,7 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
+        tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
@@ -20,15 +56,21 @@ export default function TabLayout() {
         options={{
           title: "Chats",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "chatbubbles" : "chatbubbles-outline"}
+            <TabIcon
+              icon={icons.home}
               color={color}
+              name="Chats"
+              focused={focused}
             />
+            // <TabBarIcon
+            //   name={focused ? "chatbubbles" : "chatbubbles-outline"}
+            //   color={color}
+            // />
           ),
         }}
       />
       <Tabs.Screen
-        name="community"
+        name="index"
         options={{
           title: "Community",
           tabBarIcon: ({ color, focused }) => (
