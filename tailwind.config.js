1c14bf06af816a4dee2a0e17132f7f5d58817271
xue <behicsakar@gmail.com>
2024-06-30 20:43:12 +0300

    chore: Update Tailwind CSS configuration for improved file matching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./App/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
