e1c12e1cee34ad05d75a49b7dce0cdb1b1911813
xue <behicsakar@gmail.com>
2024-06-30 20:51:43 +0300

    chore: Update Tailwind CSS configuration for improved file matching and add new color and font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#161622",
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Comfortaa", "sans-serif"],
+        pextralight: ["Comfortaa", "sans-serif"],
+        plight: ["Comfortaa", "sans-serif"],
+        pregular: ["Comfortaa", "sans-serif"],
+        pmedium: ["Comfortaa", "sans-serif"],
+        psemibold: ["Comfortaa", "sans-serif"],
+        pbold: ["Comfortaa", "sans-serif"],
+        pextrabold: ["Comfortaa", "sans-serif"],
+        pblack: ["Comfortaa", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
