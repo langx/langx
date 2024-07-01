ed0a5ab84b095addcd26846f017a9a5103723888
xue <behicsakar@gmail.com>
2024-07-02 01:36:28 +0300

    refactor: Update _layout.tsx to hide header for certain screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,7 +49,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
