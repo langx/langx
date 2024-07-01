81ef96b1420c1f3a866887ca0bb80a7c5429a9d2
xue <behicsakar@gmail.com>
2024-07-02 02:36:58 +0300

    feat: Update _layout.tsx to use centralized font loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,24 +17,11 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+import { fonts } from "@/constants/fonts";
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [fontsLoaded, error] = useFonts({
-    "Comfortaa-Light": require("../assets/fonts/Comfortaa/Comfortaa-Light.ttf"),
-    "Comfortaa-Regular": require("../assets/fonts/Comfortaa/Comfortaa-Regular.ttf"),
-    "Comfortaa-Medium": require("../assets/fonts/Comfortaa/Comfortaa-Medium.ttf"),
-    "Comfortaa-SemiBold": require("../assets/fonts/Comfortaa/Comfortaa-SemiBold.ttf"),
-    "Comfortaa-Bold": require("../assets/fonts/Comfortaa/Comfortaa-Bold.ttf"),
-    "Lexend-Thin": require("../assets/fonts/Lexend/Lexend-Thin.ttf"),
-    "Lexend-ExtraLight": require("../assets/fonts/Lexend/Lexend-ExtraLight.ttf"),
-    "Lexend-Light": require("../assets/fonts/Lexend/Lexend-Light.ttf"),
-    "Lexend-Regular": require("../assets/fonts/Lexend/Lexend-Regular.ttf"),
-    "Lexend-Medium": require("../assets/fonts/Lexend/Lexend-Medium.ttf"),
-    "Lexend-SemiBold": require("../assets/fonts/Lexend/Lexend-SemiBold.ttf"),
-    "Lexend-Bold": require("../assets/fonts/Lexend/Lexend-Bold.ttf"),
-    "Lexend-ExtraBold": require("../assets/fonts/Lexend/Lexend-ExtraBold.ttf"),
-    "Lexend-Black": require("../assets/fonts/Lexend/Lexend-Black.ttf"),
-  });
+  const [fontsLoaded, error] = useFonts(fonts);
 
   useEffect(() => {
     if (fontsLoaded) {
---
+++ constants/fonts.ts
@@ -0,0 +1,19 @@
+// fonts.ts
+type FontMap = { [key: string]: ReturnType<typeof require> };
+
+const fonts: FontMap = {
+  // Comfortaa
+  "Comfortaa-Light": require("./assets/fonts/Comfortaa/Comfortaa-Light.ttf"),
+  "Comfortaa-Regular": require("./assets/fonts/Comfortaa/Comfortaa-Regular.ttf"),
+  "Comfortaa-Medium": require("./assets/fonts/Comfortaa/Comfortaa-Medium.ttf"),
+  "Comfortaa-SemiBold": require("./assets/fonts/Comfortaa/Comfortaa-SemiBold.ttf"),
+  "Comfortaa-Bold": require("./assets/fonts/Comfortaa/Comfortaa-Bold.ttf"),
+  // Lexend
+  "Lexend-Light": require("./assets/fonts/Lexend/Lexend-Light.ttf"),
+  "Lexend-Regular": require("./assets/fonts/Lexend/Lexend-Regular.ttf"),
+  "Lexend-Medium": require("./assets/fonts/Lexend/Lexend-Medium.ttf"),
+  "Lexend-SemiBold": require("./assets/fonts/Lexend/Lexend-SemiBold.ttf"),
+  "Lexend-Bold": require("./assets/fonts/Lexend/Lexend-Bold.ttf"),
+};
+
+export { fonts };
