62b0b48cd57a64efb6dbd4b3e34706b9c4fbcf81
xue <behicsakar@gmail.com>
2024-07-01 05:32:30 +0300

    refactor: Add nativewind preset to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
