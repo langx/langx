57cfece40915156a94ae5418733691205c992163
xue <behicsakar@gmail.com>
2024-07-02 15:35:51 +0300

    refactor: Update images.js to use separate logo files for light and dark themes

--- constants/images.js
+++ constants/images.js
@@ -2,8 +2,18 @@ import profile from "../assets/images/profile.png";
 import thumbnail from "../assets/images/thumbnail.png";
 import cards from "../assets/images/cards.png";
 import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
+import logo_light from "../assets/images/logo-light.png";
+import logo_dark from "../assets/images/logo-dark.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default {
+  profile,
+  thumbnail,
+  cards,
+  path,
+  logo_light,
+  logo_dark,
+  logoSmall,
+  empty,
+};
