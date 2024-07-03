1a64cf8ac9c66882a78a812115980beb0438467c
xue <behicsakar@gmail.com>
2024-07-03 19:13:36 +0300

    refactor: Add Appwrite integration for user authentication and account management

---
+++ services/appwrite.tsx
@@ -0,0 +1,117 @@
+import { config } from "dotenv";
+config();
+
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+} from "react-native-appwrite";
+
+export const appwriteConfig = {
+  endpoint: process.env.APP_ENDPOINT,
+  platform: process.env.APP_PACKAGE_NAME,
+  projectId: process.env.APP_PROJECT,
+  databaseId: process.env.APP_DATABASE,
+  usersCollection: process.env.USERS_COLLECTION,
+};
+
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+// Register user
+export async function createUser(email, password, username) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollection,
+      newAccount.$id,
+      {
+        email: email,
+        username: username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Sign In
+export async function signIn(email, password) {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get Account
+export async function getAccount() {
+  try {
+    const currentAccount = await account.get();
+
+    return currentAccount;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get Current User
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await getAccount();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollection,
+      [Query.equal("$id", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Sign Out
+export async function signOut() {
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
