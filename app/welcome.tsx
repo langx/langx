6eb635e619ba7163db991944defec2503e0e4cb6
xue <behicsakar@gmail.com>
2024-07-03 15:22:52 +0300

    refactor: Remove unused import and update image source in welcome.tsx

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Image, StyleSheet, useColorScheme } from "react-native";
 
-import { Colors } from "@/constants/Colors";
 import images from "@/constants/images";
 
 import { ThemedText } from "@/components/atomic/ThemedText";
