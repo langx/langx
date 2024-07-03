f830388adc9b40720de81faa66ab33ef244ea5b8
xue <behicsakar@gmail.com>
2024-07-03 18:18:27 +0300

    refactor: Create ThemedFormField component for reusable themed form fields

---
+++ components/molecular/ThemedFormField.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { TextInput, Image, Pressable } from "react-native";
+
+import icons from "@/constants/icons";
+
+import { ThemedView } from "@/components/atomic/ThemedView";
+import { ThemedText } from "@/components/atomic/ThemedText";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <ThemedView>
+      <ThemedText>{title}</ThemedText>
+
+      <ThemedView>
+        <TextInput
+          value={value}
+          placeholder={placeholder}
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+
+        {title === "Password" && (
+          <Pressable onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              resizeMode="contain"
+            />
+          </Pressable>
+        )}
+      </ThemedView>
+    </ThemedView>
+  );
+};
+
+export default FormField;
