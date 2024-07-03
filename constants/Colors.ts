d6437433e0f6243941f85e7164a763c117920a96
xue <behicsakar@gmail.com>
2024-07-03 06:48:47 +0300

    refactor: Update Colors.ts to use consistent variable names and improve theme styling

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,49 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = "#ffc409";
-const tintColorDark = "#ffc409";
+const primary = "#ffc409";
+const secondary = "#ff571a";
+const tertiary = "#5260ff";
+const success = "#2dd36f";
+const warnign = "#ffc409";
+const danger = "#eb445a";
+const dark = "#222428";
+const medium = "#92949c";
+const light = "#f4f5f8";
 
 export const Colors = {
   light: {
-    text: "#11181C",
-    background: "#fff",
-    tint: tintColorLight,
-    icon: "#687076",
-    tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
+    primary: primary,
+    secondary: secondary,
+    tertiary: tertiary,
+    success: success,
+    warning: warnign,
+    danger: danger,
+    dark: dark,
+    medium: medium,
+    light: light,
+    text: dark,
+    background: light,
+    tint: primary,
+    icon: medium,
+    tabIconDefault: medium,
+    tabIconSelected: primary,
   },
   dark: {
-    text: "#ECEDEE",
-    background: "#151718",
-    tint: tintColorDark,
-    icon: "#9BA1A6",
-    tabIconDefault: "#9BA1A6",
-    tabIconSelected: tintColorDark,
+    primary: primary,
+    secondary: secondary,
+    tertiary: tertiary,
+    success: success,
+    warning: warnign,
+    danger: danger,
+    dark: light,
+    medium: medium,
+    light: dark,
+    text: light,
+    background: dark,
+    tint: primary,
+    icon: medium,
+    tabIconDefault: medium,
+    tabIconSelected: primary,
   },
 };
