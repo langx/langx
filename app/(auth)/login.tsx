f4e8027c78c4b824e69fa4f7de296e5e055ff792
xue <behicsakar@gmail.com>
2024-07-03 11:14:54 +0300

    refactor: Update login screen layout and styling

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,14 +1,77 @@
 import React from "react";
+import {
+  Image,
+  StyleSheet,
+  useColorScheme,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Link } from "expo-router";
+
+import { Colors } from "@/constants/Colors";
+import images from "@/constants/images";
 
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 
 const Login = () => {
+  const colorScheme = useColorScheme();
+
+  const openLink = () => {};
   return (
-    <ThemedView>
-      <ThemedText>Login</ThemedText>
+    <ThemedView style={styles.container}>
+      <ScrollView>
+        <Image
+          source={colorScheme === "dark" ? images.logo_light : images.logo_dark}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+
+        <ThemedText style={styles.headline}>Log In</ThemedText>
+      </ScrollView>
     </ThemedView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  logo: {
+    width: "50%",
+    left: 0,
+
+    height: 100,
+  },
+  headline: {
+    fontSize: 26,
+    fontFamily: "Comfortaa-Bold",
+    paddingVertical: 20,
+  },
+  welcomeImage: {
+    width: "100%",
+    marginVertical: 20,
+  },
+
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 80,
+    color: Colors.light.medium,
+  },
+  link: {
+    color: Colors.light.primary,
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: Colors.light.primary,
+    fontSize: 22,
+    fontWeight: "500",
+  },
+});
 
 export default Login;
