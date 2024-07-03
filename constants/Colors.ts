e863db21f1684a329aee123b554d854dc6b71fd6
xue <behicsakar@gmail.com>
2024-07-03 13:22:02 +0300

    refactor: Update Colors.ts with new color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,11 +7,18 @@ const primary = "#ffc409";
 const secondary = "#ff571a";
 const tertiary = "#5260ff";
 const success = "#2dd36f";
-const warnign = "#ffc409";
-const danger = "#eb445a";
-const dark = "#222428";
-const medium = "#92949c";
-const light = "#f4f5f8";
+const warning = "#ffc409";
+const error = "#eb445a";
+const black = "#222428";
+const white = "#f4f5f8";
+const grey0 = "#393e42";
+const grey1 = "#43484d";
+const grey2 = "#5e6977";
+const grey3 = "#86939e";
+const grey4 = "#bdc6cf";
+const grey5 = "#e1e8ee";
+const greyOutline = "#bbb";
+const searchBg = "#303337";
 
 export const Colors = {
   light: {
@@ -19,33 +26,35 @@ export const Colors = {
     secondary: secondary,
     tertiary: tertiary,
     success: success,
-    warning: warnign,
-    danger: danger,
-    dark: dark,
-    medium: medium,
-    light: light,
-    text: dark,
-    background: light,
-    tint: primary,
-    icon: medium,
-    tabIconDefault: medium,
-    tabIconSelected: primary,
+    warning: warning,
+    error: error,
+    black: black,
+    white: white,
+    background: white,
+    grey0: grey0,
+    grey1: grey1,
+    grey2: grey2,
+    grey3: grey3,
+    grey4: grey4,
+    grey5: grey5,
+    greyOutline: greyOutline,
   },
   dark: {
     primary: primary,
     secondary: secondary,
     tertiary: tertiary,
     success: success,
-    warning: warnign,
-    danger: danger,
-    dark: light,
-    medium: medium,
-    light: dark,
-    text: light,
-    background: dark,
-    tint: primary,
-    icon: medium,
-    tabIconDefault: medium,
-    tabIconSelected: primary,
+    warning: warning,
+    error: error,
+    black: white,
+    white: black,
+    background: black,
+    grey0: grey0,
+    grey1: grey1,
+    grey2: grey2,
+    grey3: grey3,
+    grey4: grey4,
+    grey5: grey5,
+    greyOutline: greyOutline,
   },
 };
