ce5c012951d527b75728d1c514076ba2f6a15cd4
xue <behicsakar@gmail.com>
2024-07-05 00:48:36 +0300

    refactor: Update RoomsScreen to use ChatRow component for rendering chats

--- app/(tabs)/rooms/index.tsx
+++ app/(tabs)/rooms/index.tsx
@@ -3,63 +3,31 @@ import { FlatList, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import { ThemedView } from "@/components/atomic/ThemedView";
-import { ThemedText } from "@/components/atomic/ThemedText";
 import { defaultStyles } from "@/constants/Styles";
 import { Colors } from "@/constants/Colors";
-import BoxedIcon from "@/components/BoxedIcon";
+import chats from "@/assets/data/chats.json";
+import ChatRow from "@/components/ChatRow";
 
 export default function RoomsScreen() {
-  const devices = [
-    {
-      name: "Broadcast Lists",
-      icon: "megaphone",
-      backgroundColor: Colors.light.success,
-    },
-    {
-      name: "Starred Messages",
-      icon: "star",
-      backgroundColor: Colors.light.primary,
-    },
-    {
-      name: "Linked Devices",
-      icon: "laptop-outline",
-      backgroundColor: Colors.light.success,
-    },
-  ];
-
   return (
     <ThemedView
       style={{
         flex: 1,
       }}
     >
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <ThemedView style={defaultStyles.block}>
-          <FlatList
-            data={devices}
-            scrollEnabled={false}
-            ItemSeparatorComponent={() => (
-              <ThemedView style={defaultStyles.separator} />
-            )}
-            renderItem={({ item }) => (
-              <ThemedView style={defaultStyles.item}>
-                <BoxedIcon
-                  name={item.icon}
-                  backgroundColor={item.backgroundColor}
-                />
-
-                <ThemedText style={{ fontSize: 18, flex: 1 }}>
-                  {item.name}
-                </ThemedText>
-                <Ionicons
-                  name="chevron-forward"
-                  size={20}
-                  color={Colors.light.grey3}
-                />
-              </ThemedView>
-            )}
-          />
-        </ThemedView>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{ paddingBottom: 40 }}
+      >
+        <FlatList
+          data={chats}
+          renderItem={({ item }) => <ChatRow {...item} />}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => (
+            <ThemedView style={[defaultStyles.separator, { marginLeft: 90 }]} />
+          )}
+          scrollEnabled={false}
+        />
       </ScrollView>
     </ThemedView>
   );
---
+++ components/ChatRow.tsx
@@ -0,0 +1,73 @@
+// import AppleStyleSwipeableRow from "@/components/AppleStyleSwipeableRow";
+import { Colors } from "@/constants/Colors";
+import { format } from "date-fns";
+import { Link } from "expo-router";
+import { FC } from "react";
+import { Image, TouchableHighlight } from "react-native";
+
+import { ThemedView } from "@/components/atomic/ThemedView";
+import { ThemedText } from "@/components/atomic/ThemedText";
+
+export interface ChatRowProps {
+  id: string;
+  from: string;
+  date: string;
+  img: string;
+  msg: string;
+  read: boolean;
+  unreadCount: number;
+}
+
+const ChatRow: FC<ChatRowProps> = ({
+  id,
+  from,
+  date,
+  img,
+  msg,
+  read,
+  unreadCount,
+}) => {
+  return (
+    // <AppleStyleSwipeableRow>
+    <Link href={`/(tabs)/chats/${id}`} asChild>
+      <TouchableHighlight
+        activeOpacity={0.8}
+        underlayColor={Colors.light.grey5}
+      >
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 14,
+            paddingLeft: 20,
+            paddingVertical: 10,
+          }}
+        >
+          <Image
+            source={{ uri: img }}
+            style={{ width: 50, height: 50, borderRadius: 50 }}
+          />
+          <ThemedView style={{ flex: 1 }}>
+            <ThemedText style={{ fontSize: 18, fontWeight: "bold" }}>
+              {from}
+            </ThemedText>
+            <ThemedText style={{ fontSize: 16, color: Colors.light.grey3 }}>
+              {msg.length > 40 ? `${msg.substring(0, 40)}...` : msg}
+            </ThemedText>
+          </ThemedView>
+          <ThemedText
+            style={{
+              color: Colors.light.grey3,
+              paddingRight: 20,
+              alignSelf: "flex-start",
+            }}
+          >
+            {format(date, "MM.dd.yy")}
+          </ThemedText>
+        </ThemedView>
+      </TouchableHighlight>
+    </Link>
+    // </AppleStyleSwipeableRow>
+  );
+};
+export default ChatRow;
