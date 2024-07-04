197b7c5192d709cda0dca1bc40a1291e82eaca43
xue <behicsakar@gmail.com>
2024-07-04 15:55:06 +0300

    feat: implement header and route functionality

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -1,10 +1,87 @@
 import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Alert } from "react-native";
+import { useDispatch } from "react-redux";
+import { useRouter, Link } from "expo-router";
+
+import { logout } from "@/services/appwrite";
+import { setLoading, setUser } from "@/store/authSlice";
+import { Colors } from "@/constants/Colors";
+import { ThemedText } from "@/components/atomic/ThemedText";
+import { useState } from "react";
 
 export default function RootLayout() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isSubmitting, setSubmitting] = useState(false);
+  const username = "useSelector"; // Replace with actual selector to get username
+
+  const signout = async () => {
+    setSubmitting(true);
+    try {
+      await logout();
+      dispatch(setUser(null));
+      dispatch(setLoading(false));
+      Alert.alert("Success", "User signed out successfully");
+      console.log("logged out");
+      router.replace("/welcome");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="settings" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerStyle: { backgroundColor: Colors.light.primary },
+          headerTitle: () => (
+            <ThemedText
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                color: Colors.light.black,
+              }}
+            >
+              @{username}
+            </ThemedText>
+          ),
+          headerRight: () => (
+            <Link href="profile/settings">
+              <Ionicons
+                name="menu-outline"
+                size={24}
+                color={Colors.light.black}
+                style={{ marginRight: 16 }}
+              />
+            </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="settings"
+        options={{
+          headerShown: true,
+          headerStyle: { backgroundColor: Colors.light.primary },
+          headerTitle: () => (
+            <ThemedText
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                color: Colors.light.black,
+              }}
+            >
+              Settings
+            </ThemedText>
+          ),
+          headerBackTitleVisible: false,
+          headerTintColor: Colors.light.black,
+        }}
+      />
     </Stack>
   );
 }
--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,24 +1,21 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, Image, Platform, Alert } from "react-native";
-import { useRouter } from "expo-router";
+import { StyleSheet, Alert, Pressable, SafeAreaView } from "react-native";
+import { Link, useRouter } from "expo-router";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
+import { setLoading, setUser } from "@/store/authSlice";
 import { logout } from "@/services/appwrite";
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Colors } from "@/constants/Colors";
 import { ThemedText } from "@/components/atomic/ThemedText";
 import { ThemedView } from "@/components/atomic/ThemedView";
-import { ThemedButton } from "@/components/atomic/ThemedButton";
-import { setLoading, setUser } from "@/store/authSlice";
 
 export default function ProfileScreen() {
   const router = useRouter();
 
   const dispatch = useDispatch();
-
   const [isSubmitting, setSubmitting] = useState(false);
+  const username = "useSelector";
 
   const signout = async () => {
     setSubmitting(true);
@@ -36,117 +33,19 @@ export default function ProfileScreen() {
     }
   };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-        <ThemedButton title="logout" onPress={signout}></ThemedButton>
-      </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ alignSelf: "center" }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-          to see how to load{" "}
-          <ThemedText style={{ fontFamily: "SpaceMono" }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <ThemedView style={styles.container}>
+      <ThemedText>aa</ThemedText>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
+  container: {
+    flex: 1,
   },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
+  username: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.light.black,
   },
 });
