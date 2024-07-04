089fc6ceba4eeb33873d03a258645cbc9f7ced5a
xue <behicsakar@gmail.com>
2024-07-05 00:47:43 +0300

    refactor: Update BoxedIcon component to use consistent quotation marks

--- components/BoxedIcon.tsx
+++ components/BoxedIcon.tsx
@@ -1,5 +1,5 @@
-import { Ionicons } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
 
 export type BoxedIconProps = {
   name: typeof Ionicons.defaultProps;
@@ -9,7 +9,7 @@ export type BoxedIconProps = {
 const BoxedIcon = ({ name, backgroundColor }: BoxedIconProps) => {
   return (
     <View style={{ backgroundColor, padding: 4, borderRadius: 6 }}>
-      <Ionicons name={name} size={22} color={'#fff'} />
+      <Ionicons name={name} size={22} color={"#fff"} />
     </View>
   );
 };
