200d34a5f54b5e50718bddfa6352e90857f5f4a5
xue <behicsakar@gmail.com>
2024-07-04 16:46:43 +0300

    refactor: Remove signout functionality from profile layout and added settings components

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -1,37 +1,12 @@
-import { useState } from "react";
 import { Stack } from "expo-router";
-import { Alert } from "react-native";
-import { useDispatch } from "react-redux";
-import { useRouter, Link } from "expo-router";
+import { Link } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
-import { logout } from "@/services/appwrite";
-import { setLoading, setUser } from "@/store/authSlice";
 
 import { Colors } from "@/constants/Colors";
 import { ThemedText } from "@/components/atomic/ThemedText";
 
 export default function RootLayout() {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const [isSubmitting, setSubmitting] = useState(false);
   const username = "username";
-
-  const signout = async () => {
-    setSubmitting(true);
-    try {
-      await logout();
-      dispatch(setUser(null));
-      dispatch(setLoading(false));
-      Alert.alert("Success", "User signed out successfully");
-      console.log("logged out");
-      router.replace("/welcome");
-    } catch (error) {
-      Alert.alert("Error", error.message);
-    } finally {
-      setSubmitting(false);
-    }
-  };
 
   return (
     <Stack>
--- app/(tabs)/profile/settings.tsx
+++ app/(tabs)/profile/settings.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import { useRouter } from "expo-router";
+import { useDispatch } from "react-redux";
+
+import { logout } from "@/services/appwrite";
+import { setLoading, setUser } from "@/store/authSlice";
+import { ThemedView } from "@/components/atomic/ThemedView";
+import { ThemedText } from "@/components/atomic/ThemedText";
 
 const Settings = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onPressLogout = async () => {
+    setSubmitting(true);
+    try {
+      await logout();
+      dispatch(setUser(null));
+      dispatch(setLoading(false));
+      Alert.alert("Success", "User signed out successfully");
+      console.log("logged out");
+      router.replace("/welcome");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ThemedView>
+      <ThemedText>Settings</ThemedText>
+    </ThemedView>
   );
 };
 
