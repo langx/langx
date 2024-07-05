1c70d56cca2f48acca172322de85e8e0a3adc980
xue <behicsakar@gmail.com>
2024-07-05 15:17:53 +0300

    refactor: Remove signout functionality from ProfileScreen

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,17 +1,8 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import {
-  StyleSheet,
-  Alert,
-  Pressable,
-  SafeAreaView,
-  StatusBar,
-} from "react-native";
-import { Link, useFocusEffect, useRouter } from "expo-router";
-import { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
+import { StyleSheet, StatusBar } from "react-native";
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback } from "react";
 
-import { setLoading, setUser } from "@/store/authSlice";
-import { logout } from "@/services/appwrite";
 import { Colors } from "@/constants/Colors";
 import { ThemedText } from "@/components/atomic/ThemedText";
 import { ThemedView } from "@/components/atomic/ThemedView";
@@ -28,25 +19,6 @@ export default function ProfileScreen() {
     }, [])
   );
 
-  const dispatch = useDispatch();
-  const [isSubmitting, setSubmitting] = useState(false);
-  const username = "useSelector";
-
-  const signout = async () => {
-    setSubmitting(true);
-    try {
-      await logout();
-      dispatch(setUser(null));
-      dispatch(setLoading(false));
-      Alert.alert("Success", "User signed out successfully");
-      console.log("logged out");
-      router.replace("/welcome");
-    } catch (error) {
-      Alert.alert("Error", error.message);
-    } finally {
-      setSubmitting(false);
-    }
-  };
   return (
     <>
       <StatusBar />
