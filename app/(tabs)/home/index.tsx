b09c39596ced52607603aba7a214597eccd54f6c
xue <behicsakar@gmail.com>
2024-07-07 18:27:00 +0300

    refactor: Update home screen to display a list of users from the database

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,57 +1,30 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { StyleSheet, FlatList, SafeAreaView } from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { HelloWave } from "@/components/HelloWave";
 import { ThemedText } from "@/components/atomic/ThemedText";
 import { ThemedView } from "@/components/atomic/ThemedView";
-import { Link } from "expo-router";
+import { listUsers } from "@/services/userService";
+import useDatabase from "@/hooks/useDatabase";
 
 export default function CommunityScreen() {
+  const { data: users, refetch } = useDatabase(listUsers);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.$id}
+        ListHeaderComponent={() => (
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">For You</ThemedText>
+          </ThemedView>
+        )}
+        renderItem={({ item }) => (
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText style={{ fontSize: 26 }}>{item.name}</ThemedText>
+            <ThemedText style={{ fontSize: 16 }}>{item.$id}</ThemedText>
+          </ThemedView>
+        )}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
