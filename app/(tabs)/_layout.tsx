cfa67fea31862bb92109106b46ec01bc7efdf6a5
xue <behicsakar@gmail.com>
2024-07-07 12:30:48 +0300

    fix: Remove unused code and update dependencies in _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs, useSegments } from "expo-router";
+import { Tabs } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { Colors } from "@/constants/Colors";
@@ -8,7 +8,6 @@ import { TabBarItem } from "@/components/navigation/TabBarItem";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const theme = colorScheme ?? "light";
-  const segments = useSegments();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
@@ -20,7 +19,6 @@ export default function TabLayout() {
           tabBarStyle: {
             backgroundColor: Colors[theme].gray5,
             height: 60,
-            display: segments[2] === "[id]" ? "none" : "flex",
           },
         }}
       >
