835dc820db01587147456523f913b5400c1de6b2
xue <behicsakar@gmail.com>
2024-07-07 16:02:41 +0300

    feat: Add useDatabase hook for fetching data from the database

---
+++ hooks/useDatabase.tsx
@@ -0,0 +1,29 @@
+import { Alert } from "react-native";
+import { useEffect, useState } from "react";
+
+const useDatabase = (fn: Function) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fn();
+      setData(res);
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => fetchData();
+
+  return { data, loading, refetch };
+};
+
+export default useDatabase;
