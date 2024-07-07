137a890cdbbc8d4a0c8298d33c5ecd35764d97af
xue <behicsakar@gmail.com>
2024-07-07 18:26:51 +0300

    refactor: Update listUsers function in userService.tsx to order users by descending updatedAt date

--- services/userService.tsx
+++ services/userService.tsx
@@ -2,16 +2,15 @@ import { Query } from "react-native-appwrite";
 
 import { listDocuments } from "@/services/apiService";
 import { USERS_COLLECTION } from "@/constants/config";
+import { User } from "@/models/User";
 
 // Sign In
 export async function listUsers() {
   try {
     const users = await listDocuments(USERS_COLLECTION, [
-      Query.orderAsc("$updatedAt"),
+      Query.orderDesc("$updatedAt"),
     ]);
-
-    console.log(users);
-    return users;
+    return users.documents as User[];
   } catch (error) {
     throw new Error(error);
   }
