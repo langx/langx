9064c92d075c1105c49c87328a9c195552c7a720
xue <behicsakar@gmail.com>
2024-07-08 08:55:07 +0300

    refactor: Update home screen to use ThemedView for consistent styling

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,8 +1,9 @@
-import { FlatList, SafeAreaView } from "react-native";
+import { FlatList } from "react-native";
 
 import { useDatabase } from "@/hooks/useDatabase";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { listUsers } from "@/services/userService";
+import { ThemedView } from "@/components/atomic/ThemedView";
 import UserCard from "@/components/home/UserCard";
 
 export default function CommunityScreen() {
@@ -10,8 +11,13 @@ export default function CommunityScreen() {
   const { data: users, loading, refetch } = useDatabase(listUsers);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <ThemedView
+      style={{
+        flex: 1,
+      }}
+    >
       <FlatList
+        contentInsetAdjustmentBehavior="automatic"
         data={users}
         numColumns={2}
         keyExtractor={(item) => item.$id}
@@ -19,6 +25,6 @@ export default function CommunityScreen() {
           <UserCard item={item} loadingItem={loading} theme={theme} />
         )}
       ></FlatList>
-    </SafeAreaView>
+    </ThemedView>
   );
 }
