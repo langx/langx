ba22fbf3e67293b088ef6efd2f47555c4b97a9b3
xue <behicsakar@gmail.com>
2024-07-09 13:16:31 +0300

    refactor: Update home screen layout to use responsive grid for user cards and add "For You" section header

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -77,6 +77,7 @@ export default function HomeLayout() {
           },
         }}
       />
+      <Stack.Screen name="recomended" />
     </Stack>
   );
 }
--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,79 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Dimensions, FlatList } from "react-native";
+import React from "react";
+
 import { useDatabase } from "@/hooks/useDatabase";
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { listUsers } from "@/services/userService";
 import { ThemedView } from "@/components/atomic/ThemedView";
 import ForYouSection from "@/components/home/ForYouSection";
-import UserCard from "@/components/home/UserCard";
-
-const breakpoints = {
-  xs: 0,
-  sm: 320,
-  md: 480,
-  lg: 768,
-  xl: 1024,
-  xxl: 1440,
-  mobile: 640,
-};
-const getNumColumns = (width) => {
-  if (width < breakpoints.sm) return 2; // xs
-  else if (width >= breakpoints.sm && width < breakpoints.md) return 2; // sm
-  else if (width >= breakpoints.md && width < breakpoints.lg) return 3; // md
-  else if (width >= breakpoints.lg && width < breakpoints.xl) return 4; // lg
-  else if (width >= breakpoints.xl && width < breakpoints.xxl) return 6; // xl
-  else if (width >= breakpoints.xxl) return 8; // xxl
-};
 
 export default function CommunityScreen() {
-  const theme = useColorScheme() === "dark" ? "dark" : "light";
   const { data: users, loading, refetch } = useDatabase(listUsers);
-
-  const [screenWidth, setScreenWidth] = useState(
-    Dimensions.get("window").width
-  );
-  const [numColumns, setNumColumns] = useState(getNumColumns(screenWidth));
-  useEffect(() => {
-    const updateLayout = () => {
-      const newWidth = Dimensions.get("window").width;
-      setScreenWidth(newWidth);
-      console.log("Screen width changed to", newWidth);
-    };
-
-    // Subscribe to dimension changes
-    const subscription = Dimensions.addEventListener("change", updateLayout);
-
-    // Return a cleanup function that removes the event listener
-    return () => subscription.remove();
-  }, []);
-
-  useEffect(() => {
-    setNumColumns(getNumColumns(screenWidth));
-  }, [screenWidth]);
-
-  const renderItem = ({ item }) => {
-    return (
-      <>
-        <UserCard item={item} loadingItem={loading} />
-      </>
-    );
-  };
 
   return (
     <ThemedView style={{ flex: 1 }}>
-      <FlatList
-        contentInsetAdjustmentBehavior="automatic"
-        data={users}
-        keyExtractor={(item) => item.$id.toString()}
-        renderItem={renderItem}
-        key={numColumns}
-        numColumns={numColumns}
-        ListHeaderComponent={
-          <ThemedView>
-            <ForYouSection users={users} />
-          </ThemedView>
-        }
-      />
+      <ForYouSection users={users} />
+      <ForYouSection users={users} />
+      <ForYouSection users={users} />
+      <ForYouSection users={users} />
     </ThemedView>
   );
 }
