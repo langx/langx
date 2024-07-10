ee9f25e7a463484e12defe7d8b288c0406d13683
xue <behicsakar@gmail.com>
2024-07-10 18:55:10 +0300

    refactor: Add VisitorsSection component to home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView } from "react-native";
 
 import { useDatabase } from "@/hooks/useDatabase";
 import { listUsers } from "@/services/userService";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
-import { ThemedText } from "@/components/themed/atomic/ThemedText";
 import RecomendedSection from "@/components/home/RecomendedSection";
 import FeaturedSection from "@/components/home/FeaturedSection";
+import VisitorsSection from "@/components/home/VisitorsSection";
 
 export default function CommunityScreen() {
   const { data: users, loading, refetch } = useDatabase(listUsers);
@@ -16,29 +16,8 @@ export default function CommunityScreen() {
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <RecomendedSection users={users} loading={loading} />
         <FeaturedSection users={users} loading={loading} />
-        <ThemedView style={styles.card}>
-          <ThemedText style={styles.cardTitle}>Visitors</ThemedText>
-        </ThemedView>
+        <VisitorsSection></VisitorsSection>
       </ScrollView>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    borderRadius: 10,
-    overflow: "hidden",
-    margin: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  cardTitle: {
-    fontSize: 24,
-    fontFamily: "Lexend-Bold",
-  },
-  cardSubtitle: {
-    fontSize: 16,
-    marginTop: 5,
-  },
-});
---
+++ components/home/VisitorsSection.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { ThemedView } from "@/components/themed/atomic/ThemedView";
+import { ThemedText } from "@/components/themed/atomic/ThemedText";
+
+const VisitorsSection = () => {
+  return (
+    <ThemedView style={styles.card}>
+      <ThemedText style={styles.cardTitle}>Visitors</ThemedText>
+    </ThemedView>
+  );
+};
+
+export default VisitorsSection;
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    overflow: "hidden",
+    margin: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontFamily: "Lexend-Bold",
+  },
+  cardSubtitle: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
