3fd2ac4a20daba9f57fee042fce5f90a5bae0436
xue <behicsakar@gmail.com>
2024-07-11 17:38:49 +0300

    refactor: Update useDatabase hook to include initialOffset parameter

--- hooks/useDatabase.tsx
+++ hooks/useDatabase.tsx
@@ -1,14 +1,15 @@
 import { Alert } from "react-native";
 import { useEffect, useState } from "react";
 
-export function useDatabase(fn: Function) {
+export function useDatabase(fn: Function, initialOffset: number = 0) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [offset, setOffset] = useState(initialOffset);
 
-  const fetchData = async () => {
+  const fetchData = async (currentOffset: number) => {
     setLoading(true);
     try {
-      const res = await fn();
+      const res = await fn(currentOffset);
       setData(res);
     } catch (error) {
       Alert.alert("Error", error.message);
@@ -18,10 +19,10 @@ export function useDatabase(fn: Function) {
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData(offset);
+  }, [offset]);
 
-  const refetch = () => fetchData();
+  const refetch = () => fetchData(offset);
 
-  return { data, loading, refetch };
+  return { data, loading, refetch, setOffset };
 }
