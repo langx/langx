8e0ae25ecaf057f44071873186f9176d78168651
xue <behicsakar@gmail.com>
2024-07-11 13:47:03 +0300

    feat: Update OtherPhotosCard component to include image modal and grid layout

--- components/profile/OtherPhotosCard.tsx
+++ components/profile/OtherPhotosCard.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { StyleSheet, Image, Pressable } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  Pressable,
+  Modal,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import Swiper from "react-native-swiper";
-
 import images from "@/constants/images";
 import { Colors } from "@/constants/Colors";
-
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
 import { ThemedText } from "@/components/themed/atomic/ThemedText";
 
 const OtherPhotosCard = ({ user }) => {
-  // const otherPics = user.otherPhotos;
-  const otherPics = [1, 2, 3, 4, 5];
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const otherPics = [1, 2, 3, 4, 5, 6]; // Replace this with user.otherPhotos;
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <ThemedView style={styles.card}>
       {otherPics.length > 0 && (
@@ -21,34 +38,49 @@ const OtherPhotosCard = ({ user }) => {
               Photos Collection
             </ThemedText>
           </ThemedView>
-          <ThemedView style={styles.cardContent}>
+          <ScrollView contentContainerStyle={styles.cardContent}>
             {otherPics.length === 0 ? (
               <ThemedText>Currently, no photos are available.</ThemedText>
             ) : (
-              <Swiper
-                style={styles.wrapper}
-                showsButtons
-                loop
-                paginationStyle={styles.pagination}
-                activeDotColor={Colors.light.primary}
-                dotColor={Colors.light.gray3}
-              >
+              <View style={styles.grid}>
                 {otherPics.map((photo, index) => (
-                  <Pressable
-                    key={index}
-                    onPress={() => console.log("Preview photo", index)}
-                  >
-                    <ThemedView style={styles.slide}>
-                      {/* <Image source={{ uri: photo }} style={styles.image} /> */}
-                      <Image source={images.profile} style={styles.image} />
-                    </ThemedView>
+                  <Pressable key={index} onPress={() => openModal(index)}>
+                    <Image
+                      source={images.thumbnail}
+                      style={[styles.image, styles.gridItem]}
+                    />
                   </Pressable>
                 ))}
-              </Swiper>
+              </View>
             )}
-          </ThemedView>
+          </ScrollView>
         </>
       )}
+
+      <Modal visible={isModalVisible} transparent={true} animationType="slide">
+        <View style={styles.modalContainer}>
+          <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+            <ThemedText style={styles.closeButtonText}>×</ThemedText>
+          </TouchableOpacity>
+          <Swiper
+            style={styles.modalSwiper}
+            index={currentIndex}
+            loop
+            showsButtons
+            paginationStyle={styles.paginationStyle}
+            activeDotColor={Colors.light.primary}
+            dotColor={Colors.light.gray3}
+            nextButton={<ThemedText style={styles.nextButton}>›</ThemedText>}
+            prevButton={<ThemedText style={styles.prevButton}>‹</ThemedText>}
+          >
+            {otherPics.map((photo, index) => (
+              <ThemedView key={index} style={styles.modalSlide}>
+                <Image source={images.thumbnail} style={styles.modalImage} />
+              </ThemedView>
+            ))}
+          </Swiper>
+        </View>
+      </Modal>
     </ThemedView>
   );
 };
@@ -77,20 +109,59 @@ const styles = StyleSheet.create({
   cardContent: {
     padding: 20,
   },
-  wrapper: {
-    height: 200,
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignItems: "flex-start",
+    padding: 10,
   },
-  pagination: {
-    bottom: -20,
+  gridItem: {
+    margin: 10,
   },
-  slide: {
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    margin: 5,
+  },
+  modalContainer: {
     flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
     justifyContent: "center",
     alignItems: "center",
   },
-  image: {
-    width: 200,
-    height: 200,
+  closeButton: {
+    position: "absolute",
+    top: 50,
+    right: 30,
+    zIndex: 1,
+  },
+  closeButtonText: {
+    fontSize: 50,
+    color: "white",
+  },
+  modalSwiper: {
+    height: "80%",
+  },
+  modalSlide: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalImage: {
+    width: "90%",
+    height: "90%",
     borderRadius: 10,
+  },
+  nextButton: {
+    fontSize: 50,
+    color: Colors.light.primary,
+  },
+  prevButton: {
+    fontSize: 50,
+    color: Colors.light.primary,
+  },
+  paginationStyle: {
+    bottom: -20,
   },
 });
