9370b34926f831dae11a9f7d09c699766dc5bdf2
xue <behicsakar@gmail.com>
2024-07-12 20:03:20 +0300

    refactor: Update RoomsScreen component to optimize rendering and remove unused code

--- app/(tabs)/rooms/index.tsx
+++ app/(tabs)/rooms/index.tsx
@@ -1,46 +1,32 @@
-import React, { useCallback } from "react";
-import { FlatList, ScrollView, StatusBar } from "react-native";
+import React from "react";
+import { FlatList, ScrollView } from "react-native";
 
 import chats from "@/assets/data/chats.json";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
 import { defaultStyles } from "@/constants/Styles";
 import RoomRow from "@/components/rooms/RoomRow";
-import { useFocusEffect } from "expo-router";
 
 export default function RoomsScreen() {
-  useFocusEffect(
-    useCallback(() => {
-      StatusBar.setBarStyle("dark-content");
-
-      // This function is returned to revert the effect when the component is not focused
-      return () => StatusBar.setBarStyle("default");
-    }, [])
-  );
   return (
-    <>
-      <StatusBar />
-      <ThemedView
-        style={{
-          flex: 1,
-        }}
+    <ThemedView
+      style={{
+        flex: 1,
+      }}
+    >
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{ paddingBottom: 40 }}
       >
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          contentContainerStyle={{ paddingBottom: 40 }}
-        >
-          <FlatList
-            data={chats}
-            renderItem={({ item }) => <RoomRow {...item} />}
-            keyExtractor={(item) => item.id.toString()}
-            ItemSeparatorComponent={() => (
-              <ThemedView
-                style={[defaultStyles.separator, { marginLeft: 90 }]}
-              />
-            )}
-            scrollEnabled={false}
-          />
-        </ScrollView>
-      </ThemedView>
-    </>
+        <FlatList
+          data={chats}
+          renderItem={({ item }) => <RoomRow {...item} />}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => (
+            <ThemedView style={[defaultStyles.separator, { marginLeft: 90 }]} />
+          )}
+          scrollEnabled={false}
+        />
+      </ScrollView>
+    </ThemedView>
   );
 }
