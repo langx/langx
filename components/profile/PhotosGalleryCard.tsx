ea859fd106af6edc15ff2a58b1332194bc5e5396
xue <behicsakar@gmail.com>
2024-07-12 03:39:10 +0300

    feat: User other pics

--- components/profile/PhotosGalleryCard.tsx
+++ components/profile/PhotosGalleryCard.tsx
@@ -1,15 +1,53 @@
-import React from "react";
-import { StyleSheet, Image, Pressable, View, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  Pressable,
+  ScrollView,
+  ActivityIndicator,
+  ImageStyle,
+} from "react-native";
 
-import images from "@/constants/images";
+import { Colors } from "@/constants/Colors";
+import { getUserImage } from "@/services/bucketService";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
 import { ThemedText } from "@/components/themed/atomic/ThemedText";
 
 const PhotosGalleryCard = ({ user }) => {
-  const otherPics = [1, 2, 3, 4, 5, 6];
+  const otherPics = user?.otherPics || [];
+  const [imageUris, setImageUris] = useState(
+    Array(otherPics.length).fill(null)
+  );
+  const [loadingStates, setLoadingStates] = useState(
+    Array(otherPics.length).fill(true)
+  );
+
+  useEffect(() => {
+    const fetchImageUris = async () => {
+      const uris = await Promise.all(
+        otherPics.map(async (picId) => {
+          const uri = await getUserImage(picId);
+          return uri.toString();
+        })
+      );
+      setImageUris(uris);
+    };
+
+    if (otherPics.length > 0) {
+      fetchImageUris();
+    }
+  }, [otherPics]);
 
   const handleImagePress = (index) => {
     console.log(`Image pressed: ${index}`);
+  };
+
+  const handleLoadEnd = (index) => {
+    setLoadingStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = false;
+      return newStates;
+    });
   };
 
   return (
@@ -21,23 +59,29 @@ const PhotosGalleryCard = ({ user }) => {
             <ThemedText style={styles.cardSubtitle}>User Collection</ThemedText>
           </ThemedView>
           <ScrollView contentContainerStyle={styles.cardContent}>
-            {otherPics.length === 0 ? (
-              <ThemedText>Currently, no photos are available.</ThemedText>
-            ) : (
-              <View style={styles.grid}>
-                {otherPics.map((photo, index) => (
-                  <Pressable
-                    key={index}
-                    onPress={() => handleImagePress(index)}
-                  >
+            <ThemedView style={styles.grid}>
+              {imageUris.map((uri, index) => (
+                <Pressable key={index} onPress={() => handleImagePress(index)}>
+                  <ThemedView style={styles.imageContainer}>
+                    {loadingStates[index] && (
+                      <ActivityIndicator
+                        style={styles.loadingIndicator}
+                        size="large"
+                        color={Colors.light.primary}
+                      />
+                    )}
                     <Image
-                      source={images.thumbnail}
-                      style={[styles.image, styles.gridItem]}
+                      source={{ uri }}
+                      style={[
+                        styles.image as ImageStyle,
+                        styles.gridItem as ImageStyle,
+                      ]}
+                      onLoadEnd={() => handleLoadEnd(index)}
                     />
-                  </Pressable>
-                ))}
-              </View>
-            )}
+                  </ThemedView>
+                </Pressable>
+              ))}
+            </ThemedView>
           </ScrollView>
         </>
       )}
@@ -80,10 +124,21 @@ const styles = StyleSheet.create({
   gridItem: {
     margin: 10,
   },
-  image: {
+  imageContainer: {
+    position: "relative",
     width: 100,
     height: 100,
+    marginBottom: 10,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
     borderRadius: 10,
-    margin: 5,
+  },
+  loadingIndicator: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -12.5 }, { translateY: -12.5 }],
   },
 });
