b983d5be90a21fbdd9495218b7792a18ec2402d0
xue <behicsakar@gmail.com>
2024-07-13 15:30:39 +0300

    refactor: Remove contentContainerStyle from RoomsScreen component

--- app/(tabs)/rooms/index.tsx
+++ app/(tabs)/rooms/index.tsx
@@ -42,7 +42,6 @@ export default function RoomsScreen() {
     >
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        contentContainerStyle={{ paddingBottom: 40 }}
         refreshControl={
           <RefreshControl
             refreshing={isRefreshing}
