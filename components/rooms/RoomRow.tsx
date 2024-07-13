1e9cd74201a65b95d1ddf16e46cb45dc4cf987c3
xue <behicsakar@gmail.com>
2024-07-13 15:10:42 +0300

    refactor: Add lazy loading for user image in RoomRow component

--- components/rooms/RoomRow.tsx
+++ components/rooms/RoomRow.tsx
@@ -1,16 +1,42 @@
 import AppleStyleSwipeableRow from "@/components/rooms/AppleStyleSwipeableRow";
-import { Colors } from "@/constants/Colors";
 import { format } from "date-fns";
 import { Link } from "expo-router";
-import { FC } from "react";
-import { Image, TouchableHighlight } from "react-native";
+import { FC, useEffect, useState } from "react";
+import { Image, TouchableHighlight, StyleSheet } from "react-native";
 
+import { Room } from "@/models/Room";
+import { Colors } from "@/constants/Colors";
+import { useDatabase } from "@/hooks/useDatabase";
+import { getUserImage } from "@/services/bucketService";
 import { ThemedText } from "@/components/themed/atomic/ThemedText";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
-import { Room } from "@/models/Room";
 
 const RoomRow: FC<{ room: Room }> = ({ room }) => {
-  // console.log(room);
+  const [userImageUrl, setUserImageUrl] = useState("");
+  const { data, loading, refetch } = useDatabase(() =>
+    getUserImage(room?.userData?.profilePic)
+  );
+
+  useEffect(() => {
+    if (data) {
+      try {
+        setUserImageUrl(data.toString());
+      } catch (error) {
+        console.error("Failed to process user image URL", error);
+      }
+    }
+  }, [data]);
+
+  if (loading) {
+    return (
+      <ThemedView style={styles.card}>
+        <ThemedView style={styles.loading}>
+          <ThemedText>Loading...</ThemedText>
+        </ThemedView>
+      </ThemedView>
+    );
+  }
+
   return (
     <AppleStyleSwipeableRow>
       <Link href={`/(tabs)/rooms/${room.$id}`} asChild>
@@ -27,11 +53,12 @@ const RoomRow: FC<{ room: Room }> = ({ room }) => {
               paddingVertical: 10,
             }}
           >
-            <ThemedText>{room.users}</ThemedText>
-            {/* <Image
-              source={{ uri: img }}
+            <ThemedText>{room.userData.$id}</ThemedText>
+            <Image
+              source={{ uri: userImageUrl }}
               style={{ width: 50, height: 50, borderRadius: 50 }}
             />
+            {/* 
             <ThemedView style={{ flex: 1 }}>
               <ThemedText style={{ fontSize: 18, fontWeight: "bold" }}>
                 {from}
@@ -63,3 +90,29 @@ const RoomRow: FC<{ room: Room }> = ({ room }) => {
 };
 
 export default RoomRow;
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  card: {
+    flex: 1,
+    margin: 10,
+    borderRadius: 10,
+    overflow: "hidden",
+    height: 200,
+    width: 150,
+    justifyContent: "flex-end",
+  },
+  imageBackground: {
+    width: "100%",
+    height: "100%",
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
