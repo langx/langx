5c034b43e7a582ba6db362b0846f930f5c1d6dff
xue <behicsakar@gmail.com>
2024-07-13 12:47:46 +0300

    refactor: Update listUsers function to handle error messages properly

--- services/userService.tsx
+++ services/userService.tsx
@@ -1,12 +1,14 @@
 import { Query } from "react-native-appwrite";
+
 import { listDocuments } from "@/services/apiService";
 import { USERS_COLLECTION, PAGINATION_LIMIT } from "@/constants/config";
 import { User } from "@/models/User";
 
-export async function listUsers(offset?: number) {
+export async function listUsers(offset?: number): Promise<User[]> {
   try {
     const queries = [
       Query.orderDesc("$updatedAt"),
+      // Query.notEqual("$id", currentUserId),
       Query.limit(PAGINATION_LIMIT),
     ];
 
@@ -17,6 +19,6 @@ export async function listUsers(offset?: number) {
     const users = await listDocuments(USERS_COLLECTION, queries);
     return users.documents as User[];
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.message);
   }
 }
