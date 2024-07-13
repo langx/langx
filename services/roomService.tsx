babfa6ed56b10d2348f81de203df6da4391f8676
xue <behicsakar@gmail.com>
2024-07-13 15:56:20 +0300

    feat: Update roomService to include user data in extendedRooms

--- services/roomService.tsx
+++ services/roomService.tsx
@@ -1,6 +1,10 @@
 import { Query } from "react-native-appwrite";
 import { listDocuments } from "@/services/apiService";
-import { ROOMS_COLLECTION, PAGINATION_LIMIT } from "@/constants/config";
+import {
+  ROOMS_COLLECTION,
+  USERS_COLLECTION,
+  PAGINATION_LIMIT,
+} from "@/constants/config";
 import { Room } from "@/models/Room";
 import { RoomExtendedInterface } from "@/models/extended/RoomExtended.interface";
 import { sampleUser } from "@/constants/sampleUser";
@@ -23,18 +27,28 @@ export async function listRooms(
       queries.push(Query.offset(offset));
     }
 
-    // const rooms = await listDocuments(ROOMS_COLLECTION, queries);
-    // return rooms.documents as Room[];
+    const fetchUserData = async (senderId: string) => {
+      const usersResponse = await listDocuments(USERS_COLLECTION, [
+        Query.equal("$id", senderId),
+        Query.limit(1),
+      ]);
+      return usersResponse.documents[0];
+    };
 
-    const extendedRooms = (
-      await listDocuments(ROOMS_COLLECTION, queries)
-    ).documents.map((room) => {
-      return {
-        ...room,
-        userData: sampleUser,
-        messages: [],
-      };
-    }) as RoomExtendedInterface[];
+    const extendedRooms = (await Promise.all(
+      (
+        await listDocuments(ROOMS_COLLECTION, queries)
+      ).documents.map(async (room) => {
+        const sender = room.users.find((user: string) => user !== userId);
+        const userData = await fetchUserData(sender);
+        return {
+          ...room,
+          userData,
+          messages: [],
+        };
+      })
+    )) as RoomExtendedInterface[];
+
     return extendedRooms;
   } catch (error) {
     throw new Error(error);
