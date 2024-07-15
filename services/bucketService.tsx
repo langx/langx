4c83d628fe540f265afc2c373ca8634be77ccc1d
xue <behicsakar@gmail.com>
2024-07-15 15:40:54 +0300

    chore: Add MESSAGE_BUCKET constant to bucketService

--- services/bucketService.tsx
+++ services/bucketService.tsx
@@ -1,6 +1,10 @@
 import { getFilePreview } from "@/services/apiService";
-import { USER_BUCKET } from "@/constants/config";
+import { USER_BUCKET, MESSAGE_BUCKET } from "@/constants/config";
 
 export async function getUserImage(imageId: string) {
   return getFilePreview(USER_BUCKET, imageId);
+}
+
+export async function getMessageImage(imageId: string) {
+  return getFilePreview(MESSAGE_BUCKET, imageId);
 }
