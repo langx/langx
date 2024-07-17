3207a1178d71e7d568e7ffd50b478e18fc7b9f78
xue <behicsakar@gmail.com>
2024-07-17 19:04:32 +0300

    refactor: Update API endpoint and add createMessage function

--- services/messageService.tsx
+++ services/messageService.tsx
@@ -1,8 +1,13 @@
 import { Query } from "react-native-appwrite";
+import axios from "axios";
 
 import { Message } from "@/models/Message";
 import { listDocuments } from "@/services/apiService";
-import { MESSAGES_COLLECTION, PAGINATION_LIMIT } from "@/constants/config";
+import {
+  API_ENDPOINT,
+  MESSAGES_COLLECTION,
+  PAGINATION_LIMIT,
+} from "@/constants/config";
 
 export async function listMessages(params: any) {
   const roomId = params?.roomId;
@@ -23,4 +28,31 @@ export async function listMessages(params: any) {
   } catch (error) {
     throw new Error(error.message);
   }
+}
+
+export async function createMessage(params: any) {
+  console.log(params);
+  try {
+    // const response = await axios.post(`${API_ENDPOINT}/message`, {
+    const response = await axios.get(`${API_ENDPOINT}`, {
+      // headers: {
+      //   'x-appwrite-user-id': currentUserId,
+      //   'x-appwrite-jwt': jwt,
+      // },
+    });
+
+    console.log("Response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+  // const currentUserId = params?.currentUserId;
+  // const jwt = params?.jwt;
+
+  // console.log("currentUserId", currentUserId);
+  // console.log("jwt", jwt);
+
+  // axios.defaults.headers.common["x-appwrite-user-id"] = currentUserId;
+  // axios.defaults.headers.common["x-appwrite-jwt"] = jwt;
 }
