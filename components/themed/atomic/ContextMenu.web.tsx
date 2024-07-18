ecfe7af67f2ef90243fa288649709c6357442113
xue <behicsakar@gmail.com>
2024-07-19 00:19:42 +0300

    refactor: Update ContextMenuWeb component to use themed ContextMenu

--- components/themed/atomic/ContextMenu.web.tsx
+++ components/themed/atomic/ContextMenu.web.tsx
@@ -6,6 +6,7 @@ import { Colors } from "@/constants/Colors";
 const ContextMenuWeb = ({ actions, onPress, children, style }) => {
   const theme = useColorScheme() === "dark" ? "dark" : "light";
   const [buttonVisible, setButtonVisible] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const handleMouseEnter = () => {
     setButtonVisible(true);
@@ -13,10 +14,35 @@ const ContextMenuWeb = ({ actions, onPress, children, style }) => {
 
   const handleMouseLeave = () => {
     setButtonVisible(false);
+    setHoveredIndex(null);
   };
 
   const handleActionClick = (index) => {
     onPress({ nativeEvent: { index } });
+  };
+
+  const getColor = (index, hoveredIndex, theme) => {
+    switch (index) {
+      case 0:
+        return hoveredIndex === index
+          ? Colors[theme].success
+          : Colors[theme].gray2;
+      case 1:
+        return hoveredIndex === index
+          ? Colors[theme].secondary
+          : Colors[theme].gray2;
+      case 2:
+        return hoveredIndex === index
+          ? Colors[theme].primary
+          : Colors[theme].gray2;
+      case 3:
+        return hoveredIndex === index
+          ? Colors[theme].error
+          : Colors[theme].gray2;
+      // Add more cases as needed
+      default:
+        return Colors[theme].defaultColor;
+    }
   };
 
   return (
@@ -32,10 +58,12 @@ const ContextMenuWeb = ({ actions, onPress, children, style }) => {
     >
       {buttonVisible && (
         <div style={{ display: "flex", gap: 3, marginRight: 6 }}>
-          {[...actions].reverse().map((action, index) => (
+          {actions.map((action, index) => (
             <button
               key={index}
               onClick={() => handleActionClick(index)}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
               style={{
                 cursor: "pointer",
                 backgroundColor: "transparent",
@@ -46,7 +74,7 @@ const ContextMenuWeb = ({ actions, onPress, children, style }) => {
               <Ionicons
                 name={action.IonIcon}
                 size={20}
-                color={Colors[theme].gray2}
+                color={getColor(index, hoveredIndex, theme)}
               />
             </button>
           ))}
