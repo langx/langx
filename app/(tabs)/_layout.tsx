6338202e90349a3bfb9379f48d7c0b2956ce9b8c
xue <behicsakar@gmail.com>
2024-07-21 22:14:08 +0300

    fix: Update TabsLayout component to use useAuth hook
    This commit updates the TabsLayout component to use the useAuth hook for retrieving the current user. The useSelector hook from react-redux has been replaced with the useAuth hook, which provides access to the current user object. The currentUserId is extracted from the currentUser object and passed to the useRealtime hook for establishing a WebSocket connection. This change improves the maintainability and readability of the code.
    
    Related to recent user commits:
    - fix: logout bug
    - refactor: prettier
    - refactor: Update login screen to use Formik and Yup for form validation
    - refactor: Reorder imports
    - refactor: Simplify renderItem function in recommended.tsx
    - refactor: Update headerLeft in PagesLayout and BackButtonForIOS component
    - refactor: Add BackButtonForIOS component to PagesLayout
    - refactor: Simplify headerLeft, headerTitle, and headerRight functions in RoomLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,88 +1,82 @@
 import { Tabs } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { useAuth } from "@/hooks/useAuth";
 import { useRealtime } from "@/hooks/useRealtime";
 import { TabBarItem } from "@/components/navigation/TabBarItem";
 
-const TabsLayout = () => {
+export default function TabsLayout() {
   const colorScheme = useColorScheme();
   const theme = colorScheme ?? "light";
   const insets = useSafeAreaInsets();
 
   // Start Realtime Websocket connection
-  const currentUser = useSelector((state: RootState) => state.auth.user);
-  if (currentUser) useRealtime(currentUser.$id);
+  const { currentUser } = useAuth();
+  const currentUserId = currentUser?.$id;
+  useRealtime(currentUserId);
 
-  return (
-    <Tabs
-      screenOptions={({ route }) => ({
-        tabBarActiveTintColor: Colors[theme].primary,
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarHideOnKeyboard: true,
-        tabBarStyle: {
-          backgroundColor: Colors[theme].gray5,
-          justifyContent: "center",
-          alignItems: "center",
-          height: insets.bottom + 60,
-        },
-      })}
-    >
-      <Tabs.Screen
-        name="rooms"
-        options={{
-          title: "Chats",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarItem
-              icon="chatbubbles"
-              color={color}
-              label="Chats"
-              focused={focused}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Community",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarItem
-              icon="compass"
-              color={color}
-              label="Community"
-              focused={focused}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarItem
-              icon="person"
-              color={color}
-              label="Profile"
-              focused={focused}
-            />
-          ),
-        }}
-      />
-    </Tabs>
-  );
-};
-
-export default function TabLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <TabsLayout />
+      <Tabs
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: Colors[theme].primary,
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarHideOnKeyboard: true,
+          tabBarStyle: {
+            backgroundColor: Colors[theme].gray5,
+            justifyContent: "center",
+            alignItems: "center",
+            height: insets.bottom + 60,
+          },
+        })}
+      >
+        <Tabs.Screen
+          name="rooms"
+          options={{
+            title: "Chats",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarItem
+                icon="chatbubbles"
+                color={color}
+                label="Chats"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Community",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarItem
+                icon="compass"
+                color={color}
+                label="Community"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarItem
+                icon="person"
+                color={color}
+                label="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </GestureHandlerRootView>
   );
 }
