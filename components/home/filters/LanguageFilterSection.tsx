d1ffaee57e92564440de9e09869ba90c34f0ba58
xue <behicsakar@gmail.com>
2024-07-23 17:06:21 +0300

    refactor: Remove unused useEffect and useState imports in LanguageFilterSection

--- components/home/filters/LanguageFilterSection.tsx
+++ components/home/filters/LanguageFilterSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FlatList, StyleSheet, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { CheckBox } from "react-native-elements";
