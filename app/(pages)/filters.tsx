c7de2926193e151c568d61ce9990d5e2caf75db9
xue <behicsakar@gmail.com>
2024-07-23 15:39:21 +0300

    refactor: Update Filters page to use ThemedView and FlatList

--- app/(pages)/filters.tsx
+++ app/(pages)/filters.tsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Pressable, View, Text } from "react-native";
-import { router } from "expo-router";
+import React, { useCallback } from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+import LanguageFilterSection from "@/components/home/filters/LanguageFilterSection";
+import { ThemedView } from "@/components/themed/atomic/ThemedView";
 
 const Filters = () => {
+  const components = [
+    {
+      component: <LanguageFilterSection />,
+      key: "LanguagesFilterSection",
+    },
+  ];
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <ThemedView style={styles.itemContainer}>{item.component}</ThemedView>
+    ),
+    []
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Filters</Text>
-      <Pressable onPress={() => router.back()}>
-        <Text>Go back</Text>
-      </Pressable>
-    </View>
+    <ThemedView style={styles.container}>
+      <FlatList
+        data={components}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
+      />
+    </ThemedView>
   );
 };
 
 export default Filters;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemContainer: {
+    marginBottom: 10,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
