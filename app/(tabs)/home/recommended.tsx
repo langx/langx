bb76e7128a6f056a32353253e96e9467db155bdc
xue <behicsakar@gmail.com>
2024-07-25 17:05:40 +0300

    feat: Update recommended screen to use responsive layout

--- app/(tabs)/home/recommended.tsx
+++ app/(tabs)/home/recommended.tsx
@@ -9,6 +9,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import _ from "lodash";
 
 import { Colors } from "@/constants/Colors";
+import { getNumColumns } from "@/constants/responsive";
 import { useDatabase } from "@/hooks/useDatabase";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { useAuth } from "@/hooks/useAuth";
@@ -17,30 +18,35 @@ import { ThemedView } from "@/components/themed/atomic/ThemedView";
 import { ThemedText } from "@/components/themed/atomic/ThemedText";
 import UserCard from "@/components/home/UserCard";
 
-const breakpoints = {
-  xs: 0,
-  sm: 320,
-  md: 480,
-  lg: 768,
-  xl: 1024,
-  xxl: 1440,
-  mobile: 640,
-};
-const getNumColumns = (width) => {
-  if (width < breakpoints.sm) return 2; // xs
-  else if (width >= breakpoints.sm && width < breakpoints.md) return 2; // sm
-  else if (width >= breakpoints.md && width < breakpoints.lg) return 3; // md
-  else if (width >= breakpoints.lg && width < breakpoints.xl) return 4; // lg
-  else if (width >= breakpoints.xl && width < breakpoints.xxl) return 6; // xl
-  else if (width >= breakpoints.xxl) return 8; // xxl
-};
-
 export default function RecomendedScreen() {
   const theme = useColorScheme() ?? "light";
   const { currentUser } = useAuth();
 
+  const [screenWidth, setScreenWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [numColumns, setNumColumns] = useState(getNumColumns(screenWidth));
   const [filters, setFilters] = useState(null);
   const isFirstRun = useRef(true);
+
+  // Responsive layout
+  useEffect(() => {
+    const updateLayout = () => {
+      const newWidth = Dimensions.get("window").width;
+      setScreenWidth(newWidth);
+      console.log("Screen width changed to", newWidth);
+    };
+
+    // Subscribe to dimension changes
+    const subscription = Dimensions.addEventListener("change", updateLayout);
+
+    // Return a cleanup function that removes the event listener
+    return () => subscription.remove();
+  }, []);
+
+  useEffect(() => {
+    setNumColumns(getNumColumns(screenWidth));
+  }, [screenWidth]);
 
   const {
     data: users,
@@ -75,28 +81,6 @@ export default function RecomendedScreen() {
 
     if (filters) refetch();
   }, [filters]);
-
-  const [screenWidth, setScreenWidth] = useState(
-    Dimensions.get("window").width
-  );
-  const [numColumns, setNumColumns] = useState(getNumColumns(screenWidth));
-  useEffect(() => {
-    const updateLayout = () => {
-      const newWidth = Dimensions.get("window").width;
-      setScreenWidth(newWidth);
-      console.log("Screen width changed to", newWidth);
-    };
-
-    // Subscribe to dimension changes
-    const subscription = Dimensions.addEventListener("change", updateLayout);
-
-    // Return a cleanup function that removes the event listener
-    return () => subscription.remove();
-  }, []);
-
-  useEffect(() => {
-    setNumColumns(getNumColumns(screenWidth));
-  }, [screenWidth]);
 
   useEffect(() => {
     console.log("recommended page:", users?.length);
