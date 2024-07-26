69a24986f11a536926ae272747815f567de6f5ca
xue <behicsakar@gmail.com>
2024-07-26 16:46:03 +0300

    feat: Add counter badge to Chats tab in TabsLayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -38,6 +38,7 @@ export default function TabsLayout() {
                 color={color}
                 label="Chats"
                 focused={focused}
+                counter={10}
               />
             ),
           }}
