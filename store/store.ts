82b1dd5e129564d82dcd52024db13404e2163e8c
xue <behicsakar@gmail.com>
2024-07-27 15:11:00 +0300

    feat: Add userSlice to store for managing user data

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/authSlice';
+import userSlice from '@/store/userSlice';
 import roomReducer from '@/store/roomSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userSlice,
     room: roomReducer,
   },
 });
