43c1c819e91458fcf54092ab9c907d1571426450
xue <behicsakar@gmail.com>
2024-07-27 14:47:42 +0300

    refactor: Update UserScreen header styling and navigation behavior

--- app/(home)/[username].tsx
+++ app/(home)/[username].tsx
@@ -5,40 +5,45 @@ import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
 
-const UserScreen = () => {
+const StackLayout = () => {
   const { username } = useLocalSearchParams<{ username: string }>();
+  return (
+    <Stack.Screen
+      options={{
+        headerShown: true,
+        headerBackVisible: true,
+        headerShadowVisible: false,
+        headerBackTitleVisible: true,
+        headerBackButtonMenuEnabled: true,
+        headerBackTitle: "Back",
+        headerStyle: { backgroundColor: Colors.light.primary },
+        headerTintColor: Colors.light.black,
+        headerBackTitleStyle: {
+          fontFamily: "Lexend-Bold",
+        },
+        headerTitle: `@${username}`,
+        headerTitleStyle: {
+          fontFamily: "Lexend-Bold",
+          color: Colors.light.black,
+        },
+        headerRight: () => (
+          <Pressable onPress={() => router.back()}>
+            <Ionicons
+              name="ellipsis-horizontal"
+              size={24}
+              color={Colors.light.black}
+            />
+          </Pressable>
+        ),
+      }}
+    />
+  );
+};
 
+const UserScreen = () => {
   return (
     <>
-      <Stack.Screen
-        options={{
-          headerShown: true,
-          headerBackVisible: true,
-          headerShadowVisible: false,
-          headerBackTitleVisible: true,
-          headerBackButtonMenuEnabled: true,
-          headerBackTitle: "Back",
-          headerStyle: { backgroundColor: Colors.light.primary },
-          headerTintColor: Colors.light.black,
-          headerBackTitleStyle: {
-            fontFamily: "Lexend-Bold",
-          },
-          headerTitle: `@${username}`,
-          headerTitleStyle: {
-            fontFamily: "Lexend-Bold",
-            color: Colors.light.black,
-          },
-          headerRight: () => (
-            <Pressable onPress={() => router.back()}>
-              <Ionicons
-                name="ellipsis-horizontal"
-                size={24}
-                color={Colors.light.black}
-              />
-            </Pressable>
-          ),
-        }}
-      />
+      <StackLayout />
       <ThemedView
         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
