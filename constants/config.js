69eb6960729a9cdfea5ecf4178b8da7160852711
xue <behicsakar@gmail.com>
2024-07-30 22:20:12 +0300

    refactor: Update config file with NEW_PASSWORD_URL

--- constants/config.js
+++ constants/config.js
@@ -2,9 +2,6 @@ export const APP_ENDPOINT = "https://app.langx.io";
 export const DB_ENDPOINT = "https://db.langx.io/v1";
 export const API_ENDPOINT = "https://api.langx.io/api";
 
-export const APP_PACKAGE_NAME = "tech.newchapter.languageXchange";
-export const PLAUSIBLE_API_URL = "https://insight.langx.io/api/event";
-
 export const APP_PROJECT = "650750d21e4a6a589be3";
 export const APP_DATABASE = "650750f16cd0c482bb83";
 export const USERS_COLLECTION = "65103e2d3a6b4d9494c8";
@@ -13,6 +10,10 @@ export const MESSAGES_COLLECTION = "65075108a4025a4f5bd7";
 export const WALLET_COLLECTION = "66622b8a000b305b236c";
 export const USER_BUCKET = "6515f94d20becd47cb40";
 export const MESSAGE_BUCKET = "655fedc46d24b615878a";
+
+export const APP_PACKAGE_NAME = "tech.newchapter.languageXchange";
+export const PLAUSIBLE_API_URL = "https://insight.langx.io/api/event";
+export const NEW_PASSWORD_URL = "langx://(auth)/new-password";
 
 export const PAGINATION_LIMIT = 20;
 export const PRESENCE_INTERVAL = 59 * 1000;
