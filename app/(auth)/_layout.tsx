c08492bb1a7e25904a7c7a5155507656f8b62e0f
xue <behicsakar@gmail.com>
2024-07-30 18:14:26 +0300

    refactor: Update login and authentication screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,21 +5,18 @@ import { Stack } from "expo-router";
 const AuthLayout = () => {
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        // options={{ headerShown: false, presentation: "modal" }}
-      />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
         name="login"
-        // options={{ headerShown: false, presentation: "modal" }}
+        // options={{ headerShown: false }}
       />
       <Stack.Screen
         name="register"
-        // options={{ headerShown: false, presentation: "modal" }}
+        // options={{ headerShown: false }}
       />
       <Stack.Screen
         name="reset-password"
-        // options={{ headerShown: false, presentation: "modal" }}
+        // options={{ headerShown: false  }}
       />
     </Stack>
   );
