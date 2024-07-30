5a14ba08b5b2de8cef93ba67a5e68f469af7022b
xue <behicsakar@gmail.com>
2024-07-30 16:06:10 +0300

    refactor: Add useSignInUser hook for signing in users

---
+++ hooks/useSingInUser.ts
@@ -0,0 +1,39 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+
+import { createJWT, getAccount, getCurrentUser } from '@/services/authService';
+import { setAccount, setJwt, setSession, setUser } from '@/store/authSlice';
+
+const useSignInUser = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const signInUser = useCallback(
+    async (session) => {
+      try {
+        const [account, user] = await Promise.all([
+          getAccount(),
+          getCurrentUser(),
+        ]);
+        const jwt = await createJWT();
+        dispatch(setAccount(account));
+        dispatch(setUser(user));
+        dispatch(setSession(session));
+        dispatch(setJwt(jwt));
+
+        router.push('/(home)/(tabs)');
+        Alert.alert('Success', 'User signed in successfully');
+      } catch (error) {
+        console.error('Error signing in user:', error);
+        Alert.alert('Error', 'Failed to sign in user');
+      }
+    },
+    [dispatch, router]
+  );
+
+  return signInUser;
+};
+
+export default useSignInUser;
