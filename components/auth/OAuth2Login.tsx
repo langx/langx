800a1eebf8bef3579e66dfb4c7a54e878f9aa92b
xue <behicsakar@gmail.com>
2024-07-30 16:06:50 +0300

    feat: Update OAuth2Login component to use expo-web-browser for social media login

--- components/auth/OAuth2Login.tsx
+++ components/auth/OAuth2Login.tsx
@@ -1,8 +1,14 @@
-import { TouchableOpacity, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { TouchableOpacity, StyleSheet, Platform } from "react-native";
 import { OAuthProvider } from "react-native-appwrite";
+import {
+  openAuthSessionAsync,
+  WebBrowserAuthSessionResult,
+} from "expo-web-browser";
 
-import { loginWithProvider } from "@/services/authService";
+import { createOAuth2Token, createSession } from "@/services/authService";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import useSignInUser from "@/hooks/useSingInUser";
 import { ThemedView } from "@/components/themed/atomic/ThemedView";
 import GoogleSVG from "@/assets/images/oauth2/Google";
 import AppleSVG from "@/assets/images/oauth2/Apple";
@@ -11,11 +17,62 @@ import DiscordSVG from "@/assets/images/oauth2/Discord";
 
 const OAuth2Login = () => {
   const theme = useColorScheme() ?? "light";
+  const signInUser = useSignInUser();
+  const [browserResult, setBrowserResult] =
+    useState<WebBrowserAuthSessionResult | null>(null);
 
-  const signInWithProvider = (provider: OAuthProvider) => {
+  const signInWithProvider = async (provider: OAuthProvider) => {
     console.log(`Signing in with ${provider}`);
-    loginWithProvider(provider);
+    const url = createOAuth2Token(provider);
+
+    if (url) {
+      const urlString = url.toString();
+      console.log(urlString);
+
+      if (Platform.OS !== "web") {
+        // Prevent the default behavior of linking to the default browser on native.
+        // event.preventDefault();
+        const result = await openAuthSessionAsync(urlString);
+        setBrowserResult(result);
+      }
+    } else {
+      console.error("Failed to obtain URL for provider:", provider);
+    }
   };
+
+  useEffect(() => {
+    const handleBrowserResult = async () => {
+      if (browserResult) {
+        console.log("Browser result:", browserResult, typeof browserResult);
+        switch (browserResult.type) {
+          case "success":
+            console.log("Browser result success:", browserResult);
+            const urlObject = new URL(browserResult.url);
+            const secret = urlObject.searchParams.get("secret");
+            const userId = urlObject.searchParams.get("userId");
+
+            try {
+              const session = await createSession(userId, secret);
+              signInUser(session); // Use the custom hook to handle the sign-in process
+            } catch (error) {
+              console.error("Error creating session:", error);
+            }
+            break;
+          case "cancel":
+            console.log("Browser result cancel:", browserResult);
+            break;
+          case "dismiss":
+            console.log("Browser result dismiss:", browserResult);
+            break;
+          default:
+            console.log("Browser result unknown:", browserResult);
+            break;
+        }
+      }
+    };
+
+    handleBrowserResult();
+  }, [browserResult, signInUser]);
 
   return (
     <ThemedView style={styles.container}>
