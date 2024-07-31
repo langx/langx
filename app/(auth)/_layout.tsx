530735c125c0278f740d0140033bd0692a7e4da9
xue <behicsakar@gmail.com>
2024-07-31 08:55:28 +0300

    refactor: Update AuthLayout component to include Toast component
    The commit updates the AuthLayout component in the _layout.tsx file to include the Toast component for displaying success and error messages. The Toast component is imported from the react-native-toast-message library and added as a child component within the AuthLayout component. This enhancement improves the user experience by providing visual feedback for various actions within the authentication flow.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,97 +1,101 @@
 import React from "react";
-
 import { Stack } from "expo-router";
+import Toast from "react-native-toast-message";
+
 import { Colors } from "@/constants/Colors";
 
 const AuthLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="login"
-        options={{
-          headerTitle: "Login",
-          headerShown: false,
-          headerBackVisible: true,
-          headerShadowVisible: false,
-          headerBackTitleVisible: true,
-          headerBackButtonMenuEnabled: true,
-          headerBackTitle: "Back",
-          headerTintColor: Colors.light.black,
-          headerStyle: {
-            backgroundColor: Colors.light.primary,
-          },
-          headerLargeTitleStyle: {
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-          headerBackTitleStyle: {
-            fontFamily: "Lexend-Bold",
-          },
-          headerTitleStyle: {
-            fontSize: 18,
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-        }}
-      />
-      <Stack.Screen
-        name="register"
-        options={{
-          headerTitle: "Register",
-          headerShown: false,
-          headerBackVisible: true,
-          headerShadowVisible: false,
-          headerBackTitleVisible: true,
-          headerBackButtonMenuEnabled: true,
-          headerBackTitle: "Back",
-          headerTintColor: Colors.light.black,
-          headerStyle: {
-            backgroundColor: Colors.light.primary,
-          },
-          headerLargeTitleStyle: {
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-          headerBackTitleStyle: {
-            fontFamily: "Lexend-Bold",
-          },
-          headerTitleStyle: {
-            fontSize: 18,
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-        }}
-      />
-      <Stack.Screen
-        name="reset-password"
-        options={{
-          headerShown: false,
-          headerTitle: "Reset Password",
-          headerBackVisible: true,
-          headerShadowVisible: false,
-          headerBackTitleVisible: true,
-          headerBackButtonMenuEnabled: true,
-          headerBackTitle: "Back",
-          headerTintColor: Colors.light.black,
-          headerStyle: {
-            backgroundColor: Colors.light.primary,
-          },
-          headerLargeTitleStyle: {
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-          headerBackTitleStyle: {
-            fontFamily: "Lexend-Bold",
-          },
-          headerTitleStyle: {
-            fontSize: 18,
-            color: Colors.light.black,
-            fontFamily: "Lexend-Bold",
-          },
-        }}
-      />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="login"
+          options={{
+            headerTitle: "Login",
+            headerShown: false,
+            headerBackVisible: true,
+            headerShadowVisible: false,
+            headerBackTitleVisible: true,
+            headerBackButtonMenuEnabled: true,
+            headerBackTitle: "Back",
+            headerTintColor: Colors.light.black,
+            headerStyle: {
+              backgroundColor: Colors.light.primary,
+            },
+            headerLargeTitleStyle: {
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+            headerBackTitleStyle: {
+              fontFamily: "Lexend-Bold",
+            },
+            headerTitleStyle: {
+              fontSize: 18,
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="register"
+          options={{
+            headerTitle: "Register",
+            headerShown: false,
+            headerBackVisible: true,
+            headerShadowVisible: false,
+            headerBackTitleVisible: true,
+            headerBackButtonMenuEnabled: true,
+            headerBackTitle: "Back",
+            headerTintColor: Colors.light.black,
+            headerStyle: {
+              backgroundColor: Colors.light.primary,
+            },
+            headerLargeTitleStyle: {
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+            headerBackTitleStyle: {
+              fontFamily: "Lexend-Bold",
+            },
+            headerTitleStyle: {
+              fontSize: 18,
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="reset-password"
+          options={{
+            headerShown: false,
+            headerTitle: "Reset Password",
+            headerBackVisible: true,
+            headerShadowVisible: false,
+            headerBackTitleVisible: true,
+            headerBackButtonMenuEnabled: true,
+            headerBackTitle: "Back",
+            headerTintColor: Colors.light.black,
+            headerStyle: {
+              backgroundColor: Colors.light.primary,
+            },
+            headerLargeTitleStyle: {
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+            headerBackTitleStyle: {
+              fontFamily: "Lexend-Bold",
+            },
+            headerTitleStyle: {
+              fontSize: 18,
+              color: Colors.light.black,
+              fontFamily: "Lexend-Bold",
+            },
+          }}
+        />
+      </Stack>
+      <Toast />
+    </>
   );
 };
 export default AuthLayout;
